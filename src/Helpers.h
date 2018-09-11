9f7a1ad61f2412ba3359bf8b1c0d65d120d3e1de
zufuliu <zufuliu@gmail.com>
2018-09-11 16:34:57 +0800

    Fix clang-tidy error.

--- src/Helpers.h
+++ src/Helpers.h
@@ -24,7 +24,7 @@
 #if (defined(__GNUC__) || defined(__clang__)) && !defined(__cplusplus)
 // https://stackoverflow.com/questions/19452971/array-size-macro-that-rejects-pointers
 // trigger error for pointer: GCC: void value not ignored as it ought to be. Clang: invalid operands to binary expression.
-#define COUNTOF(ar)	_Generic(&(ar), typeof((ar)[0]) **: (void)0, default: _countof(ar))
+#define COUNTOF(ar)	_Generic(&(ar), __typeof__((ar)[0]) **: (void)0, default: _countof(ar))
 // trigger warning for non-literal string: GCC: division by zero [-Wdiv-by-zero]. Clang: division by zero is undefined [-Wdivision-by-zero].
 #define CSTRLEN(s)	(__builtin_constant_p(s) ? (_countof(s) - 1) : (1 / 0))
 #else
@@ -157,11 +157,11 @@ BOOL IniSectionGetBoolImpl(LPCWSTR lpCachedIniSection, LPCWSTR lpName, int keyLe
 BOOL IniSectionSetString(LPWSTR lpCachedIniSection, LPCWSTR lpName, LPCWSTR lpString);
 
 #define IniSectionGetInt(lpCachedIniSection, lpName, iDefault) \
-	IniSectionGetIntImpl(lpCachedIniSection, lpName, CSTRLEN(lpName), iDefault)
+	IniSectionGetIntImpl(lpCachedIniSection, lpName, CSTRLEN(lpName), (iDefault))
 #define IniSectionGetBool(lpCachedIniSection, lpName, bDefault) \
-	IniSectionGetBoolImpl(lpCachedIniSection, lpName, CSTRLEN(lpName), bDefault)
+	IniSectionGetBoolImpl(lpCachedIniSection, lpName, CSTRLEN(lpName), (bDefault))
 #define IniSectionGetString(lpCachedIniSection, lpName, lpDefault, lpReturnedString, cchReturnedString) \
-	IniSectionGetStringImpl(lpCachedIniSection, lpName, CSTRLEN(lpName), lpDefault, lpReturnedString, cchReturnedString)
+	IniSectionGetStringImpl(lpCachedIniSection, lpName, CSTRLEN(lpName), (lpDefault), (lpReturnedString), (cchReturnedString))
 
 static inline int IniSectionGetIntEx(LPCWSTR lpCachedIniSection, LPCWSTR lpName, int iDefault) {
 	return IniSectionGetIntImpl(lpCachedIniSection, lpName, lstrlen(lpName), iDefault);
