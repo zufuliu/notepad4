zufuliu/notepad4

commit 8bfe3ec2ea6c2f0a8b1ffea616ed7b225a134e19

Only update statusbar item text after it has been changed, fix statusbar flickering.

src/Notepad2.c
@@ -299,11 +299,17 @@ WCHAR	wchWndClass[16] = WC_NOTEPAD2;
 
 // rarely changed statusbar items
 struct CachedStatusItem {
-	WCHAR tchLexerName[128];
 	WCHAR tchZoom[16];
 
-	LPCWSTR pszLexerName; // point to tchLexerName or a literal string
+	LPCWSTR pszLexerName;
+	LPCWSTR pszEOLMode;
 	LPCWSTR pszOvrMode;
+
+	BOOL lexerNameChanged;	// STATUS_LEXER
+	BOOL encodingChanged;	// STATUS_CODEPAGE
+	BOOL eolModeChanged;	// STATUS_EOLMODE
+	BOOL ovrModeChanged;	// STATUS_OVRMODE
+	BOOL zoomChanged;		// STATUS_DOCZOOM
 } cachedStatusItem;
 
 HINSTANCE	g_hInstance;
@@ -744,9 +750,9 @@ HWND InitInstance(HINSTANCE hInstance, int nCmdShow) {
 		iSrcEncoding = Encoding_MatchW(lpEncodingArg);
 	}
 
+	BOOL bOpened = FALSE;
 	// Pathname parameter
 	if (lpFileArg /*&& !flagNewFromClipboard*/) {
-		BOOL bOpened = FALSE;
 
 		// Open from Directory
 		if (PathIsDirectory(lpFileArg)) {
@@ -786,7 +792,7 @@ HWND InitInstance(HINSTANCE hInstance, int nCmdShow) {
 	}
 
 	if (!bFileLoadCalled) {
-		FileLoad(TRUE, TRUE, FALSE, FALSE, L"");
+		bOpened = FileLoad(TRUE, TRUE, FALSE, FALSE, L"");
 	}
 
 	// reset
@@ -895,6 +901,12 @@ HWND InitInstance(HINSTANCE hInstance, int nCmdShow) {
 		SetNotifyIconTitle(hwndMain);
 	}
 
+	if (!bOpened) {
+		UpdateStatusBarCache(STATUS_CODEPAGE);
+		UpdateStatusBarCache(STATUS_EOLMODE);
+	}
+	UpdateStatusBarCache(STATUS_OVRMODE);
+	UpdateStatusBarCache(STATUS_DOCZOOM);
 	UpdateToolbar();
 	UpdateStatusbar();
 
@@ -1424,13 +1436,13 @@ void UpdateWindowTitle(void) {
 				IDS_READONLY, bReadOnly, szTitleExcerpt);
 }
 
-void UpdateSelectionMarginWidth() {
+void UpdateSelectionMarginWidth(void) {
 	// fixed width to put arrow cursor.
 	int width = bShowSelectionMargin ? RoundToCurrentDPI(16) : 0;
 	SciCall_SetMarginWidth(MARGIN_SELECTION, width);
 }
 
-void UpdateFoldMarginWidth() {
+void UpdateFoldMarginWidth(void) {
 	if (bShowCodeFolding) {
 		const int iLineMarginWidthNow = SciCall_GetMarginWidth(MARGIN_FOLD_INDEX);
 		const int iLineMarginWidthFit = SciCall_TextWidth(STYLE_LINENUMBER, "+_");
@@ -1623,7 +1635,6 @@ LRESULT MsgCreate(HWND hwnd, WPARAM wParam, LPARAM lParam) {
 
 	// Create Toolbar and Statusbar
 	CreateBars(hwnd, hInstance);
-	UpdateStatusBarCache(STATUS_OVRMODE);
 
 	// Window Initialization
 
@@ -2007,15 +2018,30 @@ void MsgSize(HWND hwnd, WPARAM wParam, LPARAM lParam) {
 void UpdateStatusBarCache(int item) {
 	switch (item) {
 	case STATUS_LEXER:
-		cachedStatusItem.pszLexerName = Style_GetCurrentLexerName(cachedStatusItem.tchLexerName, COUNTOF(cachedStatusItem.tchLexerName));
+		cachedStatusItem.pszLexerName = Style_GetCurrentLexerName();
+		cachedStatusItem.lexerNameChanged = TRUE;
+		UpdateStatusBarWidth();
+		break;
+
+	case STATUS_CODEPAGE:
+		Encoding_GetLabel(iEncoding);
+		cachedStatusItem.encodingChanged = TRUE;
+		UpdateStatusBarWidth();
+		break;
+
+	case STATUS_EOLMODE:
+		cachedStatusItem.pszEOLMode = (iEOLMode == SC_EOL_LF) ? L"LF" : ((iEOLMode == SC_EOL_CR) ? L"CR" : L"CR+LF");
+		cachedStatusItem.eolModeChanged = TRUE;
 		break;
 
 	case STATUS_OVRMODE:
 		cachedStatusItem.pszOvrMode = SendMessage(hwndEdit, SCI_GETOVERTYPE, 0, 0) ? L"OVR" : L"INS";
+		cachedStatusItem.ovrModeChanged = TRUE;
 		break;
 
 	case STATUS_DOCZOOM:
 		wsprintf(cachedStatusItem.tchZoom, L" %i%%", iZoomLevel);
+		cachedStatusItem.zoomChanged = TRUE;
 		break;
 	}
 }
@@ -2026,8 +2052,6 @@ void UpdateStatusBarWidth(void) {
 
 	GetClientRect(hwndMain, &rc);
 	const int cx = rc.right - rc.left;
-
-	Encoding_GetLabel(iEncoding);
 	const int iBytes = SciCall_GetLength();
 
 	aWidth[1] = StatusCalcPaneWidth(hwndStatus, cachedStatusItem.pszLexerName) + 4;
@@ -2802,8 +2826,8 @@ LRESULT MsgCommand(HWND hwnd, WPARAM wParam, LPARAM lParam) {
 				iEncoding = iNewEncoding;
 			}
 
+			UpdateStatusBarCache(STATUS_CODEPAGE);
 			UpdateToolbar();
-			UpdateStatusBarWidth();
 			UpdateStatusbar();
 			UpdateWindowTitle();
 		}
@@ -2850,6 +2874,7 @@ LRESULT MsgCommand(HWND hwnd, WPARAM wParam, LPARAM lParam) {
 		SendMessage(hwndEdit, SCI_SETEOLMODE, iEOLMode, 0);
 		SendMessage(hwndEdit, SCI_CONVERTEOLS, iEOLMode, 0);
 		EditFixPositions(hwndEdit);
+		UpdateStatusBarCache(STATUS_EOLMODE);
 		UpdateToolbar();
 		UpdateStatusbar();
 		UpdateWindowTitle();
@@ -6726,18 +6751,28 @@ void UpdateStatusbar(void) {
 	StrFormatByteSize(iBytes, tchBytes, COUNTOF(tchBytes));
 	FormatString(tchDocSize, COUNTOF(tchDocSize), IDS_DOCSIZE, tchBytes);
 
-	Encoding_GetLabel(iEncoding);
-	LPCWSTR pszEOLMode = (iEOLMode == SC_EOL_LF) ? L"LF" : ((iEOLMode == SC_EOL_CR) ? L"CR" : L"CR+LF");
-
 	StatusSetText(hwndStatus, STATUS_DOCPOS, tchDocPos);
-	StatusSetText(hwndStatus, STATUS_LEXER, cachedStatusItem.pszLexerName);
-	StatusSetText(hwndStatus, STATUS_CODEPAGE, mEncoding[iEncoding].wchLabel);
-	StatusSetText(hwndStatus, STATUS_EOLMODE, pszEOLMode);
-	StatusSetText(hwndStatus, STATUS_OVRMODE, cachedStatusItem.pszOvrMode);
+	if (cachedStatusItem.lexerNameChanged) {
+		StatusSetText(hwndStatus, STATUS_LEXER, cachedStatusItem.pszLexerName);
+		cachedStatusItem.lexerNameChanged = FALSE;
+	}
+	if (cachedStatusItem.encodingChanged) {
+		StatusSetText(hwndStatus, STATUS_CODEPAGE, mEncoding[iEncoding].wchLabel);
+		cachedStatusItem.encodingChanged = FALSE;
+	}
+	if (cachedStatusItem.eolModeChanged) {
+		StatusSetText(hwndStatus, STATUS_EOLMODE, cachedStatusItem.pszEOLMode);
+		cachedStatusItem.eolModeChanged = FALSE;
+	}
+	if (cachedStatusItem.ovrModeChanged) {
+		StatusSetText(hwndStatus, STATUS_OVRMODE, cachedStatusItem.pszOvrMode);
+		cachedStatusItem.ovrModeChanged = FALSE;
+	}
 	StatusSetText(hwndStatus, STATUS_DOCSIZE, tchDocSize);
-	StatusSetText(hwndStatus, STATUS_DOCZOOM, cachedStatusItem.tchZoom);
-
-	//InvalidateRect(hwndStatus, NULL, TRUE);
+	if (cachedStatusItem.zoomChanged) {
+		StatusSetText(hwndStatus, STATUS_DOCZOOM, cachedStatusItem.tchZoom);
+		cachedStatusItem.zoomChanged = FALSE;
+	}
 }
 
 //=============================================================================
@@ -6861,6 +6896,8 @@ BOOL FileLoad(BOOL bDontSave, BOOL bNew, BOOL bReload, BOOL bNoEncDetect, LPCWST
 		iEncoding = iDefaultEncoding;
 		iOriginalEncoding = iDefaultEncoding;
 		SendMessage(hwndEdit, SCI_SETCODEPAGE, (iDefaultEncoding == CPI_DEFAULT) ? iDefaultCodePage : SC_CP_UTF8, 0);
+		UpdateStatusBarCache(STATUS_CODEPAGE);
+		UpdateStatusBarCache(STATUS_EOLMODE);
 		UpdateWindowTitle();
 
 		// Terminate file watching
@@ -6942,12 +6979,11 @@ BOOL FileLoad(BOOL bDontSave, BOOL bNew, BOOL bReload, BOOL bNoEncDetect, LPCWST
 		}
 		iOriginalEncoding = iEncoding;
 		bModified = FALSE;
+		UpdateStatusBarCache(STATUS_CODEPAGE);
+		UpdateStatusBarCache(STATUS_EOLMODE);
 		if (!lexerSpecified) { // flagLexerSpecified will be cleared
 			np2LexLangIndex = 0;
 			Style_SetLexerFromFile(hwndEdit, szCurFile);
-		} else if (bReload) {
-			UpdateStatusBarWidth();
-			UpdateStatusbar();
 		}
 		if (!lexerSpecified) {
 			UpdateLineNumberWidth();
@@ -6958,7 +6994,6 @@ BOOL FileLoad(BOOL bDontSave, BOOL bNew, BOOL bReload, BOOL bNoEncDetect, LPCWST
 		if (flagUseSystemMRU == 2) {
 			SHAddToRecentDocs(SHARD_PATHW, szFileName);
 		}
-		UpdateWindowTitle();
 
 		// Install watching of the current file
 		if (!bReload && bResetFileWatching) {
@@ -6988,6 +7023,9 @@ BOOL FileLoad(BOOL bDontSave, BOOL bNew, BOOL bReload, BOOL bNoEncDetect, LPCWST
 			}
 		}
 #endif
+
+		UpdateStatusbar();
+		UpdateWindowTitle();
 		// Show warning: Unicode file loaded as ANSI
 		if (bUnicodeErr) {
 			MsgBox(MBWARN, IDS_ERR_UNICODE);
@@ -7655,16 +7693,11 @@ void GetRelaunchParameters(LPWSTR szParameters, LPCWSTR lpszFile, BOOL newWind, 
 			lstrcat(szParameters, L" -x");
 			break;
 
-		default: {
-			ZeroMemory(tch, sizeof(tch));
-			x = np2LexLangIndex;
-			np2LexLangIndex = 0;
-			LPCWSTR pszLexerName = Style_GetCurrentLexerName(tch, COUNTOF(tch));
-			np2LexLangIndex = x;
+		default:
 			lstrcat(szParameters, L" -s \"");
-			lstrcat(szParameters, pszLexerName);
+			lstrcat(szParameters, pLexCurrent->pszName);
 			lstrcat(szParameters, L"\"");
-		}
+			break;
 		}
 
 		// position

src/Styles.c
@@ -1019,7 +1019,6 @@ void Style_SetLexer(HWND hwnd, PEDITLEXER pLexNew) {
 	// Save current lexer
 	pLexCurrent = pLexNew;
 	UpdateStatusBarCache(STATUS_LEXER);
-	UpdateStatusBarWidth();
 	UpdateStatusbar();
 	UpdateLineNumberWidth();
 	UpdateFoldMarginWidth();
@@ -1152,7 +1151,7 @@ PEDITLEXER __fastcall Style_SniffShebang(char *pchText) {
 // find lexer from <!DOCTYPE type PUBLIC > or <%@ Page Language="lang" %>
 // Style_GetDocTypeLanguage()
 //
-int Style_GetDocTypeLanguage() {
+int Style_GetDocTypeLanguage(void) {
 	char *p = NULL, *pb = NULL;
 	char tchText[4096] = ""; // maybe contains header comments
 	SendMessage(hwndEdit, SCI_GETTEXT, (WPARAM)(COUNTOF(tchText) - 1), (LPARAM)tchText);
@@ -1428,10 +1427,7 @@ PEDITLEXER Style_DetectObjCAndMatlab(void) {
 //
 // Style_GetCurrentLexerName()
 //
-LPCWSTR Style_GetCurrentLexerName(LPWSTR lpszName, int cchName) {
-	if (GetString(pLexCurrent->rid, lpszName, cchName)) {
-		return lpszName;
-	}
+LPCWSTR Style_GetCurrentLexerName(void) {
 	if (np2LexLangIndex == 0) {
 		return pLexCurrent->pszName;
 	}
@@ -2065,7 +2061,7 @@ void Style_ToggleUse2ndDefault(HWND hwnd) {
 //
 // Style_GetUse2ndDefault()
 //
-BOOL Style_GetUse2ndDefault() {
+BOOL Style_GetUse2ndDefault(void) {
 	return bUse2ndDefaultStyle;
 }
 

src/Styles.h
@@ -50,10 +50,10 @@ void	Style_SetLexerFromName(HWND hwnd, LPCWSTR lpszFile, LPCWSTR lpszName);
 void	Style_SetLexerFromID(HWND hwnd, int id);
 int		Style_GetEditLexerId(int lexer);
 
-int		Style_GetDocTypeLanguage();
+int		Style_GetDocTypeLanguage(void);
 void	Style_UpdateLexerKeywords(PEDITLEXER pLexNew);
 void	Style_UpdateLexerKeywordAttr(PEDITLEXER pLexNew);
-LPCWSTR Style_GetCurrentLexerName(LPWSTR lpszName, int cchName);
+LPCWSTR Style_GetCurrentLexerName(void);
 void	Style_SetLexerByLangIndex(HWND hwnd, int lang);
 void	Style_UpdateSchemeMenu(HMENU hmenu);
 
@@ -63,7 +63,7 @@ void	Style_UpdateCaret(HWND hwnd);
 void	Style_SetLongLineColors(HWND hwnd);
 void	Style_SetCurrentLineBackground(HWND hwnd);
 void	Style_ToggleUse2ndDefault(HWND hwnd);
-BOOL	Style_GetUse2ndDefault();
+BOOL	Style_GetUse2ndDefault(void);
 BOOL	Style_GetOpenDlgFilterStr(LPWSTR lpszFilter, int cchFilter);
 
 BOOL	Style_StrGetFont(LPCWSTR lpszStyle, LPWSTR lpszFont, int cchFont);

commit 2f646ebbf3f98e0510b719179a1828a04ae7b593

Fix localization broken for lexer display name, introduced by 8bfe3ec2ea6c2f0a8b1ffea616ed7b225a134e19.

src/EditLexer.h
@@ -13,6 +13,11 @@
 #define MAX_EDITSTYLE_VALUE_SIZE	128
 #define MAX_EDITLEXER_EXT_SIZE		128
 
+// localization, get lexer and style name from resuorce
+#define NP2_GET_LEXER_STYLE_NAME_FROM_RES	0
+#define MAX_EDITLEXER_NAME_SIZE		128
+#define MAX_EDITSTYLE_NAME_SIZE		128
+
 typedef struct _editstyle {
 	union {
 		const INT32 iStyle;

src/Notepad2.c
@@ -304,7 +304,10 @@ WCHAR	wchWndClass[16] = WC_NOTEPAD2;
 
 // rarely changed statusbar items
 struct CachedStatusItem {
-	WCHAR tchZoom[16];
+#if NP2_GET_LEXER_STYLE_NAME_FROM_RES
+	WCHAR tchLexerName[MAX_EDITLEXER_NAME_SIZE];
+#endif
+	WCHAR tchZoom[8];
 
 	LPCWSTR pszLexerName;
 	LPCWSTR pszEOLMode;
@@ -2027,7 +2030,11 @@ void MsgSize(HWND hwnd, WPARAM wParam, LPARAM lParam) {
 void UpdateStatusBarCache(int item) {
 	switch (item) {
 	case STATUS_LEXER:
+#if NP2_GET_LEXER_STYLE_NAME_FROM_RES
+		cachedStatusItem.pszLexerName = Style_GetCurrentLexerDisplayName(cachedStatusItem.tchLexerName, MAX_EDITLEXER_NAME_SIZE);
+#else
 		cachedStatusItem.pszLexerName = Style_GetCurrentLexerName();
+#endif
 		cachedStatusItem.lexerNameChanged = TRUE;
 		UpdateStatusBarWidth();
 		break;

src/Styles.c
@@ -1451,6 +1451,15 @@ PEDITLEXER Style_DetectObjCAndMatlab(void) {
 //
 // Style_GetCurrentLexerName()
 //
+#if NP2_GET_LEXER_STYLE_NAME_FROM_RES
+LPCWSTR Style_GetCurrentLexerDisplayName(LPWSTR lpszName, int cchName) {
+	if (GetString(pLexCurrent->rid, lpszName, cchName)) {
+		return lpszName;
+	}
+	return Style_GetCurrentLexerName();
+}
+#endif
+
 LPCWSTR Style_GetCurrentLexerName(void) {
 	if (np2LexLangIndex == 0) {
 		return pLexCurrent->pszName;
@@ -2719,8 +2728,9 @@ int Style_GetLexerIconId(PEDITLEXER pLex) {
 //
 HTREEITEM Style_AddLexerToTreeView(HWND hwnd, PEDITLEXER pLex) {
 	int i = 0;
+#if NP2_GET_LEXER_STYLE_NAME_FROM_RES
 	WCHAR tch[128];
-
+#endif
 	HTREEITEM hTreeNode;
 
 	TVINSERTSTRUCT tvis;
@@ -2729,11 +2739,15 @@ HTREEITEM Style_AddLexerToTreeView(HWND hwnd, PEDITLEXER pLex) {
 	tvis.hInsertAfter = TVI_LAST;
 
 	tvis.item.mask = TVIF_TEXT | TVIF_IMAGE | TVIF_SELECTEDIMAGE | TVIF_PARAM;
+#if NP2_GET_LEXER_STYLE_NAME_FROM_RES
 	if (GetString(pLex->rid, tch, COUNTOF(tch))) {
 		tvis.item.pszText = tch;
 	} else {
 		tvis.item.pszText = (WCHAR *)pLex->pszName;
 	}
+#else
+	tvis.item.pszText = (WCHAR *)pLex->pszName;
+#endif
 	tvis.item.iImage = Style_GetLexerIconId(pLex);
 	tvis.item.iSelectedImage = tvis.item.iImage;
 	tvis.item.lParam = (LPARAM)pLex;
@@ -2747,11 +2761,15 @@ HTREEITEM Style_AddLexerToTreeView(HWND hwnd, PEDITLEXER pLex) {
 	//tvis.item.iSelectedImage = -1;
 
 	while (pLex->Styles[i].iStyle != -1) {
+#if NP2_GET_LEXER_STYLE_NAME_FROM_RES
 		if (GetString(pLex->Styles[i].rid, tch, COUNTOF(tch))) {
 			tvis.item.pszText = tch;
 		} else {
 			tvis.item.pszText = (WCHAR *)pLex->Styles[i].pszName;
 		}
+#else
+		tvis.item.pszText = (WCHAR *)pLex->Styles[i].pszName;
+#endif
 		tvis.item.lParam = (LPARAM)(&pLex->Styles[i]);
 		TreeView_InsertItem(hwnd, &tvis);
 		i++;
@@ -2765,17 +2783,23 @@ HTREEITEM Style_AddLexerToTreeView(HWND hwnd, PEDITLEXER pLex) {
 // Style_AddLexerToListView()
 //
 void Style_AddLexerToListView(HWND hwnd, PEDITLEXER pLex) {
-	WCHAR tch[128];
+#if NP2_GET_LEXER_STYLE_NAME_FROM_RES
+	WCHAR tch[MAX_EDITLEXER_NAME_SIZE];
+#endif
 	LVITEM lvi;
 	ZeroMemory(&lvi, sizeof(LVITEM));
 
 	lvi.mask = LVIF_IMAGE | LVIF_PARAM | LVIF_TEXT;
 	lvi.iItem = ListView_GetItemCount(hwnd);
+#if NP2_GET_LEXER_STYLE_NAME_FROM_RES
 	if (GetString(pLex->rid, tch, COUNTOF(tch))) {
 		lvi.pszText = tch;
 	} else {
 		lvi.pszText = (WCHAR *)pLex->pszName;
 	}
+#else
+	lvi.pszText = (WCHAR *)pLex->pszName;
+#endif
 	lvi.iImage = Style_GetLexerIconId(pLex);
 	lvi.lParam = (LPARAM)pLex;
 

src/Styles.h
@@ -54,6 +54,9 @@ int		Style_GetEditLexerId(int lexer);
 int		Style_GetDocTypeLanguage(void);
 void	Style_UpdateLexerKeywords(PEDITLEXER pLexNew);
 void	Style_UpdateLexerKeywordAttr(PEDITLEXER pLexNew);
+#if NP2_GET_LEXER_STYLE_NAME_FROM_RES
+LPCWSTR Style_GetCurrentLexerDisplayName(LPWSTR lpszName, int cchName);
+#endif
 LPCWSTR Style_GetCurrentLexerName(void);
 void	Style_SetLexerByLangIndex(HWND hwnd, int lang);
 void	Style_UpdateSchemeMenu(HMENU hmenu);