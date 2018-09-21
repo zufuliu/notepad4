c541817569680a1d15a4f2e73475a1a26cb16322
zufuliu <zufuliu@gmail.com>
2018-09-21 21:21:47 +0800

    Fix Clang warnings when CSTRLEN() is used in some macro, like EDITLEXER_HOLE().

--- metapath/src/Helpers.h
+++ metapath/src/Helpers.h
@@ -31,7 +31,12 @@
 // trigger error for pointer: GCC: void value not ignored as it ought to be. Clang: invalid operands to binary expression.
 #define COUNTOF(ar)	_Generic(&(ar), __typeof__((ar)[0]) **: (void)0, default: _countof(ar))
 // trigger warning for non-literal string: GCC: division by zero [-Wdiv-by-zero]. Clang: division by zero is undefined [-Wdivision-by-zero].
+#if defined(__GNUC__)
 #define CSTRLEN(s)	(__builtin_constant_p(s) ? (_countof(s) - 1) : (1 / 0))
+#else
+// Clang emit warnings when CSTRLEN() is used in some macro, like EDITLEXER_HOLE()
+#define CSTRLEN(s)	(COUNTOF(s) - 1)
+#endif
 #else
 // C++ template based version of _countof(), or plain old unsafe version
 #define COUNTOF(ar)	_countof(ar)
