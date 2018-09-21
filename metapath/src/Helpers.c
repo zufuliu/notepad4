dc9a682dbb22007b1537105f8f10b0aad97adda1
zufuliu <zufuliu@gmail.com>
2018-09-21 13:47:22 +0800

    Disable auto-completion after single format specifier (printf, scanf, etc.).

--- metapath/src/Helpers.c
+++ metapath/src/Helpers.c
@@ -553,7 +553,7 @@ int Toolbar_SetButtons(HWND hwnd, LPCWSTR lpszButtons, LPCTBBUTTON ptbb, int ctb
 	}
 
 	LPCWSTR p = lpszButtons;
-	ctbb--;
+	--ctbb;
 	while (TRUE) {
 		LPWSTR end;
 		int iCmd = (int)wcstol(p, &end, 10);
@@ -562,6 +562,9 @@ int Toolbar_SetButtons(HWND hwnd, LPCWSTR lpszButtons, LPCTBBUTTON ptbb, int ctb
 			SendMessage(hwnd, TB_ADDBUTTONS, (WPARAM)1, (LPARAM)&ptbb[iCmd]);
 			p = end;
 			++count;
+			//if (count == MAX_TOOLBAR_ITEM_COUNT_WITH_SEPARATOR) {
+			//	break;
+			//}
 		} else {
 			break;
 		}
