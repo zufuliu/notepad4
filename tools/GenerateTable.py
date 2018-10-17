5fa4914e59949f91e57422a921722d5c923b5e80
zufuliu <zufuliu@gmail.com>
2018-10-17 20:07:53 +0800

    Simplify EOL table in previous commit.

--- tools/GenerateTable.py
+++ tools/GenerateTable.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
 
 def GenerateEOLTable():
-	table = [0] * 256
+	table = [0] * 16
 	table[0] = 3
 	table[ord('\n')] = 1
 	table[ord('\r')] = 2
-	lines = []
-	for i in range(0, len(table), 32):
-		line = ', '.join(str(c) for c in table[i:i + 32])
-		line = line + ', // %02X - %02X' % (i, i + 31)
-		lines.append(line)
-	
-	print('EOLTable:')
-	print('\n'.join(lines))
+	line = ', '.join(str(c) for c in table)
+	line = line + ', // %02X - %02X' % (0, 15)
+	print('EOLTable:', line)
 
 if __name__ == '__main__':
 	GenerateEOLTable()
