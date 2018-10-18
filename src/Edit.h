9ab912b5cdc1a3cfda9f61c9f46b6b0afa81dc45
zufuliu <zufuliu@gmail.com>
2018-10-18 08:43:34 +0800

    Switch to the new EOL detection method, and scan whole file to detect EOL mode.

--- src/Edit.h
+++ src/Edit.h
@@ -69,7 +69,7 @@ BOOL	EditSetNewEncoding(HWND hwnd, int iCurrentEncoding,
 
 char	*EditGetClipboardText(HWND hwnd); // LocalFree()
 BOOL	EditCopyAppend(HWND hwnd);
-int 	EditDetectEOLMode(LPSTR lpData, DWORD cbData);
+int 	EditDetectEOLMode(LPCSTR lpData, DWORD cbData);
 BOOL	EditLoadFile(HWND hwnd, LPWSTR pszFile, BOOL bSkipEncodingDetection,
 					 int *iEncoding, int *iEOLMode, BOOL *pbUnicodeErr, BOOL *pbFileTooBig);
 BOOL	EditSaveFile(HWND hwnd, LPCWSTR pszFile, int iEncoding, BOOL *pbCancelDataLoss, BOOL bSaveCopy);
