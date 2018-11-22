9cd1880cd3ba0ba67928f9331a415a21bff2c2f2
zufuliu <zufuliu@gmail.com>
2018-11-22 23:40:49 +0800

    Rename CurrentPPI to DefaultDPI as it refer to the default DPI after booting or login.

--- src/Styles.c
+++ src/Styles.c
@@ -2662,7 +2662,7 @@ void Style_SetStyles(HWND hwnd, int iStyle, LPCWSTR lpszStyle) {
 
 	// Size
 	if (Style_StrGetSizeEx(lpszStyle, &iValue)) {
-		iValue = SystemToCurrentDPI(iValue);
+		iValue = DefaultToCurrentDPI(iValue);
 		SendMessage(hwnd, SCI_STYLESETSIZEFRACTIONAL, iStyle, iValue);
 	}
 
@@ -2710,7 +2710,7 @@ static void Style_Parse(struct DetailStyle *style, LPCWSTR lpszStyle) {
 
 	// Size
 	if (Style_StrGetSizeEx(lpszStyle, &iValue)) {
-		style->fontSize = SystemToCurrentDPI(iValue);
+		style->fontSize = DefaultToCurrentDPI(iValue);
 		mask |= STYLE_MASK_FONT_SIZE;
 	}
 
