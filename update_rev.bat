2bab7d5b462ab5d6962be01f3dad90444ebe65b5
zufuliu <zufuliu@gmail.com>
2018-12-27 12:01:51 +0800

    Add update_rev.bat (from Notepad2-Mod) to update build reversion.

---
+++ update_rev.bat
@@ -0,0 +1,32 @@
+@ECHO OFF
+SETLOCAL
+
+PUSHD %~dp0
+
+IF EXIST "build.user.bat" (CALL "build.user.bat")
+
+SET PATH=%MSYS%\bin;%PATH%
+
+FOR %%G IN (bash.exe) DO (SET FOUND=%%~$PATH:G)
+IF NOT DEFINED FOUND GOTO MissingVar
+
+bash.exe ./version.sh 1
+
+
+:END
+POPD
+ENDLOCAL
+EXIT /B
+
+
+:MissingVar
+COLOR 0C
+TITLE ERROR
+ECHO MSYS or MSYS2 (bash.exe) wasn't found. Create a file build.user.bat and set the variable there.
+ECHO.
+ECHO SET "MSYS=C:\MSYS64"
+ECHO. & ECHO.
+ECHO Press any key to exit...
+PAUSE >NUL
+ENDLOCAL
+EXIT /B
