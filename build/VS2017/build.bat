c115f525461c2d361338f3a60a83a2b35c5b2ece
zufuliu <zufuliu@gmail.com>
2018-12-28 01:55:40 +0800

    Avoid blocking on script failure.

--- build/VS2017/build.bat
+++ build/VS2017/build.bat
@@ -16,6 +16,8 @@
 SETLOCAL ENABLEEXTENSIONS
 CD /D %~dp0
 
+SET "EXIST_ON_ERROR=%~4"
+
 @rem Check for the help switches
 IF /I "%~1" == "help"   GOTO SHOWHELP
 IF /I "%~1" == "/help"  GOTO SHOWHELP
@@ -198,7 +200,7 @@ ECHO. & ECHO ______________________________
 ECHO [%~1] %~2
 ECHO ______________________________ & ECHO.
 IF /I "%~1" == "ERROR" (
-  IF "%~4" == "" PAUSE
+  IF "%EXIST_ON_ERROR%" == "" PAUSE
   EXIT /B
 ) ELSE (
   EXIT /B
