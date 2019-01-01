780a8c673408b691d17e8a4590a66bba8fd1eb3e
zufuliu <zufuliu@gmail.com>
2019-01-01 09:50:25 +0800

    Fix make_zip.bat not works for Tab-indented Version.h, revert commit 49e211e080c8bc96451663366bee0595fb5bcecd.

--- metapath/src/version.h
+++ metapath/src/version.h
@@ -26,8 +26,7 @@
 #define DO_STRINGIFY(x)		TEXT(#x)
 #define STRINGIFY(x)		DO_STRINGIFY(x)
 
-//! indent with space to ensure make_zip.bat works
-#define VERSION_MAJOR 4
+#define VERSION_MAJOR				4
 
 #define MY_APPNAME					L"metapath"
 #define VERSION_FILEVERSION_NUM		VERSION_MAJOR,VERSION_MINOR,VERSION_BUILD,VERSION_REV
