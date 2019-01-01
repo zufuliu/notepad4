44953d794517e9f7f6d40f0687487aecdde9a80b
zufuliu <zufuliu@gmail.com>
2019-01-01 11:04:03 +0800

    Use different color for double quoted string, single quoted string and backticks, since them have different meanings.

--- src/EditLexers/stlBatch.c
+++ src/EditLexers/stlBatch.c
@@ -2,6 +2,7 @@
 #include "EditStyle.h"
 
 // https://en.wikibooks.org/wiki/Windows_Batch_Scripting
+// https://www.robvanderwoude.com/battech.php
 
 static KEYWORDLIST Keywords_Batch = {{
 "defined do not else for goto if in "
@@ -47,7 +48,9 @@ static EDITSTYLE Styles_Batch[] = {
 	{ SCE_BAT_COMMENT, NP2STYLE_Comment, EDITSTYLE_HOLE(L"Comment"), L"fore:#008000" },
 	{ SCE_BAT_WORD, NP2STYLE_Keyword, EDITSTYLE_HOLE(L"Keyword"), L"bold; fore:#FF8000" },
 	{ SCE_BAT_COMMAND, NP2STYLE_Command, EDITSTYLE_HOLE(L"Command"), L"bold; fore:#0080FF" },
-	{ MULTI_STYLE(SCE_BAT_STRINGDQ, SCE_BAT_STRINGSQ, SCE_BAT_STRINGBT, 0), NP2STYLE_String, EDITSTYLE_HOLE(L"String"), L"fore:#008080" },
+	{ SCE_BAT_STRINGDQ, 63413, EDITSTYLE_HOLE(L"Double Quoted String"), L"fore:#008080" },
+	{ SCE_BAT_STRINGSQ, 63412, EDITSTYLE_HOLE(L"Single Quoted String"), L"fore:#C08000" },
+	{ SCE_BAT_STRINGBT, NP2STYLE_Backticks, EDITSTYLE_HOLE(L"Backticks"), L"fore:#F08000" },
 	{ SCE_BAT_VARIABLE, NP2STYLE_Variable, EDITSTYLE_HOLE(L"Variable"), L"fore:#003CE6; back:#FFF1A8" },
 	{ SCE_BAT_OPERATOR, NP2STYLE_Operator, EDITSTYLE_HOLE(L"Operator"), L"fore:#B000B0" },
 	{ SCE_BAT_LABEL, NP2STYLE_Label, EDITSTYLE_HOLE(L"Label"), L"fore:#C80000; back:#F4F4F4" },
