zufuliu/notepad4

commit 9d38515ce7b2c6066d1a0a2e10cc0b5d385816f3

Optimize DBCS character testing.

scintilla/win32/PlatWin.cxx
@@ -43,7 +43,7 @@
 #include "Platform.h"
 #include "XPM.h"
 #include "UniConversion.h"
-#include "DBCS.h"
+#include "CharClassify.h"
 #include "FontQuality.h"
 
 #include "PlatWin.h"
@@ -2115,12 +2115,12 @@ void SurfaceD2D::MeasureWidths(const Font &font_, std::string_view text, XYPOSIT
 		}
 
 	} else {
-
+		const DBCSCharClassify *dbcs = DBCSCharClassify::Get(codePageText);
 		// May be one or two bytes per position
 		int ui = 0;
 		for (size_t i = 0; i < text.length() && ui < tbuf.tlen;) {
 			positions[i] = poses.buffer[ui];
-			if (DBCSIsLeadByte(codePageText, text[i])) {
+			if (dbcs->IsLeadByte(text[i])) {
 				positions[i + 1] = poses.buffer[ui];
 				i += 2;
 			} else {

build/CodeLite/Notepad2.project
@@ -90,8 +90,6 @@
       <File Name="../../scintilla/src/CharClassify.h"/>
       <File Name="../../scintilla/src/ContractionState.cxx"/>
       <File Name="../../scintilla/src/ContractionState.h"/>
-      <File Name="../../scintilla/src/DBCS.cxx"/>
-      <File Name="../../scintilla/src/DBCS.h"/>
       <File Name="../../scintilla/src/Decoration.cxx"/>
       <File Name="../../scintilla/src/Decoration.h"/>
       <File Name="../../scintilla/src/Document.cxx"/>

build/VS2017/Notepad2.vcxproj
@@ -614,7 +614,6 @@
     <ClCompile Include="..\..\scintilla\src\CellBuffer.cxx" />
     <ClCompile Include="..\..\scintilla\src\CharClassify.cxx" />
     <ClCompile Include="..\..\scintilla\src\ContractionState.cxx" />
-    <ClCompile Include="..\..\scintilla\src\DBCS.cxx" />
     <ClCompile Include="..\..\scintilla\src\Decoration.cxx" />
     <ClCompile Include="..\..\scintilla\src\Document.cxx" />
     <ClCompile Include="..\..\scintilla\src\EditModel.cxx" />
@@ -754,7 +753,6 @@
     <ClInclude Include="..\..\scintilla\src\CellBuffer.h" />
     <ClInclude Include="..\..\scintilla\src\CharClassify.h" />
     <ClInclude Include="..\..\scintilla\src\ContractionState.h" />
-    <ClInclude Include="..\..\scintilla\src\DBCS.h" />
     <ClInclude Include="..\..\scintilla\src\Decoration.h" />
     <ClInclude Include="..\..\scintilla\src\Document.h" />
     <ClInclude Include="..\..\scintilla\src\EditModel.h" />

build/VS2017/Notepad2.vcxproj.filters
@@ -453,9 +453,6 @@
     <ClCompile Include="..\..\scintilla\win32\HanjaDic.cxx">
       <Filter>Scintilla\win32</Filter>
     </ClCompile>
-    <ClCompile Include="..\..\scintilla\src\DBCS.cxx">
-      <Filter>Scintilla\src</Filter>
-    </ClCompile>
   </ItemGroup>
   <ItemGroup>
     <ClInclude Include="..\..\scintilla\include\ILexer.h">
@@ -654,9 +651,6 @@
       <Filter>Scintilla\src</Filter>
     </ClInclude>
     <ClInclude Include="..\..\scintilla\src\UniqueString.h">
-      <Filter>Scintilla\src</Filter>
-    </ClInclude>
-    <ClInclude Include="..\..\scintilla\src\DBCS.h">
       <Filter>Scintilla\src</Filter>
     </ClInclude>
   </ItemGroup>

scintilla/src/CharClassify.cxx
@@ -9,6 +9,9 @@
 #include <cassert>
 #include <cstring>
 
+#include <vector>
+#include <memory>
+
 #include "CharacterSet.h"
 #include "CharClassify.h"
 
@@ -102,3 +105,125 @@ CharClassify::cc CharClassify::ClassifyCharacter(unsigned int ch) noexcept {
 	return static_cast<cc>(CharClassifyTable[7744 + ch]);
 }
 //--Autogenerated -- end of section automatically generated
+
+
+namespace {
+
+inline void SetRange(bool bs[256], int low, int high) noexcept {
+	while (low <= high) {
+		bs[low] = true;
+		++low;
+	}
+}
+
+constexpr bool IsDBCSCodePage(int codePage) noexcept {
+	return codePage == 932
+		|| codePage == 936
+		|| codePage == 949
+		|| codePage == 950
+		|| codePage == 1361;
+}
+
+std::vector<std::unique_ptr<DBCSCharClassify>> dbcsCharClassifyList;
+
+}
+
+DBCSCharClassify::DBCSCharClassify(int codePage_) noexcept:
+	codePage(codePage_),
+	leadByte{},
+	invalidLeadByte{},
+	invalidTrailByte{} {
+	// Byte ranges found in Wikipedia articles with relevant search strings in each case
+	switch (codePage_) {
+	case 932:
+		// Shift_JIS
+		SetRange(leadByte, 0x81, 0x9F);
+		SetRange(leadByte, 0xE0, 0xFC);
+		// Lead bytes F0 to FC may be a Microsoft addition.
+
+		invalidLeadByte[0x85] = true;
+		invalidLeadByte[0x86] = true;
+		invalidLeadByte[0xEB] = true;
+		invalidLeadByte[0xEC] = true;
+		invalidLeadByte[0xEF] = true;
+		invalidLeadByte[0xFA] = true;
+		invalidLeadByte[0xFB] = true;
+		invalidLeadByte[0xFC] = true;
+
+		SetRange(invalidTrailByte, 0x00, 0x3F);
+		invalidTrailByte[0x7F] = true;
+		SetRange(invalidTrailByte, 0xFD, 0xFF);
+		break;
+
+	case 936:
+		// GBK
+		SetRange(leadByte, 0x81, 0xFE);
+
+		invalidLeadByte[0x80] = true;
+		invalidLeadByte[0xFF] = true;
+
+		SetRange(invalidTrailByte, 0x00, 0x3F);
+		invalidTrailByte[0x7F] = true;
+		invalidTrailByte[0xFF] = true;
+		break;
+
+	case 949:
+		// Korean Unified Hangul Code, Wansung KS C-5601-1987
+		SetRange(leadByte, 0x81, 0xFE);
+
+		invalidLeadByte[0x80] = true;
+		invalidLeadByte[0xC9] = true;
+		invalidLeadByte[0xFE] = true;
+		invalidLeadByte[0xFF] = true;
+
+		SetRange(invalidTrailByte, 0x00, 0x40);
+		SetRange(invalidTrailByte, 0x5B, 0x60);
+		SetRange(invalidTrailByte, 0x7B, 0x80);
+		invalidTrailByte[0xFF] = true;
+		break;
+
+	case 950:
+		// Big5
+		SetRange(leadByte, 0x81, 0xFE);
+
+		SetRange(invalidLeadByte, 0x80, 0xA0);
+		invalidLeadByte[0xC8] = true;
+		SetRange(invalidLeadByte, 0xFA, 0xFF);
+
+		SetRange(invalidTrailByte, 0x00, 0x3F);
+		SetRange(invalidTrailByte, 0x7F, 0xA0);
+		invalidTrailByte[0xFF] = true;
+		break;
+
+	case 1361:
+		// Korean Johab, KS C-5601-1992
+		SetRange(leadByte, 0x84, 0xD3);
+		SetRange(leadByte, 0xD8, 0xDE);
+		SetRange(leadByte, 0xE0, 0xF9);
+
+		SetRange(invalidLeadByte, 0x80, 0x83);
+		SetRange(invalidLeadByte, 0xD4, 0xD8);
+		invalidLeadByte[0xDF] = true;
+		SetRange(invalidLeadByte, 0xFA, 0xFF);
+
+		SetRange(invalidTrailByte, 0x00, 0x30);
+		invalidTrailByte[0xFF] = true;
+		invalidTrailByte[0x80] = true;
+		invalidTrailByte[0xFF] = true;
+		break;
+	}
+}
+
+const DBCSCharClassify* DBCSCharClassify::Get(int codePage) {
+	if (IsDBCSCodePage(codePage)) {
+		for (const auto &dbcs : dbcsCharClassifyList) {
+			if (dbcs->codePage == codePage) {
+				return dbcs.get();
+			}
+		}
+
+		dbcsCharClassifyList.push_back(std::unique_ptr<DBCSCharClassify>(new DBCSCharClassify(codePage)));
+		return dbcsCharClassifyList.back().get();
+	}
+	return nullptr;
+}

scintilla/src/CharClassify.h
@@ -37,6 +37,29 @@ private:
 	unsigned char charClass[maxChar];    // not type cc to save space
 };
 
+class DBCSCharClassify {
+public:
+	static const DBCSCharClassify* Get(int codePage);
+
+	bool IsLeadByte(char ch) const noexcept {
+		return leadByte[static_cast<unsigned char>(ch)];
+	}
+	bool IsLeadByteInvalid(char ch) const noexcept {
+		return invalidLeadByte[static_cast<unsigned char>(ch)];
+	}
+	bool IsTrailByteInvalid(char ch) const noexcept {
+		return invalidTrailByte[static_cast<unsigned char>(ch)];
+	}
+
+private:
+	DBCSCharClassify(int codePage_) noexcept;
+
+	int codePage;
+	bool leadByte[256];
+	bool invalidLeadByte[256];
+	bool invalidTrailByte[256];
+};
+
 }
 
 #endif

scintilla/src/DBCS.cxx
@@ -1,118 +1,0 @@
-// Scintilla source code edit control
-/** @file DBCS.cxx
- ** Functions to handle DBCS double byte encodings like Shift-JIS.
- **/
-// Copyright 2017 by Neil Hodgson <neilh@scintilla.org>
-// The License.txt file describes the conditions under which this software may be distributed.
-
-#include "DBCS.h"
-
-using namespace Scintilla;
-
-namespace Scintilla {
-
-bool DBCSIsLeadByte(int codePage, char ch) noexcept {
-	// Byte ranges found in Wikipedia articles with relevant search strings in each case
-	const unsigned char uch = ch;
-	switch (codePage) {
-	case 932:
-		// Shift_jis
-		return ((uch >= 0x81) && (uch <= 0x9F)) ||
-			((uch >= 0xE0) && (uch <= 0xFC));
-		// Lead bytes F0 to FC may be a Microsoft addition.
-	case 936:
-		// GBK
-		return (uch >= 0x81) && (uch <= 0xFE);
-	case 949:
-		// Korean Wansung KS C-5601-1987
-		return (uch >= 0x81) && (uch <= 0xFE);
-	case 950:
-		// Big5
-		return (uch >= 0x81) && (uch <= 0xFE);
-	case 1361:
-		// Korean Johab KS C-5601-1992
-		return
-			((uch >= 0x84) && (uch <= 0xD3)) ||
-			((uch >= 0xD8) && (uch <= 0xDE)) ||
-			((uch >= 0xE0) && (uch <= 0xF9));
-	}
-	return false;
-}
-
-bool DBCSIsLeadByteInvalid(int codePage, char ch) noexcept {
-	const unsigned char lead = ch;
-	switch (codePage) {
-	case 932:
-		// Shift_jis
-		return
-			(lead == 0x85) ||
-			(lead == 0x86) ||
-			(lead == 0xEB) ||
-			(lead == 0xEC) ||
-			(lead == 0xEF) ||
-			(lead == 0xFA) ||
-			(lead == 0xFB) ||
-			(lead == 0xFC);
-	case 936:
-		// GBK
-		return (lead == 0x80) || (lead == 0xFF);
-	case 949:
-		// Korean Wansung KS C-5601-1987
-		return (lead == 0x80) || (lead == 0xC9) || (lead >= 0xFE);
-	case 950:
-		// Big5
-		return
-			((lead >= 0x80) && (lead <= 0xA0)) ||
-			(lead == 0xC8) ||
-			(lead >= 0xFA);
-	case 1361:
-		// Korean Johab KS C-5601-1992
-		return
-			((lead >= 0x80) && (lead <= 0x83)) ||
-			((lead >= 0xD4) && (lead <= 0xD8)) ||
-			(lead == 0xDF) ||
-			(lead >= 0xFA);
-	}
-	return false;
-}
-
-bool DBCSIsTrailByteInvalid(int codePage, char ch) noexcept {
-	const unsigned char trail = ch;
-	switch (codePage) {
-	case 932:
-		// Shift_jis
-		return
-			(trail <= 0x3F) ||
-			(trail == 0x7F) ||
-			(trail >= 0xFD);
-	case 936:
-		// GBK
-		return
-			(trail <= 0x3F) ||
-			(trail == 0x7F) ||
-			(trail == 0xFF);
-	case 949:
-		// Korean Wansung KS C-5601-1987
-		return
-			(trail <= 0x40) ||
-			((trail >= 0x5B) && (trail <= 0x60)) ||
-			((trail >= 0x7B) && (trail <= 0x80)) ||
-			(trail == 0xFF);
-	case 950:
-		// Big5
-		return
-			(trail <= 0x3F) ||
-			((trail >= 0x7F) && (trail <= 0xA0)) ||
-			(trail == 0xFF);
-	case 1361:
-		// Korean Johab KS C-5601-1992
-		return
-			(trail <= 0x30) ||
-			(trail == 0x7F) ||
-			(trail == 0x80) ||
-			(trail == 0xFF);
-	}
-	return false;
-}
-
-}

scintilla/src/DBCS.h
@@ -1,19 +1,0 @@
-// Scintilla source code edit control
-/** @file DBCS.h
- ** Functions to handle DBCS double byte encodings like Shift-JIS.
- **/
-// Copyright 2017 by Neil Hodgson <neilh@scintilla.org>
-// The License.txt file describes the conditions under which this software may be distributed.
-
-#ifndef DBCS_H
-#define DBCS_H
-
-namespace Scintilla {
-
-bool DBCSIsLeadByte(int codePage, char ch) noexcept;
-bool DBCSIsLeadByteInvalid(int codePage, char ch) noexcept;
-bool DBCSIsTrailByteInvalid(int codePage, char ch) noexcept;
-
-}
-
-#endif

commit 44d31dff9e8a60c54842b48aa2693e4c4051b3f3

Fix crash when opening file with ANSI encoding on some systems (issue #97, introduced by 9d38515ce7b2c6066d1a0a2e10cc0b5d385816f3).

scintilla/win32/PlatWin.cxx
@@ -2106,28 +2106,28 @@ void SurfaceD2D::MeasureWidths(const Font &font_, std::string_view text, XYPOSIT
 		while (i < text.length()) {
 			positions[i++] = lastPos;
 		}
-	} else if (codePageText == 0) {
-
-		// One char per position
-		PLATFORM_ASSERT(text.length() == static_cast<size_t>(tbuf.tlen));
-		for (int kk = 0; kk < tbuf.tlen; kk++) {
-			positions[kk] = poses.buffer[kk];
-		}
-
 	} else {
 		const DBCSCharClassify *dbcs = DBCSCharClassify::Get(codePageText);
-		// May be one or two bytes per position
-		int ui = 0;
-		for (size_t i = 0; i < text.length() && ui < tbuf.tlen;) {
-			positions[i] = poses.buffer[ui];
-			if (dbcs->IsLeadByte(text[i])) {
-				positions[i + 1] = poses.buffer[ui];
-				i += 2;
-			} else {
-				i++;
-			}
+		if (dbcs) {
+			// May be one or two bytes per position
+			int ui = 0;
+			for (size_t i = 0; i < text.length() && ui < tbuf.tlen;) {
+				positions[i] = poses.buffer[ui];
+				if (dbcs->IsLeadByte(text[i])) {
+					positions[i + 1] = poses.buffer[ui];
+					i += 2;
+				} else {
+					i++;
+				}
 
-			ui++;
+				ui++;
+			}
+		} else {
+			// One char per position
+			PLATFORM_ASSERT(text.length() == static_cast<size_t>(tbuf.tlen));
+			for (int kk = 0; kk < tbuf.tlen; kk++) {
+				positions[kk] = poses.buffer[kk];
+			}
 		}
 	}
 }