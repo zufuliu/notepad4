6bddea5afc5d7596be0e985c1564dd7718c2ceab
zufuliu <zufuliu@gmail.com>
2019-03-22 08:35:22 +0800

    Support JSON5 (https://json5.org/) single quoted string and multi-line string.

--- src/EditLexers/stlJSON.c
+++ src/EditLexers/stlJSON.c
@@ -2,6 +2,8 @@
 #include "EditStyle.h"
 
 // https://www.json.org
+// https://json5.org/
+// https://spec.json5.org/
 
 static KEYWORDLIST Keywords_JSON = {{
 "true false null"
@@ -14,7 +16,7 @@ static EDITSTYLE Styles_JSON[] = {
 	EDITSTYLE_DEFAULT,
 	{ SCE_C_WORD, NP2STYLE_Keyword, EDITSTYLE_HOLE(L"Keyword"), L"fore:#0000FF" },
 	{ SCE_C_LABEL, NP2STYLE_Property, EDITSTYLE_HOLE(L"Property"), L"fore:#A46000" },
-	{ SCE_C_STRING, NP2STYLE_String, EDITSTYLE_HOLE(L"String"), L"fore:#008000" },
+	{ MULTI_STYLE(SCE_C_STRING, SCE_C_CHARACTER, SCE_C_STRINGEOL, 0), NP2STYLE_String, EDITSTYLE_HOLE(L"String"), L"fore:#008000" },
 	{ SCE_C_NUMBER, NP2STYLE_Number, EDITSTYLE_HOLE(L"Number"), L"fore:#FF0000" },
 	{ SCE_C_OPERATOR, NP2STYLE_Operator, EDITSTYLE_HOLE(L"Operator"), L"fore:#B000B0" },
 	{ MULTI_STYLE(SCE_C_COMMENT, SCE_C_COMMENTLINE, 0, 0), NP2STYLE_Comment, EDITSTYLE_HOLE(L"Comment"), L"fore:#608060" },
