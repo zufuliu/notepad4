zufuliu/notepad4

commit f6d50806538f3206157b6c66c49de9cbcb36e8d5

Fix potential bug in style attribute parsing.

src/Styles.c
@@ -1085,12 +1085,29 @@ static inline void Style_SetDefaultStyle(int index) {
 	Style_SetStyles(pLexGlobal->Styles[index].iStyle, pLexGlobal->Styles[index].szValue);
 }
 
-static inline BOOL Style_StrGetAttributeEx(LPCWSTR lpszStyle, LPCWSTR key, int keyLen) {
+// parse a style attribute separated by ';'
+// e.g.: 'bold', 'bold;', '; bold' and '; bold;'
+static BOOL Style_StrGetAttributeEx(LPCWSTR lpszStyle, LPCWSTR key, int keyLen) {
 	LPCWSTR p = StrStr(lpszStyle, key);
-	if (p != NULL) {
-		const WCHAR ch1 = (p == lpszStyle) ? L' ' : p[-1];
-		const WCHAR ch2 = p[keyLen];
-		return (ch1 == L' ' || ch1 == L';') && (ch2 == L'\0' || ch2 == L' ' || ch2 == L';');
+	while (p != NULL) {
+		WCHAR chPrev = (p == lpszStyle) ? L';' : p[-1];
+		if (chPrev == L' ') {
+			LPCWSTR t = p - 2;
+			while (t > lpszStyle && *t == L' ') {
+				--t;
+			}
+			chPrev = (t <= lpszStyle) ? L';' : t[-1];
+		}
+		p += keyLen;
+		if (chPrev == L';') {
+			while (*p == L' ') {
+				++p;
+			}
+			if (*p == L'\0' || *p == L';') {
+				return TRUE;
+			}
+		}
+		p = StrStr(p, key);
 	}
 	return FALSE;
 }

commit 0b714c904a0dbd78d4b8270d6c16e32b3b1cd1f5

Fix bug in style parsing (introduced by commit f6d50806538f3206157b6c66c49de9cbcb36e8d5).

src/Styles.c
@@ -1103,7 +1103,7 @@ static BOOL Style_StrGetAttributeEx(LPCWSTR lpszStyle, LPCWSTR key, int keyLen) 
 			while (t > lpszStyle && *t == L' ') {
 				--t;
 			}
-			chPrev = (t <= lpszStyle) ? L';' : t[-1];
+			chPrev = (t <= lpszStyle) ? L';' : *t;
 		}
 		p += keyLen;
 		if (chPrev == L';') {