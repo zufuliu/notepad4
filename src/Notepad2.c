zufuliu/notepad4

commit 5b75e544d342b03e3dc6cc88f22aecdd816867c2

Change print format specifier for line and column to unsigned int to make supporting large file (2GiB to 4GiB) easy.

src/Notepad2.c
@@ -261,8 +261,8 @@ int		iEncoding;
 int		iDefaultCodePage;
 int		iDefaultCharSet;
 
-static int iInitialLine;
-static int iInitialColumn;
+static Sci_Line iInitialLine;
+static Sci_Position iInitialColumn;
 static int iInitialLexer;
 
 static BOOL bLastCopyFromMe = FALSE;
@@ -291,7 +291,7 @@ const int iLineEndings[3] = {
 
 static int iSortOptions = 0;
 static int iAlignMode	= 0;
-int		iMatchesCount	= 0;
+UINT	iMatchesCount	= 0;
 extern int iFontQuality;
 extern int iCaretStyle;
 extern int iOvrCaretStyle;
@@ -5820,8 +5820,8 @@ int ParseCommandLineOption(LPWSTR lp1, LPWSTR lp2, BOOL *bIsNotepadReplacement) 
 				if (itok != 0) {
 					flagJumpTo = 1;
 					state = 1;
-					iInitialLine = cord[0];
-					iInitialColumn = cord[1];
+					iInitialLine = (UINT)cord[0];
+					iInitialColumn = (UINT)cord[1];
 				}
 			}
 			break;
@@ -6711,28 +6711,28 @@ void UpdateStatusbar(void) {
 	const Sci_Position iPos =  SciCall_GetCurrentPos();
 
 	const Sci_Line iLn = SciCall_LineFromPosition(iPos) + 1;
-	wsprintf(tchLn, L"%i", (int)iLn);
+	wsprintf(tchLn, L"%u", (UINT)iLn);
 	FormatNumberStr(tchLn);
 
 	const Sci_Line iLines = SciCall_GetLineCount();
-	wsprintf(tchLines, L"%i", (int)iLines);
+	wsprintf(tchLines, L"%u", (UINT)iLines);
 	FormatNumberStr(tchLines);
 
 	Sci_Position iCol = SciCall_GetColumn(iPos) + 1;
-	wsprintf(tchCol, L"%i", (int)iCol);
+	wsprintf(tchCol, L"%u", (UINT)iCol);
 	FormatNumberStr(tchCol);
 
 	const Sci_Position iLineStart = SciCall_PositionFromLine(iLn - 1);
 	const Sci_Position iLineEnd = SciCall_GetLineEndPosition(iLn - 1);
 	iCol = SciCall_CountCharacters(iLineStart, iPos) + 1;
 	const Sci_Position iLineChar = SciCall_CountCharacters(iLineStart, iLineEnd);
-	wsprintf(tchCh, L"%i", (int)iCol);
-	wsprintf(tchChs, L"%i", (int)iLineChar);
+	wsprintf(tchCh, L"%u", (UINT)iCol);
+	wsprintf(tchChs, L"%u", (UINT)iLineChar);
 	FormatNumberStr(tchCh);
 	FormatNumberStr(tchChs);
 
 	iCol = SciCall_GetColumn(iLineEnd);
-	wsprintf(tchCols, L"%i", (int)iCol);
+	wsprintf(tchCols, L"%u", (UINT)iCol);
 	FormatNumberStr(tchCols);
 
 	const Sci_Position iSelStart = SciCall_GetSelectionStart();
@@ -6742,10 +6742,10 @@ void UpdateStatusbar(void) {
 		lstrcpy(tchSelCh, L"0");
 	} else if (!SciCall_IsRectangleSelection()) {
 		Sci_Position iSel = SciCall_GetSelTextLength() - 1;
-		wsprintf(tchSel, L"%i", (int)iSel);
+		wsprintf(tchSel, L"%u", (UINT)iSel);
 		FormatNumberStr(tchSel);
 		iSel = SciCall_CountCharacters(iSelStart, iSelEnd);
-		wsprintf(tchSelCh, L"%i", (int)iSel);
+		wsprintf(tchSelCh, L"%u", (UINT)iSel);
 		FormatNumberStr(tchSelCh);
 	} else {
 		lstrcpy(tchSel, L"--");
@@ -6764,16 +6764,16 @@ void UpdateStatusbar(void) {
 		if (iSelStart != iSelEnd && iStartOfLinePos != iSelEnd) {
 			iLinesSelected += 1;
 		}
-		wsprintf(tchLinesSelected, L"%i", (int)iLinesSelected);
+		wsprintf(tchLinesSelected, L"%u", (UINT)iLinesSelected);
 		FormatNumberStr(tchLinesSelected);
-		wsprintf(tchMatchesCount, L"%i", (int)iMatchesCount);
+		wsprintf(tchMatchesCount, L"%u", iMatchesCount);
 		FormatNumberStr(tchMatchesCount);
 	}
 
 	wsprintf(tchDocPos, cachedStatusItem.tchDocPosFmt, tchLn, tchLines,
 				 tchCol, tchCols, tchCh, tchChs, tchSelCh, tchSel, tchLinesSelected, tchMatchesCount);
 
-	const int iBytes = (int)SciCall_GetLength();
+	const Sci_Position iBytes = SciCall_GetLength();
 	StrFormatByteSize(iBytes, tchDocSize, COUNTOF(tchDocSize));
 
 	StatusSetText(hwndStatus, STATUS_DOCPOS, tchDocPos);
@@ -7780,7 +7780,7 @@ void GetRelaunchParameters(LPWSTR szParameters, LPCWSTR lpszFile, BOOL newWind, 
 		if (pos > 0) {
 			const Sci_Line line = SciCall_LineFromPosition(pos) + 1;
 			const Sci_Position col = SciCall_GetColumn(pos) + 1;
-			wsprintf(tch, L" -g %i,%i", (int)line, (int)col);
+			wsprintf(tch, L" -g %u,%u", (UINT)line, (UINT)col);
 			lstrcat(szParameters, tch);
 		}
 

src/Edit.c
@@ -2376,9 +2376,9 @@ void EditModifyLines(LPCWSTR pwszPrefix, LPCWSTR pwszAppend) {
 			if (bPrefixNum) {
 				char tchNum[64];
 				if (bPrefixNumPadZero) {
-					sprintf(tchNum, "%0*i", iPrefixNumWidth, (int)iPrefixNum);
+					sprintf(tchNum, "%0*u", iPrefixNumWidth, (UINT)iPrefixNum);
 				} else {
-					sprintf(tchNum, "%*i", iPrefixNumWidth, (int)iPrefixNum);
+					sprintf(tchNum, "%*u", iPrefixNumWidth, (UINT)iPrefixNum);
 				}
 				strcat(mszInsert, tchNum);
 				strcat(mszInsert, mszPrefix2);
@@ -2397,9 +2397,9 @@ void EditModifyLines(LPCWSTR pwszPrefix, LPCWSTR pwszAppend) {
 			if (bAppendNum) {
 				char tchNum[64];
 				if (bAppendNumPadZero) {
-					sprintf(tchNum, "%0*i", iAppendNumWidth, (int)iAppendNum);
+					sprintf(tchNum, "%0*u", iAppendNumWidth, (UINT)iAppendNum);
 				} else {
-					sprintf(tchNum, "%*i", iAppendNumWidth, (int)iAppendNum);
+					sprintf(tchNum, "%*u", iAppendNumWidth, (UINT)iAppendNum);
 				}
 				strcat(mszInsert, tchNum);
 				strcat(mszInsert, mszAppend2);
@@ -4861,7 +4861,7 @@ BOOL EditReplace(HWND hwnd, LPEDITFINDREPLACE lpefr) {
 // Mark all occurrences of the text currently selected (by Aleksandar Lekov)
 //
 
-extern int iMatchesCount;
+extern UINT iMatchesCount;
 
 void EditMarkAll_Clear(void) {
 	if (iMatchesCount == 0) {
@@ -5222,7 +5222,7 @@ static INT_PTR CALLBACK EditLineNumDlgProc(HWND hwnd, UINT umsg, WPARAM wParam, 
 		const Sci_Line iMaxLine = SciCall_GetLineCount();
 		const Sci_Position iLength = SciCall_GetLength();
 
-		SetDlgItemInt(hwnd, IDC_LINENUM, (int)iCurLine, FALSE);
+		SetDlgItemInt(hwnd, IDC_LINENUM, (UINT)iCurLine, FALSE);
 		SendDlgItemMessage(hwnd, IDC_LINENUM, EM_LIMITTEXT, 20, 0);
 		SendDlgItemMessage(hwnd, IDC_COLNUM, EM_LIMITTEXT, 20, 0);
 
@@ -5230,13 +5230,13 @@ static INT_PTR CALLBACK EditLineNumDlgProc(HWND hwnd, UINT umsg, WPARAM wParam, 
 		WCHAR tchLines[64];
 		WCHAR tchFmt[64];
 
-		wsprintf(tchLn, L"%i", (int)iMaxLine);
+		wsprintf(tchLn, L"%u", (UINT)iMaxLine);
 		FormatNumberStr(tchLn);
 		GetDlgItemText(hwnd, IDC_LINE_RANGE, tchFmt, COUNTOF(tchFmt));
 		wsprintf(tchLines, tchFmt, tchLn);
 		SetDlgItemText(hwnd, IDC_LINE_RANGE, tchLines);
 
-		wsprintf(tchLn, L"%i", (int)iLength);
+		wsprintf(tchLn, L"%u", (UINT)iLength);
 		FormatNumberStr(tchLn);
 		GetDlgItemText(hwnd, IDC_COLUMN_RANGE, tchFmt, COUNTOF(tchFmt));
 		wsprintf(tchLines, tchFmt, tchLn);
@@ -5252,13 +5252,15 @@ static INT_PTR CALLBACK EditLineNumDlgProc(HWND hwnd, UINT umsg, WPARAM wParam, 
 			BOOL fTranslated;
 			BOOL fTranslated2;
 			WCHAR tchLn[32];
-			int iNewLine = 0;
-			int iNewCol;
+			Sci_Line iNewLine = 0;
+			Sci_Position iNewCol;
 
 			// Extract line number from the text entered
 			// For example: "5410:" will result in 5410
 			GetDlgItemText(hwnd, IDC_LINENUM, tchLn, COUNTOF(tchLn));
-			fTranslated = CRTStrToInt(tchLn, &iNewLine);
+			int iLine = 0;
+			fTranslated = CRTStrToInt(tchLn, &iLine);
+			iNewLine = (UINT)iLine;
 
 			if (SendDlgItemMessage(hwnd, IDC_COLNUM, WM_GETTEXTLENGTH, 0, 0) > 0) {
 				iNewCol = GetDlgItemInt(hwnd, IDC_COLNUM, &fTranslated2, FALSE);

src/EditAutoC.c
@@ -93,6 +93,7 @@ UINT WordList_OrderCase(const void *pWord, unsigned int len) {
 	len = min_u(len, 4);
 	while (len) {
 		unsigned char ch = *ptr++;
+		// convert to lower case to match _stricmp() / strcasecmp().
 		if (ch >= 'A' && ch <= 'Z') {
 			ch = ch + 'a' - 'A';
 		}

src/Notepad2.h
@@ -34,8 +34,8 @@ typedef struct np2params {
 	int		iInitialLexer;
 	int		flagQuietCreate;
 	int		flagJumpTo;
-	int		iInitialLine;
-	int		iInitialColumn;
+	Sci_Line		iInitialLine;
+	Sci_Position	iInitialColumn;
 	int		iSrcEncoding;
 	int		flagSetEncoding;
 	int		flagSetEOLMode;

commit e84d0a46d15a1b14b0a43cdf698f00caf1d4fb2e

Fix '/g -1' broken by commit 5b75e544d342b03e3dc6cc88f22aecdd816867c2.

src/Notepad2.c
@@ -5816,7 +5816,7 @@ int ParseCommandLineOption(LPWSTR lp1, LPWSTR lp2, BOOL *bIsNotepadReplacement) 
 				if (itok != 0) {
 					flagJumpTo = 1;
 					state = 1;
-					iInitialLine = (UINT)cord[0];
+					iInitialLine = (cord[0] == INVALID_POSITION)? INVALID_POSITION : (Sci_Line)((UINT)cord[0]);
 					iInitialColumn = (UINT)cord[1];
 				}
 			}