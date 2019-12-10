06fa13035fe604b353606e436daba166eac79f86
zufuliu <zufuliu@gmail.com>
2019-12-10 19:57:27 +0800

    Fix bug in toggling code folding for current level for indentation based file (Text File, Python and YAML).

--- src/Edit.c
+++ src/Edit.c
@@ -7364,23 +7364,13 @@ void FoldToggleCurrentLevel(FOLD_ACTION action) {
 
 	if (level != 0 && IsFoldIndentationBased(pLexCurrent->iLexer)) {
 		level = 0;
-		while (line > 0) {
-			--line;
-			if (!(SciCall_GetFoldLevel(line) & SC_FOLDLEVELHEADERFLAG)) {
-				line = SciCall_GetFoldParent(line);
-				if (line < 0) {
-					break;
-				}
+		while (line != 0 && level < MAX_EDIT_TOGGLE_FOLD_LEVEL) {
+			line = SciCall_GetFoldParent(line);
+			if (line < 0) {
+				break;
 			}
 			++level;
 		}
-#if 1
-		if (level > MAX_EDIT_TOGGLE_FOLD_LEVEL - 1) {
-			return;
-		}
-#else
-		level = min_i(level, MAX_EDIT_TOGGLE_FOLD_LEVEL - 1);
-#endif
 	}
 
 	FoldToggleLevel(level, action);
