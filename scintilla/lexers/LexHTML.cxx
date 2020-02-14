zufuliu/notepad4

commit eb74edf4180b2580bdf5dce7a4e76f1e2fb578f6

Tiny optimization for HTML lexer.

scintilla/lexers/LexHTML.cxx
@@ -35,6 +35,10 @@ namespace {
 enum script_type { eScriptNone = 0, eScriptJS, eScriptVBS, eScriptPython, eScriptPHP, eScriptXML, eScriptSGML, eScriptSGMLblock, eScriptComment };
 enum script_mode { eHtml = 0, eNonHtmlScript, eNonHtmlPreProc, eNonHtmlScriptPreProc };
 
+constexpr bool IsEmpty(const char *text) noexcept {
+	return text == '\0';
+}
+
 inline void GetTextSegment(Accessor &styler, Sci_PositionU start, Sci_PositionU end, char *s, size_t len) noexcept {
 	styler.GetRangeLowered(start, end + 1, s, len);
 }
@@ -415,7 +419,7 @@ constexpr bool IsScriptCommentState(const int state) noexcept {
 }
 
 bool isMakoBlockEnd(const int ch, const int chNext, const char *blockType) noexcept {
-	if (strlen(blockType) == 0) {
+	if (IsEmpty(blockType)) {
 		return ((ch == '%') && (chNext == '>'));
 	} else if ((0 == strcmp(blockType, "inherit")) ||
 		(0 == strcmp(blockType, "namespace")) ||
@@ -435,7 +439,7 @@ bool isMakoBlockEnd(const int ch, const int chNext, const char *blockType) noexc
 }
 
 bool isDjangoBlockEnd(const int ch, const int chNext, const char *blockType) noexcept {
-	if (strlen(blockType) == 0) {
+	if (IsEmpty(blockType)) {
 		return false;
 	} else if (0 == strcmp(blockType, "%")) {
 		return ((ch == '%') && (chNext == '}'));
@@ -842,8 +846,8 @@ void ColouriseHyperTextDoc(Sci_PositionU startPos, Sci_Position length, int init
 			default :
 				// check if the closing tag is a script tag
 				if (const char *tag =
-						state == SCE_HJ_COMMENTLINE || isXml ? "script" :
-						state == SCE_H_COMMENT ? "comment" : nullptr) {
+						(state == SCE_HJ_COMMENTLINE || isXml) ? "script" :
+						((state == SCE_H_COMMENT) ? "comment" : nullptr)) {
 					Sci_Position j = i + 2;
 					char chr;
 					do {
@@ -1948,7 +1952,7 @@ void ColouriseHyperTextDoc(Sci_PositionU startPos, Sci_Position length, int init
 				} else if (styler.Match(i, "<<<")) {
 					int heardocQuotes = 0;
 					i = FindPhpStringDelimiter(phpStringDelimiter, sizeof(phpStringDelimiter), i + 3, lengthDoc, styler, heardocQuotes);
-					if (strlen(phpStringDelimiter)) {
+					if (!IsEmpty(phpStringDelimiter)) {
 						state = ((heardocQuotes == 1) ? SCE_HPHP_NOWDOC : SCE_HPHP_HEREDOC);
 						if (foldHeredoc) levelCurrent++;
 					}
@@ -2071,7 +2075,7 @@ void ColouriseHyperTextDoc(Sci_PositionU startPos, Sci_Position length, int init
 			} else if (styler.Match(i, "<<<")) {
 				int heardocQuotes = 0;
 				i = FindPhpStringDelimiter(phpStringDelimiter, sizeof(phpStringDelimiter), i + 3, lengthDoc, styler, heardocQuotes);
-				if (strlen(phpStringDelimiter)) {
+				if (!IsEmpty(phpStringDelimiter)) {
 					state = ((heardocQuotes == 1) ? SCE_HPHP_NOWDOC : SCE_HPHP_HEREDOC);
 					if (foldHeredoc) levelCurrent++;
 				}

commit ebf5cea76d18abe0cb5dbd80886c9ceeb6d22d04

Fix bug in LexHTML.cxx introduced by eb74edf4180b2580bdf5dce7a4e76f1e2fb578f6.

scintilla/lexers/LexHTML.cxx
@@ -36,7 +36,7 @@ enum script_type { eScriptNone = 0, eScriptJS, eScriptVBS, eScriptPython, eScrip
 enum script_mode { eHtml = 0, eNonHtmlScript, eNonHtmlPreProc, eNonHtmlScriptPreProc };
 
 constexpr bool IsEmpty(const char *text) noexcept {
-	return text == '\0';
+	return *text == '\0';
 }
 
 inline void GetTextSegment(Accessor &styler, Sci_PositionU start, Sci_PositionU end, char *s, size_t len) noexcept {