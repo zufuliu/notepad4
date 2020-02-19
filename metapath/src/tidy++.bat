e25b2d69ff9b2fc40cff830da3372fe8000c7de3
zufuliu <zufuliu@gmail.com>
2020-02-19 18:34:29 +0800

    Remove `-Wrange-loop-analysis` compiler option for Clang builds. Change clang-tidy C++ standard to c++20.

--- metapath/src/tidy++.bat
+++ metapath/src/tidy++.bat
@@ -1,1 +1,1 @@
-@clang-tidy %1 -- -m64 -x c++ -std=c++2a -ferror-limit=1000 -D_WIN64 -DNDEBUG -DUNICODE -D_UNICODE -D_WIN32_WINNT=0x0502 -DWINVER=0x0502 -D_CRT_SECURE_NO_WARNINGS -D_SCL_SECURE_NO_WARNINGS -Wall -Wextra -Wshadow -Wimplicit-fallthrough -Wformat=2 -Wcomma -Wrange-loop-analysis 1>tidy.log
+@clang-tidy %1 -- -m64 -x c++ -std=c++20 -ferror-limit=1000 -D_WIN64 -DNDEBUG -DUNICODE -D_UNICODE -D_WIN32_WINNT=0x0502 -DWINVER=0x0502 -D_CRT_SECURE_NO_WARNINGS -D_SCL_SECURE_NO_WARNINGS -Wall -Wextra -Wshadow -Wimplicit-fallthrough -Wformat=2 -Wcomma 1>tidy.log
