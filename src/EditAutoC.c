zufuliu/notepad4

commit aa27634c30ea88ad872b429900ee122c1c1a6d1f

Add SGML/DTD keywords, fix SGML/DTD coloring bug (issue #159). Fix out-dated XML keyword list.

src/EditAutoC.c
@@ -1053,10 +1053,8 @@ INT AutoC_AddSpecWord(struct WordList *pWList, int iCurrentStyle, int ch, int ch
 	}
 	else if ((pLexCurrent->rid == NP2LEX_HTML || pLexCurrent->rid == NP2LEX_XML || pLexCurrent->rid == NP2LEX_CONF)
 			   && ((ch == '<') || (chPrev == '<' && ch == '/'))) {
-		if (pLexCurrent->rid == NP2LEX_HTML || pLexCurrent->rid == NP2LEX_CONF) { // HTML Tag
-			WordList_AddList(pWList, pLexCurrent->pKeyWords->pszKeyWords[0]);// HTML Tag
-		} else {
-			WordList_AddList(pWList, pLexCurrent->pKeyWords->pszKeyWords[3]);// XML Tag
+		WordList_AddListEx(pWList, pLexCurrent->pKeyWords->pszKeyWords[0]);// Tag
+		if (pLexCurrent->rid == NP2LEX_XML) {
 			if (np2_LexKeyword) { // XML Tag
 				WordList_AddList(pWList, (*np2_LexKeyword)[0]);
 			}

src/EditLexers/stlHTML.c
@@ -3,7 +3,7 @@
 
 static KEYWORDLIST Keywords_HTML = {{
 // Tag
-"!doctype !DOCTYPE a abbr address area article aside audio b base bdi bdo "
+"a abbr address area article aside audio b base bdi bdo "
 "blockquote body br button canvas caption cite code col colgroup command datalist "
 "data dd del details dfn dialog div dl dt em embed fieldset figcaption figure footer "
 "form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd "
@@ -57,7 +57,10 @@ NULL
 "__CLASS__ __DIR__ __FILE__ __FUNCTION__ __LINE__ __METHOD__ __NAMESPACE__ __TRAIT__ "
 
 , // 5 SGML DTD
-NULL
+"doctype DOCTYPE PUBLIC SYSTEM "
+"ELEMENT ATTLIST ENTITY "
+"ANY CDATA EMPT ENTITIES ID IDREF IDREFS NDATA NOTATION NMTOKEN NMTOKENS "
+"IMPLIED FIXED PCDATA REQUIRED "
 
 , // 6 Attribute
 // Attribute
@@ -134,9 +137,8 @@ NULL
 "allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation "
 "row col rowgroup colgroup any "
 "command context toolbar soft hard "
-, NULL
 
-, NULL, NULL, NULL, NULL, NULL, NULL, NULL
+,NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL
 }};
 
 static EDITSTYLE Styles_HTML[] = {

src/EditLexers/stlXML.c
@@ -2,22 +2,37 @@
 #include "EditStyleX.h"
 
 static KEYWORDLIST Keywords_XML = {{
-NULL, NULL, NULL
+// Tag
+NULL
 
-, // 4 Tag
-"xml xmlns xsi DOCTYPE doctype PUBLIC "
+, // 1 JavaScript
+NULL
 
-, // 5 Attribute
-"version encoding standalone noNamespaceSchemaLocation schemaLocation CDATA "
+, // 2 VBScript
+NULL
+
+, // 3 Python
+NULL
+
+, // 4 PHP
+NULL
+
+, // 5 SGML DTD
+"doctype DOCTYPE PUBLIC SYSTEM "
+"ELEMENT ATTLIST ENTITY "
+"ANY CDATA EMPT ENTITIES ID IDREF IDREFS NDATA NOTATION NMTOKEN NMTOKENS "
+"IMPLIED FIXED PCDATA REQUIRED "
+
+, // 6 Attribute
+"xml xmlns xsi "
+"version encoding standalone noNamespaceSchemaLocation schemaLocation "
 "lang id name type value "
 
-, // 6 Value
+, // 7 Value
 "utf-8 UTF-8 ISO-8859-1 "
 "true false yes no on off "
 
-, NULL, NULL, NULL
-
-, NULL, NULL, NULL, NULL, NULL, NULL, NULL
+,NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL
 }};
 
 static EDITSTYLE Styles_XML[] = {

commit 6e784b4300a31f1ea4454ef5b0f84690de42125a

Fix auto-completion crash for XML tags, issue #163. This bug was introduced by aa27634c30ea88ad872b429900ee122c1c1a6d1f.

src/EditAutoC.c
@@ -1057,7 +1057,7 @@ INT AutoC_AddSpecWord(struct WordList *pWList, int iCurrentStyle, int ch, int ch
 	}
 	else if ((pLexCurrent->rid == NP2LEX_HTML || pLexCurrent->rid == NP2LEX_XML || pLexCurrent->rid == NP2LEX_CONF)
 			   && ((ch == '<') || (chPrev == '<' && ch == '/'))) {
-		WordList_AddListEx(pWList, pLexCurrent->pKeyWords->pszKeyWords[0]);// Tag
+		WordList_AddList(pWList, pLexCurrent->pKeyWords->pszKeyWords[0]);// Tag
 		if (pLexCurrent->rid == NP2LEX_XML) {
 			if (np2_LexKeyword) { // XML Tag
 				WordList_AddList(pWList, (*np2_LexKeyword)[0]);