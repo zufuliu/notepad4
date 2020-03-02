0f6c7cb7c8be2f6962b0f621bfb405f615b0574b
zufuliu <zufuliu@gmail.com>
2020-03-02 22:01:56 +0800

    Add Clang ARM (Windows RT) AppVeyor build configuration. update LLVM utils to v20.03.

--- build/install_llvm.bat
+++ build/install_llvm.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 @rem used on AppVeyor
 
-curl -Ls -o "LLVM_VS2017.zip" "https://github.com/zufuliu/llvm-utils/releases/download/v19.05/LLVM_VS2017.zip"
+curl -Ls -o "LLVM_VS2017.zip" "https://github.com/zufuliu/llvm-utils/releases/download/v20.03/LLVM_VS2017.zip"
 7z x -y "LLVM_VS2017.zip" >NUL
 CALL "LLVM_VS2017\install.bat" %1
