aae40e07b74cac6251a45e020120210fa1a4241d
zufuliu <zufuliu@gmail.com>
2020-04-15 13:11:51 +0800

    Update wiki and release note template.

--- src-grep.bat
+++ src-grep.bat
@@ -1,2 +1,4 @@
-@rem see https://github.com/BurntSushi/ripgrep for anothor tool
+@rem see https://github.com/BurntSushi/ripgrep
+@rem or https://github.com/ggreer/the_silver_searcher
+@rem for similar tools with colored output.
 @grep --exclude-dir=.git --exclude-dir=build --exclude-dir=doc --exclude-dir=res -nr %1
