f73bf0632cdbd378c668012daec389f5b08d6cda
zufuliu <zufuliu@gmail.com>
2020-04-22 19:10:57 +0800

    Simplify SSE2 and AVX2 preprocessors. Increase threshold for without PerLine to 1024 bytes.

--- scintilla/include/VectorISA.h
+++ scintilla/include/VectorISA.h
@@ -28,22 +28,16 @@
 #else
 	#define NP2_TARGET_ARM64	0
 	#define NP2_TARGET_ARM32	0
-	#ifndef NP2_USE_SSE2
-		// SSE2 enabled by default
-		#define NP2_USE_SSE2	1
-	#endif
+	// SSE2 enabled by default
+	#define NP2_USE_SSE2		1
 
 	// Clang & GCC use -mavx2 -mpopcnt -mbmi (to enable tzcnt).
 	// MSVC use /arch:AVX2
-	#if defined(_WIN64) && !defined(NP2_USE_AVX2)
-		#if defined(__AVX2__)
-			#define NP2_USE_AVX2	1
-		#else
-			#define NP2_USE_AVX2	0
-		#endif
+	#if defined(_WIN64) && defined(__AVX2__)
+		#define NP2_USE_AVX2	1
 	#else
 		#define NP2_USE_AVX2	0
-	#endif // NP2_USE_AVX2
+	#endif
 
 	// TODO: use __isa_enabled/__isa_available in MSVC build to dynamic enable AVX2 code.
 	//#if defined(_MSC_VER) || (defined(__has_include) && __has_include(<isa_availability.h>))
@@ -68,10 +62,11 @@
 #if !(NP2_TARGET_ARM64 || NP2_TARGET_ARM32)
 
 // count trailing zero bits
+//!NOTE: TZCNT is compatible with BSF for non-zero value;
+//! but LZCNT is not compatible with BSR, LZCNT = 31 - BSR.
 #if defined(__clang__) || defined(__GNUC__)
 	#define np2_ctz(x)		__builtin_ctz(x)
 #elif defined(_MSC_VER) || defined(__INTEL_COMPILER_BUILD_DATE)
-	//! NOTE: TZCNT is compatible with BSF; but LZCNT is not compatible with BSR, LZCNT = 31 - BSR.
 	#define np2_ctz(x)		_tzcnt_u32(x)
 #elif defined(_WIN32)
 	// https://software.intel.com/sites/landingpage/IntrinsicsGuide/#text=bsf
