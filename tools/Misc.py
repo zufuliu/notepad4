acd3f0420aa5ba0d8f0de2a086c46b0aef6bceb8
zufuliu <zufuliu@gmail.com>
2020-05-07 12:33:27 +0800

    Increase resource id for styles by 100 to allow adding more lexer in the further, issue #182.

---
+++ tools/Misc.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#-*- coding: UTF-8 -*-
+import sys
+import os.path
+import re
+
+def increase_style_resource_id_value(path, delta=100):
+	doc = open(path, encoding='utf-8', newline='\n').read()
+	updated = re.sub(r'\d{5}', lambda m: str(int(m.group(0)) + delta), doc)
+	print('update:', path)
+	with open(path, 'w', encoding='utf-8', newline='\n') as fp:
+		fp.write(updated)
+
+increase_style_resource_id_value('../src/EditLexers/EditStyle.h')
