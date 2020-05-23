37e3523f671f8afad41e9cb5c627cfd4cf33de9b
zufuliu <zufuliu@gmail.com>
2020-05-23 08:40:18 +0800

    Add GitHub action.

--- build/install_llvm.bat
+++ build/install_llvm.bat
@@ -1,5 +1,11 @@
 @ECHO OFF
-@rem used on AppVeyor
+@rem used on AppVeyor and GitHub
+
+IF /I "%~1" == "latest" (
+  SHIFT
+  curl -Ls -o "LLVM-10.0.0-win64.exe" "https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/LLVM-10.0.0-win64.exe"
+  LLVM-10.0.0-win64.exe /S
+)
 
 curl -Ls -o "LLVM_VS2017.zip" "https://github.com/zufuliu/llvm-utils/releases/download/v20.03/LLVM_VS2017.zip"
 7z x -y "LLVM_VS2017.zip" >NUL
