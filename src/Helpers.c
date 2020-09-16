f5fa03c8913ee99e2b0e000afeb3141b7ac555fc
zufuliu <zufuliu@gmail.com>
2020-09-16 23:17:04 +0800

    Fix INI parser bug, remove duplicated code.

--- src/Helpers.c
+++ src/Helpers.c
@@ -144,10 +144,9 @@ BOOL IniSectionParse(IniSection *section, LPWSTR lpCachedIniSection) {
 			node->key = p;
 			node->value = v;
 			++count;
-			p = StrEnd(v) + 1;
-		} else {
-			p = StrEnd(p) + 1;
+			p = v;
 		}
+		p = StrEnd(p) + 1;
 	} while (*p && count < capacity);
 
 	if (count == 0) {
