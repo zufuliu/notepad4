zufuliu/notepad4

commit 62a7bb9148beba02d7ba7c8b3bffc4c272998be4

Improve application responsive while selecting on long line in large file, issue #236.

src/Edit.c
@@ -94,19 +94,24 @@ static HMODULE hELSCoreDLL = NULL;
 
 #define MAX_NON_UTF8_SIZE	(UINT_MAX/2 - 16)
 
-static struct EditMarkAllStatus {
+typedef struct EditMarkAllStatus {
+	volatile LONG token;
+	BOOL done;
+	CRITICAL_SECTION criticalSection;
 	int findFlag;
 	Sci_Position iSelCount;
 	LPSTR pszText;
-} editMarkAllStatus;
+} EditMarkAllStatus;
+static EditMarkAllStatus editMarkAllStatus;
 
 void Edit_ReleaseResources(void) {
 	DStringW_Free(&wchPrefixSelection);
 	DStringW_Free(&wchAppendSelection);
 	DStringW_Free(&wchPrefixLines);
 	DStringW_Free(&wchAppendLines);
-	if (editMarkAllStatus.pszText) {
-		NP2HeapFree(editMarkAllStatus.pszText);
+	if (editMarkAllStatus.done) {
+		EditMarkAll_Clear();
+		DeleteCriticalSection(&editMarkAllStatus.criticalSection);
 	}
 #if NP2_DYNAMIC_LOAD_ELSCORE_DLL
 	if (hELSCoreDLL != NULL) {
@@ -5622,24 +5627,81 @@ BOOL EditReplace(HWND hwnd, LPEDITFINDREPLACE lpefr) {
 extern Sci_Position iMatchesCount;
 
 void EditMarkAll_Clear(void) {
-	if (iMatchesCount == 0) {
-		return;
+	InterlockedIncrement(&editMarkAllStatus.token);
+	if (iMatchesCount != 0) {
+		iMatchesCount = 0;
+		// clear existing indicator
+		SciCall_SetIndicatorCurrent(IndicatorNumber_MarkOccurrence);
+		SciCall_IndicatorClearRange(0, SciCall_GetLength());
 	}
 
-	iMatchesCount = 0;
-	// clear existing indicator
-	SciCall_SetIndicatorCurrent(IndicatorNumber_MarkOccurrence);
-	SciCall_IndicatorClearRange(0, SciCall_GetLength());
-
+	EnterCriticalSection(&editMarkAllStatus.criticalSection);
 	if (editMarkAllStatus.pszText) {
-		NP2HeapFree(editMarkAllStatus.pszText);
+		LocalFree(editMarkAllStatus.pszText);
 	}
 	editMarkAllStatus.findFlag = 0;
 	editMarkAllStatus.iSelCount= 0;
 	editMarkAllStatus.pszText = NULL;
+	LeaveCriticalSection(&editMarkAllStatus.criticalSection);
+}
+
+void EditMarkAll_Run(EditMarkAllStatus *status) {
+	const LONG token = status->token;
+
+	struct Sci_TextToFind ttf = {{0, SciCall_GetLength()}, status->pszText, {0, 0}};
+	const Sci_Position iSelCount = status->iSelCount;
+
+	Sci_Position matchCount = 0;
+	UINT index = 0;
+	Sci_Position posList[256];
+
+	BOOL done = FALSE;
+	while (!done) {
+		HANDLE timer = WaitableTimer_New(WaitableTimer_DefaultTimeSlot);
+		while (WaitForSingleObject(timer, 0) != WAIT_OBJECT_0) {
+			const Sci_Position pos = SciCall_FindText(status->findFlag, &ttf);
+			if (pos == -1 || token != editMarkAllStatus.token) {
+				done = TRUE;
+				break;
+			}
+			++matchCount;
+			if (index == COUNTOF(posList)) {
+				iMatchesCount = matchCount;
+				SciCall_SetIndicatorCurrent(IndicatorNumber_MarkOccurrence);
+				for (UINT i = 0; i < index; i++) {
+					SciCall_IndicatorFillRange(posList[i], iSelCount);
+				}
+				index = 0;
+			}
+			posList[index++] = pos;
+			ttf.chrg.cpMin = ttf.chrgText.cpMax;
+		}
+		CloseHandle(timer);
+		if (token == editMarkAllStatus.token) {
+			iMatchesCount = matchCount;
+			if (index) {
+				SciCall_SetIndicatorCurrent(IndicatorNumber_MarkOccurrence);
+				for (UINT i = 0; i < index; i++) {
+					SciCall_IndicatorFillRange(posList[i], iSelCount);
+				}
+			}
+			UpdateStatusbar();
+		}
+		if (!done) {
+			WaitableTimer_Yield(WaitableTimer_DefaultYieldTime);
+		}
+	}
+
+	NP2HeapFree(status->pszText);
+	NP2HeapFree(status);
 }
 
 void EditMarkAll(BOOL bChanged, BOOL bMarkOccurrencesMatchCase, BOOL bMarkOccurrencesMatchWords) {
+	if (!editMarkAllStatus.done) {
+		InitializeCriticalSection(&editMarkAllStatus.criticalSection);
+		editMarkAllStatus.done = TRUE;
+	}
+
 	// get current selection
 	Sci_Position iSelStart = SciCall_GetSelectionStart();
 	const Sci_Position iSelEnd = SciCall_GetSelectionEnd();
@@ -5674,40 +5736,31 @@ void EditMarkAll(BOOL bChanged, BOOL bMarkOccurrencesMatchCase, BOOL bMarkOccurr
 	}
 
 	const int findFlag = (bMarkOccurrencesMatchCase ? SCFIND_MATCHCASE : 0) | (bMarkOccurrencesMatchWords ? SCFIND_WHOLEWORD : 0);
-	if (!bChanged && findFlag == editMarkAllStatus.findFlag && editMarkAllStatus.iSelCount == iSelCount) {
-		// _stricmp() is not safe for DBCS string.
-		if (memcmp(pszText, editMarkAllStatus.pszText, iSelCount) == 0) {
-			NP2HeapFree(pszText);
-			return;
+	if (!bChanged) {
+		EnterCriticalSection(&editMarkAllStatus.criticalSection);
+		if (findFlag == editMarkAllStatus.findFlag && editMarkAllStatus.iSelCount == iSelCount) {
+			// _stricmp() is not safe for DBCS string.
+			if (memcmp(pszText, editMarkAllStatus.pszText, iSelCount) == 0) {
+				NP2HeapFree(pszText);
+				LeaveCriticalSection(&editMarkAllStatus.criticalSection);
+				return;
+			}
 		}
+		LeaveCriticalSection(&editMarkAllStatus.criticalSection);
 	}
 
 	EditMarkAll_Clear();
-	SciCall_SetIndicatorCurrent(IndicatorNumber_MarkOccurrence);
-
-	SciCall_SetSearchFlags(findFlag);
-	const Sci_Position iDocLen = SciCall_GetLength();
-
-	Sci_Position iPos = 0;
-	do {
-		SciCall_SetTargetRange(iPos, iDocLen);
-		iPos = SciCall_SearchInTarget(iSelCount, pszText);
-		if (iPos == -1) {
-			break;
-		}
-		// mark this match
-		++iMatchesCount;
-		SciCall_IndicatorFillRange(iPos, iSelCount);
-		iPos += iSelCount;
-	} while (iPos < iDocLen);
-
-	if (iMatchesCount > 1) {
-		editMarkAllStatus.findFlag = findFlag;
-		editMarkAllStatus.iSelCount = iSelCount;
-		editMarkAllStatus.pszText = pszText;
-	} else {
-		NP2HeapFree(pszText);
-	}
+	EditMarkAllStatus *status = (EditMarkAllStatus *)NP2HeapAlloc(sizeof(EditMarkAllStatus));
+	status->token = editMarkAllStatus.token;
+	status->findFlag = findFlag;
+	status->iSelCount = iSelCount;
+	status->pszText = pszText;
+	EnterCriticalSection(&editMarkAllStatus.criticalSection);
+	editMarkAllStatus.findFlag = findFlag;
+	editMarkAllStatus.iSelCount = iSelCount;
+	editMarkAllStatus.pszText = StrDupA(pszText);
+	LeaveCriticalSection(&editMarkAllStatus.criticalSection);
+	EditMarkAll_Run(status);
 }
 
 static void ShwowReplaceCount(Sci_Position iCount) {

metapath/src/metapath.h
@@ -76,18 +76,17 @@
 #define IDP_POPUP_SUBMENU_TRAY	4
 
 //==== Statusbar ==============================================================
-#define ID_FILEINFO   0
-#define ID_MENUHELP (255 | SBT_NOBORDERS)
+#define ID_FILEINFO		0
+#define ID_MENUHELP		(255 | SBT_NOBORDERS)
 
 //==== Timer for Change Notifications =========================================
-#define ID_TIMER 0xA000
+#define ID_TIMER		0xA000
 
 /**
  * App message used to center MessageBox to the window of the program.
  */
 #define APPM_CENTER_MESSAGE_BOX		(WM_APP + 1)
-//==== Callback Message from System Tray ======================================
-#define APPM_TRAYMESSAGE			(WM_APP + 4)
+#define APPM_TRAYMESSAGE			(WM_APP + 4) // Callback Message from System Tray
 
 #define INI_SECTION_NAME_METAPATH			L"metapath"
 #define INI_SECTION_NAME_SETTINGS			L"Settings"

src/Helpers.c
@@ -443,6 +443,26 @@ HBITMAP ResizeImageForDPI(HBITMAP hbmp, UINT dpi, int height) {
 	return hbmp;
 }
 
+HANDLE WaitableTimer_New(DWORD milliseconds) {
+	HANDLE timer = CreateWaitableTimer(NULL, FALSE, NULL);
+	LARGE_INTEGER dueTime;
+	dueTime.QuadPart = -INT64_C(10*1000)*milliseconds;
+	SetWaitableTimer(timer, &dueTime, 0, NULL, NULL, FALSE);
+	return timer;
+}
+
+void WaitableTimer_Yield(DWORD milliseconds) {
+	HANDLE timer = WaitableTimer_New(milliseconds);
+	while (WaitForSingleObject(timer, 0) != WAIT_OBJECT_0) {
+		MSG msg;
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+	CloseHandle(timer);
+}
+
 //=============================================================================
 //
 // PrivateSetCurrentProcessExplicitAppUserModelID()

src/Helpers.h
@@ -572,6 +572,11 @@ NP2_inline BOOL KeyboardIsKeyDown(int key) {
 	return (GetKeyState(key) & 0x8000) != 0;
 }
 
+#define WaitableTimer_DefaultTimeSlot		100
+#define WaitableTimer_DefaultYieldTime		50
+HANDLE WaitableTimer_New(DWORD milliseconds);
+void WaitableTimer_Yield(DWORD milliseconds);
+
 HRESULT PrivateSetCurrentProcessExplicitAppUserModelID(PCWSTR AppID);
 BOOL IsElevated(void);
 

src/Notepad2.h
@@ -80,19 +80,14 @@ typedef struct NP2PARAMS {
  * App message used to center MessageBox to the window of the program.
  */
 #define APPM_CENTER_MESSAGE_BOX		(WM_APP + 1)
-//==== Change Notifications ===================================================
-#define ID_WATCHTIMER		0xA000
-#define APPM_CHANGENOTIFY			(WM_APP + 2)
+#define APPM_CHANGENOTIFY			(WM_APP + 2)	// file change notifications
 //#define APPM_CHANGENOTIFYCLEAR	(WM_APP + 3)
+#define APPM_TRAYMESSAGE			(WM_APP + 4)	// callback message from system tray
 
-//==== Callback Message from System Tray ======================================
-#define APPM_TRAYMESSAGE			(WM_APP + 4)
+#define ID_WATCHTIMER				0xA000	// FILE WATCH TIMER
+#define ID_PASTEBOARDTIMER			0xA001	// PASTE BOARD TIMER
 
-//==== Paste Board Timer ======================================================
-#define ID_PASTEBOARDTIMER			0xA001
-
-//==== Reuse Window Lock Timeout ==============================================
-#define REUSEWINDOWLOCKTIMEOUT		1000
+#define REUSEWINDOWLOCKTIMEOUT		1000	// Reuse Window Lock Timeout
 
 // Settings Version
 enum {

commit c018156ddb20fad03c667e54a62ce2f37ad32b07

Fix crash on turning off mark occurrences, introduced by 62a7bb9148beba02d7ba7c8b3bffc4c272998be4.

src/Edit.c
@@ -96,7 +96,7 @@ static HMODULE hELSCoreDLL = NULL;
 
 typedef struct EditMarkAllStatus {
 	volatile LONG token;
-	LONG done;
+	volatile LONG done;
 	CRITICAL_SECTION criticalSection;
 	int findFlag;
 	Sci_Position iSelCount;
@@ -5633,6 +5633,9 @@ void EditMarkAll_Clear(void) {
 		// clear existing indicator
 		SciCall_SetIndicatorCurrent(IndicatorNumber_MarkOccurrence);
 		SciCall_IndicatorClearRange(0, SciCall_GetLength());
+	}
+	if (!editMarkAllStatus.done) {
+		return;
 	}
 
 	EnterCriticalSection(&editMarkAllStatus.criticalSection);