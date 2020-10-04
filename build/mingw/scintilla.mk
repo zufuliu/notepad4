81e66391e6a362da5de1b37d7ac4d6c1df7fb230
zufuliu <zufuliu@gmail.com>
2020-10-04 17:42:35 +0800

    Format Makefile, rename BINDIR to OBJDIR, complete Win32/i686 building, issue #240.

--- build/mingw/scintilla.mk
+++ build/mingw/scintilla.mk
@@ -2,7 +2,7 @@
 
 PROJ = scintilla
 NAME = lib$(PROJ).a
-BINDIR = $(BINFOLDER)/$(PROJ)
+OBJDIR = $(BINFOLDER)/obj/$(PROJ)
 
 CPPFLAGS += -DSCI_LEXER -DNO_CXX11_REGEX
 
@@ -13,41 +13,41 @@ scisrc_dir = $(scintilla_dir)/src
 sciwin32_dir = $(scintilla_dir)/win32
 
 INCDIR = \
--I"$(scintilla_dir)/include" \
--I"$(scintilla_dir)/lexlib" \
--I"$(scintilla_dir)/src"
+	-I"$(scintilla_dir)/include" \
+	-I"$(scintilla_dir)/lexlib" \
+	-I"$(scintilla_dir)/src"
 
 lexers_src = $(wildcard $(lexers_dir)/*.cxx)
-lexers_obj = $(patsubst $(lexers_dir)/%.cxx,$(BINDIR)/%.obj,$(lexers_src))
+lexers_obj = $(patsubst $(lexers_dir)/%.cxx,$(OBJDIR)/%.obj,$(lexers_src))
 
 lexlib_src = $(wildcard $(lexlib_dir)/*.cxx)
-lexlib_obj = $(patsubst $(lexlib_dir)/%.cxx,$(BINDIR)/%.obj,$(lexlib_src))
+lexlib_obj = $(patsubst $(lexlib_dir)/%.cxx,$(OBJDIR)/%.obj,$(lexlib_src))
 
 scisrc_src = $(wildcard $(scisrc_dir)/*.cxx)
-scisrc_obj = $(patsubst $(scisrc_dir)/%.cxx,$(BINDIR)/%.obj,$(scisrc_src))
+scisrc_obj = $(patsubst $(scisrc_dir)/%.cxx,$(OBJDIR)/%.obj,$(scisrc_src))
 
 sciwin32_src = $(wildcard $(sciwin32_dir)/*.cxx)
-sciwin32_obj = $(patsubst $(sciwin32_dir)/%.cxx,$(BINDIR)/%.obj,$(sciwin32_src))
+sciwin32_obj = $(patsubst $(sciwin32_dir)/%.cxx,$(OBJDIR)/%.obj,$(sciwin32_src))
 
-all : $(BINDIR) $(NAME)
+all : $(OBJDIR) $(NAME)
 
-$(BINDIR) :
-	@mkdir -p $(BINDIR)
+$(OBJDIR) :
+	@mkdir -p $(OBJDIR)
 
 $(NAME) : $(lexers_obj) $(lexlib_obj) $(scisrc_obj) $(sciwin32_obj)
-	$(AR) cru $(BINDIR)/$@ $^
+	$(AR) cru $(OBJDIR)/$@ $^
 
-$(lexers_obj): $(BINDIR)/%.obj: $(lexers_dir)/%.cxx
-	$(CXX) -c $(CPPFLAGS) $(CXXFLAGS) $(INCDIR) $< -o $(BINDIR)/$*.obj
+$(lexers_obj): $(OBJDIR)/%.obj: $(lexers_dir)/%.cxx
+	$(CXX) -c $(CPPFLAGS) $(CXXFLAGS) $(INCDIR) $< -o $(OBJDIR)/$*.obj
 
-$(lexlib_obj): $(BINDIR)/%.obj: $(lexlib_dir)/%.cxx
-	$(CXX) -c $(CPPFLAGS) $(CXXFLAGS) $(INCDIR) $< -o $(BINDIR)/$*.obj
+$(lexlib_obj): $(OBJDIR)/%.obj: $(lexlib_dir)/%.cxx
+	$(CXX) -c $(CPPFLAGS) $(CXXFLAGS) $(INCDIR) $< -o $(OBJDIR)/$*.obj
 
-$(scisrc_obj): $(BINDIR)/%.obj: $(scisrc_dir)/%.cxx
-	$(CXX) -c $(CPPFLAGS) $(CXXFLAGS) $(INCDIR) $< -o $(BINDIR)/$*.obj
+$(scisrc_obj): $(OBJDIR)/%.obj: $(scisrc_dir)/%.cxx
+	$(CXX) -c $(CPPFLAGS) $(CXXFLAGS) $(INCDIR) $< -o $(OBJDIR)/$*.obj
 
-$(sciwin32_obj): $(BINDIR)/%.obj: $(sciwin32_dir)/%.cxx
-	$(CXX) -c $(CPPFLAGS) $(CXXFLAGS) $(INCDIR) $< -o $(BINDIR)/$*.obj
+$(sciwin32_obj): $(OBJDIR)/%.obj: $(sciwin32_dir)/%.cxx
+	$(CXX) -c $(CPPFLAGS) $(CXXFLAGS) $(INCDIR) $< -o $(OBJDIR)/$*.obj
 
 clean :
-	@$(RM) -r $(BINDIR)
+	@$(RM) -r $(OBJDIR)
