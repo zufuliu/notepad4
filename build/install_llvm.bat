e1bd2506b96366f52d66a788818291462abc6d6e
zufuliu <zufuliu@gmail.com>
2020-10-13 22:22:37 +0800

    Fix CI failure.

--- build/install_llvm.bat
+++ build/install_llvm.bat
@@ -4,7 +4,7 @@
 IF /I "%~1" == "latest" (
   SHIFT
   curl -fsSL -o "LLVM-11.0.0-win64.exe" "https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/LLVM-11.0.0-win64.exe"
-  LLVM-10.0.0-win64.exe /S
+  LLVM-11.0.0-win64.exe /S
 )
 
 curl -fsSL -o "LLVM_VS2017.zip" "https://github.com/zufuliu/llvm-utils/releases/download/v20.03/LLVM_VS2017.zip"
