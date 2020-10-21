5dccb4c91649d0fa3471ce23221de89707f84695
zufuliu <zufuliu@gmail.com>
2020-10-22 07:46:53 +0800

    Fix GitHub Actions failure.

--- build/install_mingw.bat
+++ build/install_mingw.bat
@@ -1,6 +1,5 @@
 @ECHO OFF
 @rem used for GitHub Actions
-SETLOCAL ENABLEEXTENSIONS
 
 @rem https://github.com/actions/virtual-environments/blob/main/images/win/Windows2019-Readme.md#msys2
 SET "PATH=C:\msys64\usr\bin;%PATH%"
@@ -20,7 +19,6 @@ IF /I "%~1" == "i686" (
 IF /I "%~1" == "llvm" (
   SHIFT
   @rem for CI purpose only, the result binary is dynamic linked against api-ms-win-crt*.dll instead of msvcrt.dll
-  SET "FILE=llvm-mingw-20201020-ucrt-x86_64.zip"
-  curl -fsSL -o "%FILE%" "https://github.com/mstorsjo/llvm-mingw/releases/download/20201020/%FILE%"
-  7z x -y -o"C:\" "%FILE%" >NUL
+  curl -fsSL -o "llvm-mingw-20201020-ucrt-x86_64.zip" "https://github.com/mstorsjo/llvm-mingw/releases/download/20201020/llvm-mingw-20201020-ucrt-x86_64.zip"
+  7z x -y -o"C:\" "llvm-mingw-20201020-ucrt-x86_64.zip" >NUL
 )
