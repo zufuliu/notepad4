882ad7483034f327cecf8d502b765c2be7d063e4
zufuliu <zufuliu@gmail.com>
2021-01-07 21:40:48 +0800

    LLVM 11.0.1

--- build/install_llvm.bat
+++ build/install_llvm.bat
@@ -3,8 +3,8 @@
 
 IF /I "%~1" == "latest" (
   SHIFT
-  curl -fsSL -o "LLVM-11.0.0-win64.exe" "https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/LLVM-11.0.0-win64.exe"
-  LLVM-11.0.0-win64.exe /S
+  curl -fsSL -o "LLVM-11.0.1-win64.exe" "https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.1/LLVM-11.0.1-win64.exe"
+  LLVM-11.0.1-win64.exe /S
 )
 
 curl -fsSL -o "LLVM_VS2017.zip" "https://github.com/zufuliu/llvm-utils/releases/download/v20.03/LLVM_VS2017.zip"
