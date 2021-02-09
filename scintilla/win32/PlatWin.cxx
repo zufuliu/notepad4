zufuliu/notepad4

commit 5297846df0bd1c9db903b2f0cbe0edc083a01bf2

Extract rectangles conversion functions.

scintilla/win32/PlatWin.cxx
@@ -35,6 +35,8 @@
 #include <shlwapi.h>
 
 #include "Platform.h"
+#include "VectorISA.h"
+
 #include "Scintilla.h"
 #include "XPM.h"
 #include "CharClassify.h"
@@ -111,7 +113,7 @@ void Scintilla_LoadDpiForWindow(void) {
 	}
 }
 
-UINT GetWindowDPI(HWND hwnd) NP2_noexcept {
+UINT GetWindowDPI(HWND hwnd) NP2F_noexcept {
 	if (fnGetDpiForWindow) {
 		return fnGetDpiForWindow(hwnd);
 	}
@@ -126,7 +128,7 @@ UINT GetWindowDPI(HWND hwnd) NP2_noexcept {
 	return g_uSystemDPI;
 }
 
-int SystemMetricsForDpi(int nIndex, UINT dpi) NP2_noexcept {
+int SystemMetricsForDpi(int nIndex, UINT dpi) NP2F_noexcept {
 	if (fnGetSystemMetricsForDpi) {
 		return fnGetSystemMetricsForDpi(nIndex, dpi);
 	}
@@ -136,7 +138,7 @@ int SystemMetricsForDpi(int nIndex, UINT dpi) NP2_noexcept {
 	return value;
 }
 
-BOOL AdjustWindowRectForDpi(LPRECT lpRect, DWORD dwStyle, DWORD dwExStyle, UINT dpi) NP2_noexcept {
+BOOL AdjustWindowRectForDpi(LPRECT lpRect, DWORD dwStyle, DWORD dwExStyle, UINT dpi) NP2F_noexcept {
 	if (fnAdjustWindowRectExForDpi) {
 		return fnAdjustWindowRectExForDpi(lpRect, dwStyle, FALSE, dwExStyle, dpi);
 	}
@@ -755,14 +757,14 @@ void SurfaceGDI::Polygon(const Point *pts, size_t npts, ColourDesired fore, Colo
 void SurfaceGDI::RectangleDraw(PRectangle rc, ColourDesired fore, ColourDesired back) noexcept {
 	PenColour(fore);
 	BrushColour(back);
-	const RECT rcw = RectFromPRectangle(rc);
+	const RECT rcw = RectFromPRectangleEx(rc);
 	::Rectangle(hdc, rcw.left, rcw.top, rcw.right, rcw.bottom);
 }
 
 void SurfaceGDI::FillRectangle(PRectangle rc, ColourDesired back) noexcept {
 	// Using ExtTextOut rather than a FillRect ensures that no dithering occurs.
 	// There is no need to allocate a brush either.
-	const RECT rcw = RectFromPRectangle(rc);
+	const RECT rcw = RectFromPRectangleEx(rc);
 	::SetBkColor(hdc, back.AsInteger());
 	::ExtTextOut(hdc, rcw.left, rcw.top, ETO_OPAQUE, &rcw, L"", 0, nullptr);
 }
@@ -774,7 +776,7 @@ void SurfaceGDI::FillRectangle(PRectangle rc, Surface &surfacePattern) noexcept 
 	} else {	// Something is wrong so display in red
 		br = ::CreateSolidBrush(RGB(0xff, 0, 0));
 	}
-	const RECT rcw = RectFromPRectangle(rc);
+	const RECT rcw = RectFromPRectangleEx(rc);
 	::FillRect(hdc, &rcw, br);
 	::DeleteObject(br);
 }
@@ -782,7 +784,7 @@ void SurfaceGDI::FillRectangle(PRectangle rc, Surface &surfacePattern) noexcept 
 void SurfaceGDI::RoundedRectangle(PRectangle rc, ColourDesired fore, ColourDesired back) noexcept {
 	PenColour(fore);
 	BrushColour(back);
-	const RECT rcw = RectFromPRectangle(rc);
+	const RECT rcw = RectFromPRectangleEx(rc);
 	::RoundRect(hdc,
 		rcw.left + 1, rcw.top,
 		rcw.right - 1, rcw.bottom,
@@ -1024,7 +1026,7 @@ void SurfaceGDI::DrawRGBAImage(PRectangle rc, int width, int height, const unsig
 void SurfaceGDI::Ellipse(PRectangle rc, ColourDesired fore, ColourDesired back) noexcept {
 	PenColour(fore);
 	BrushColour(back);
-	const RECT rcw = RectFromPRectangle(rc);
+	const RECT rcw = RectFromPRectangleEx(rc);
 	::Ellipse(hdc, rcw.left, rcw.top, rcw.right, rcw.bottom);
 }
 
@@ -1044,8 +1046,8 @@ using TextPositionsI = VarBuffer<int, stackBufferLength>;
 
 void SurfaceGDI::DrawTextCommon(PRectangle rc, const Font &font_, XYPOSITION ybase, std::string_view text, UINT fuOptions) {
 	SetFont(font_);
-	const RECT rcw = RectFromPRectangle(rc);
-	const int x = static_cast<int>(rc.left);
+	const RECT rcw = RectFromPRectangleEx(rc);
+	const int x = static_cast<int>(rcw.left);
 	const int yBaseInt = static_cast<int>(ybase);
 
 	if (unicodeMode) {
@@ -1527,9 +1529,7 @@ void SurfaceD2D::FillRectangle(PRectangle rc, Surface &surfacePattern) {
 		hr = pRenderTarget->CreateBitmapBrush(pBitmap, brushProperties, &pBitmapBrush);
 		ReleaseUnknown(pBitmap);
 		if (SUCCEEDED(hr) && pBitmapBrush) {
-			pRenderTarget->FillRectangle(
-				D2D1::RectF(rc.left, rc.top, rc.right, rc.bottom),
-				pBitmapBrush);
+			pRenderTarget->FillRectangle(RectangleFromPRectangle(rc), pBitmapBrush);
 			ReleaseUnknown(pBitmapBrush);
 		}
 	}
@@ -2333,7 +2333,7 @@ void Window::Destroy() noexcept {
 PRectangle Window::GetPosition() const noexcept {
 	RECT rc;
 	::GetWindowRect(HwndFromWindowID(wid), &rc);
-	return PRectangle::FromInts(rc.left, rc.top, rc.right, rc.bottom);
+	return PRectangleFromRectEx(rc);
 }
 
 void Window::SetPosition(PRectangle rc) noexcept {
@@ -2369,7 +2369,7 @@ void Window::SetPositionRelative(PRectangle rc, const Window *relativeTo) noexce
 		::ClientToScreen(HwndFromWindow(*relativeTo), &ptOther);
 		rc.Move(static_cast<XYPOSITION>(ptOther.x), static_cast<XYPOSITION>(ptOther.y));
 
-		const RECT rcMonitor = RectFromPRectangle(rc);
+		const RECT rcMonitor = RectFromPRectangleEx(rc);
 
 		HMONITOR hMonitor = MonitorFromRect(&rcMonitor, MONITOR_DEFAULTTONEAREST);
 		// If hMonitor is NULL, that's just the main screen anyways.
@@ -2397,7 +2397,7 @@ PRectangle Window::GetClientPosition() const noexcept {
 	RECT rc {};
 	if (wid)
 		::GetClientRect(HwndFromWindowID(wid), &rc);
-	return PRectangle::FromInts(rc.left, rc.top, rc.right, rc.bottom);
+	return PRectangleFromRectEx(rc);
 }
 
 void Window::Show(bool show) const noexcept {
@@ -2412,7 +2412,7 @@ void Window::InvalidateAll() noexcept {
 }
 
 void Window::InvalidateRectangle(PRectangle rc) noexcept {
-	const RECT rcw = RectFromPRectangle(rc);
+	const RECT rcw = RectFromPRectangleEx(rc);
 	::InvalidateRect(HwndFromWindowID(wid), &rcw, FALSE);
 }
 
@@ -2986,7 +2986,7 @@ void ListBoxX::SetList(const char *list, const char separator, const char typese
 }
 
 void ListBoxX::AdjustWindowRect(PRectangle *rc, UINT dpi) noexcept {
-	RECT rcw = RectFromPRectangle(*rc);
+	RECT rcw = RectFromPRectangleEx(*rc);
 #if LISTBOXX_USE_THICKFRAME
 	AdjustWindowRectForDpi(&rcw, WS_THICKFRAME, WS_EX_WINDOWEDGE, dpi);
 #elif LISTBOXX_USE_BORDER
@@ -2994,7 +2994,7 @@ void ListBoxX::AdjustWindowRect(PRectangle *rc, UINT dpi) noexcept {
 #else
 	AdjustWindowRectForDpi(&rcw, 0, WS_EX_WINDOWEDGE, dpi);
 #endif
-	*rc = PRectangle::FromInts(rcw.left, rcw.top, rcw.right, rcw.bottom);
+	*rc = PRectangleFromRectEx(rcw);
 #if LISTBOXX_USE_FAKE_FRAME
 	*rc = rc->Inflate(ListBoxXFakeFrameSize, ListBoxXFakeFrameSize);
 #endif

scintilla/win32/PlatWin.h
@@ -70,12 +70,12 @@
 #else
 #define NP2_HAS_GETDPIFORWINDOW					0
 #if NP2_FORCE_COMPILE_C_AS_CPP
-#define NP2_noexcept noexcept
+#define NP2F_noexcept noexcept
 extern UINT GetWindowDPI(HWND hwnd) noexcept;
 extern int SystemMetricsForDpi(int nIndex, UINT dpi) noexcept;
 extern BOOL AdjustWindowRectForDpi(LPRECT lpRect, DWORD dwStyle, DWORD dwExStyle, UINT dpi) noexcept;
 #else
-#define NP2_noexcept
+#define NP2F_noexcept
 extern "C" UINT GetWindowDPI(HWND hwnd);
 extern "C" int SystemMetricsForDpi(int nIndex, UINT dpi);
 extern "C" BOOL AdjustWindowRectForDpi(LPRECT lpRect, DWORD dwStyle, DWORD dwExStyle, UINT dpi);
@@ -92,6 +92,45 @@ constexpr RECT RectFromPRectangle(PRectangle prc) noexcept {
 		static_cast<LONG>(prc.right), static_cast<LONG>(prc.bottom) };
 	return rc;
 }
+
+constexpr PRectangle PRectangleFromRect(RECT rc) noexcept {
+	return PRectangle::FromInts(rc.left, rc.top, rc.right, rc.bottom);
+}
+
+#if NP2_USE_SSE2
+inline PRectangle PRectangleFromRectEx(RECT rc) noexcept {
+	PRectangle prc;
+	__m128i i32x4 = _mm_load_si128((__m128i *)(&rc));
+	__m128 f32x4 = _mm_cvtepi32_ps(i32x4);
+	_mm_store_ps((float *)(&prc), f32x4);
+	return prc;
+}
+
+inline RECT RectFromPRectangleEx(const PRectangle *prc) noexcept {
+	RECT rc;
+	__m128 f32x4 = _mm_load_ps((float *)(prc));
+	__m128i i32x4 = _mm_cvtps_epi32(f32x4);
+	_mm_store_si128((__m128i *)(&rc), i32x4);
+	return rc;
+}
+
+inline RECT RectFromPRectangleEx(PRectangle prc) noexcept {
+	return RectFromPRectangleEx(&prc);
+}
+
+#else
+constexpr PRectangle PRectangleFromRectEx(RECT rc) noexcept {
+	return PRectangleFromRect(rc);
+}
+
+constexpr RECT RectFromPRectangleEx(PRectangle prc) noexcept {
+	return RectFromPRectangle(prc);
+}
+
+constexpr RECT RectFromPRectangleEx(const PRectangle *prc) noexcept {
+	return RectFromPRectangle(*prc);
+}
+#endif
 
 constexpr POINT POINTFromPoint(Point pt) noexcept {
 	return POINT { static_cast<LONG>(pt.x), static_cast<LONG>(pt.y) };

scintilla/win32/ScintillaWin.cxx
@@ -52,6 +52,7 @@ Used by VSCode, Atom etc.
 #define Enable_ChromiumWebCustomMIMEDataFormat	0
 
 #include "Platform.h"
+#include "VectorISA.h"
 
 #include "ILoader.h"
 #include "ILexer.h"
@@ -910,7 +911,7 @@ bool BoundsContains(PRectangle rcBounds, HRGN hRgnBounds, PRectangle rcCheck) no
 			contains = false;
 		} else if (hRgnBounds) {
 			// In bounding rectangle so check more accurately using region
-			const RECT rcw = RectFromPRectangle(rcCheck);
+			const RECT rcw = RectFromPRectangleEx(rcCheck);
 			HRGN hRgnCheck = ::CreateRectRgnIndirect(&rcw);
 			if (hRgnCheck) {
 				HRGN hRgnDifference = ::CreateRectRgn(0, 0, 0, 0);
@@ -1067,7 +1068,7 @@ sptr_t ScintillaWin::WndPaint() {
 	hRgnUpdate = ::CreateRectRgn(0, 0, 0, 0);
 	::GetUpdateRgn(MainHWND(), hRgnUpdate, FALSE);
 	::BeginPaint(MainHWND(), &ps);
-	rcPaint = PRectangle::FromInts(ps.rcPaint.left, ps.rcPaint.top, ps.rcPaint.right, ps.rcPaint.bottom);
+	rcPaint = PRectangleFromRectEx(ps.rcPaint);
 	const PRectangle rcClient = GetClientRectangle();
 	paintingAllText = BoundsContains(rcPaint, hRgnUpdate, rcClient);
 	if (!PaintDC(ps.hdc)) {

scintilla/include/Platform.h
@@ -141,7 +141,7 @@ typedef void *IdlerID;
  * A geometric point class.
  * Point is similar to the Win32 POINT and GTK GdkPoint types.
  */
-class Point {
+class Point final {
 public:
 	XYPOSITION x;
 	XYPOSITION y;
@@ -172,7 +172,7 @@ public:
  * PRectangle is similar to Win32 RECT.
  * PRectangles contain their top and left sides, but not their right and bottom sides.
  */
-class PRectangle {
+class PRectangle final {
 public:
 	XYPOSITION left;
 	XYPOSITION top;

commit 2fbbe686829ae17f5238255885a837407fd4cf34

Fix Notepad2.exe built by Clang crashed on startup, introduced by 5297846df0bd1c9db903b2f0cbe0edc083a01bf2.

scintilla/win32/PlatWin.cxx
@@ -1116,8 +1116,9 @@ void SurfaceGDI::MeasureWidths(const Font &font_, std::string_view text, XYPOSIT
 			if (byteCount == 4) {	// Non-BMP
 				ui++;
 			}
+			const XYPOSITION pos = static_cast<XYPOSITION>(poses.buffer[ui]);
 			for (unsigned int bytePos = 0; (bytePos < byteCount) && (i < len); bytePos++) {
-				positions[i++] = static_cast<XYPOSITION>(poses.buffer[ui]);
+				positions[i++] = pos;
 			}
 		}
 	} else {
@@ -2270,8 +2271,9 @@ XYPOSITION SurfaceD2D::AverageCharWidth(const Font &font_) {
 	if (pIDWriteFactory && pTextFormat) {
 		// Create a layout
 		IDWriteTextLayout *pTextLayout = nullptr;
-		const WCHAR wszAllAlpha[] = L"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		static const WCHAR wszAllAlpha[] = L"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		constexpr size_t lenAllAlpha = (sizeof(wszAllAlpha) / sizeof(WCHAR)) - 1;
+		static_assert(lenAllAlpha == 52);
 		const HRESULT hr = pIDWriteFactory->CreateTextLayout(wszAllAlpha, static_cast<UINT32>(lenAllAlpha),
 			pTextFormat, 1000.0, 1000.0, &pTextLayout);
 		if (SUCCEEDED(hr) && pTextLayout) {

scintilla/win32/PlatWin.h
@@ -102,7 +102,7 @@ inline PRectangle PRectangleFromRectEx(RECT rc) noexcept {
 	PRectangle prc;
 	__m128i i32x4 = _mm_load_si128((__m128i *)(&rc));
 	__m128 f32x4 = _mm_cvtepi32_ps(i32x4);
-	_mm_store_ps((float *)(&prc), f32x4);
+	_mm_storeu_ps((float *)(&prc), f32x4);
 	return prc;
 }
 
@@ -110,7 +110,7 @@ inline RECT RectFromPRectangleEx(const PRectangle *prc) noexcept {
 	RECT rc;
 	__m128 f32x4 = _mm_load_ps((float *)(prc));
 	__m128i i32x4 = _mm_cvtps_epi32(f32x4);
-	_mm_store_si128((__m128i *)(&rc), i32x4);
+	_mm_storeu_si128((__m128i *)(&rc), i32x4);
 	return rc;
 }
 

scintilla/win32/ScintillaWin.cxx
@@ -122,7 +122,7 @@ using namespace Scintilla;
 
 namespace {
 
-const TCHAR *callClassName = L"CallTip";
+constexpr const TCHAR *callClassName = L"CallTip";
 
 inline void SetWindowID(HWND hWnd, int identifier) noexcept {
 	::SetWindowLongPtr(hWnd, GWLP_ID, identifier);