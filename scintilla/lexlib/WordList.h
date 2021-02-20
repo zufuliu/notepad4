fa8e6925a579cc21e32bf0ca759129093a6826e1
zufuliu <zufuliu@gmail.com>
2021-02-20 23:07:53 +0800

    optimize ranges in WordList.

--- scintilla/lexlib/WordList.h
+++ scintilla/lexlib/WordList.h
@@ -11,16 +11,27 @@ namespace Scintilla {
 /**
  */
 class WordList final {
+public:
+	using range_t = unsigned int;
+private:
 	// Each word contains at least one character - a empty word acts as sentinel at the end.
 	char **words;
 	char *list;
-	int len;
-	// words in [start, end) starts with same character.
+	range_t len;
+	// words in [start, end) starts with same character, maximum word count limited to 0xffff.
 	struct Range {
-		int start;
-		int end;
+		range_t start;
+		const range_t end;
+		explicit constexpr Range(range_t range) noexcept : start(range & 0xffff), end(range >> 16) {}
+		constexpr range_t Length() const noexcept {
+			return end - start;
+		}
+		bool Next() noexcept {
+			++start;
+			return start < end;
+		}
 	};
-	Range ranges[128];	// only ASCII, most word starts with character in '_a-zA-Z'
+	range_t ranges[128];	// only ASCII, most word starts with character in '_a-zA-Z'
 public:
 	explicit WordList() noexcept;
 	~WordList();
@@ -29,14 +40,14 @@ public:
 	bool operator==(const WordList &other) const noexcept {
 		return !(*this != other);
 	}
-	int Length() const noexcept;
+	range_t Length() const noexcept;
 	void Clear() noexcept;
 	bool Set(const char *s, bool toLower);
 	bool InList(const char *s) const noexcept;
 	bool InListPrefixed(const char *s, char marker) const noexcept;
 	bool InListAbbreviated(const char *s, char marker) const noexcept;
 	bool InListAbridged(const char *s, char marker) const noexcept;
-	const char *WordAt(int n) const noexcept;
+	const char *WordAt(range_t n) const noexcept;
 };
 
 }
