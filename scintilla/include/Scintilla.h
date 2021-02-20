8262ebc8444322f4a40b572e4af17a918b9369f6
zufuliu <zufuliu@gmail.com>
2021-02-20 22:41:12 +0800

    Remove lexer messages that does nothing in Scintilla 5.

--- scintilla/include/Scintilla.h
+++ scintilla/include/Scintilla.h
@@ -1043,8 +1043,6 @@ typedef sptr_t (*SciFnDirect)(sptr_t ptr, unsigned int iMessage, uptr_t wParam, 
 #define KEYWORDSET_INDEXMASK 31
 #define KEYWORDSET_TOLOWER 32
 #define SCI_SETKEYWORDS 4005
-#define SCI_SETLEXERLANGUAGE 4006
-#define SCI_LOADLEXERLIBRARY 4007
 #define SCI_GETPROPERTY 4008
 #define SCI_GETPROPERTYEXPANDED 4009
 #define SCI_GETPROPERTYINT 4010
