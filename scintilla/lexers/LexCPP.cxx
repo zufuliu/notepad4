zufuliu/notepad4

commit 085ae2daf2bb0c537276f86b2ade22d5d4410e71

Code refactoring, fix C++ code folding bug for document comment.

scintilla/lexers/LexCPP.cxx
@@ -861,21 +861,16 @@ static void ColouriseCppDoc(Sci_PositionU startPos, Sci_Position length, int ini
 						sc.Forward();
 					} else if (sc.ch == '`' && (lexType == LEX_D)) {
 						sc.SetState(SCE_C_DSTRINGB);
-					} else if (!SharpComment(lexType) && sc.Match('/', '*')) {
-						if (visibleChars == 0 && styler.MatchAny(sc.currentPos + 2, '*', '!')) {
-							sc.SetState(SCE_C_COMMENTDOC);
-						} else {
-							sc.SetState(SCE_C_COMMENT);
+					} else if (!SharpComment(lexType) && sc.ch == '/' && (sc.chNext == '/' || sc.chNext == '*')) {
+						const int chNext = sc.chNext;
+						sc.SetState((chNext == '/') ? SCE_C_COMMENTLINE : SCE_C_COMMENT);
+						sc.Forward(2);
+						if (sc.ch == '!' || (sc.ch == chNext && sc.chNext != chNext)) {
+							sc.ChangeState((chNext == '/') ? SCE_C_COMMENTLINEDOC : SCE_C_COMMENTDOC);
 						}
-						sc.Forward();
+						continue;
 					} else if ((SharpComment(lexType) || lexType == LEX_PHP) && sc.ch == '#') {
 						sc.SetState(SCE_C_COMMENTLINE);
-					} else if (!SharpComment(lexType) && sc.Match('/', '/')) {
-						const char chNext = (visibleChars == 0) ? styler.SafeGetCharAt(sc.currentPos + 2) : '\0';
-						if (chNext == '!' || (chNext == '/' && styler.SafeGetCharAt(sc.currentPos + 3) != '/'))
-							sc.SetState(SCE_C_COMMENTLINEDOC);
-						else
-							sc.SetState(SCE_C_COMMENTLINE);
 					} else if (sc.ch == '$' && (lexType == LEX_JAM && sc.chNext == '(')) {
 						++numDTSBrace;
 						if (sc.chNext == '(')
@@ -1161,8 +1156,10 @@ static bool IsCppFoldingLine(Sci_Line line, LexAccessor &styler, int kind) noexc
 #define IsUnDefLine(line)		IsCppFoldingLine(line, styler, 5)
 
 static constexpr bool IsStreamCommentStyle(int style) noexcept {
-	return style == SCE_C_COMMENT || style == SCE_C_COMMENTDOC
-		|| style == SCE_C_COMMENTDOC_TAG || style == SCE_C_COMMENTDOC_TAG_XML;
+	return style == SCE_C_COMMENT || style == SCE_C_COMMENTDOC;
+}
+static constexpr bool IsInnerCommentStyle(int style) noexcept {
+	return style == SCE_C_COMMENTDOC_TAG || style == SCE_C_COMMENTDOC_TAG_XML;	
 }
 static constexpr bool IsHear_NowDocStyle(int style) noexcept {
 	return style == SCE_C_HEREDOC || style == SCE_C_NOWDOC;
@@ -1235,9 +1232,9 @@ static void FoldCppDoc(Sci_PositionU startPos, Sci_Position length, int initStyl
 				}
 			}
 			else if (IsStreamCommentStyle(style)) {
-				if (!IsStreamCommentStyle(stylePrev)) {
+				if (style != stylePrev && !IsInnerCommentStyle(stylePrev)) {
 					levelNext++;
-				} else if (!IsStreamCommentStyle(styleNext) && !atEOL) {
+				} else if (style != styleNext && !IsInnerCommentStyle(styleNext)) {
 					levelNext--;
 				}
 			}
@@ -1249,17 +1246,10 @@ static void FoldCppDoc(Sci_PositionU startPos, Sci_Position length, int initStyl
 				}
 			}
 		}
-		if (lexType == LEX_PHP && IsHear_NowDocStyle(style)) {
-			if (!IsHear_NowDocStyle(stylePrev)) {
-				levelNext++;
-			} else if (!IsHear_NowDocStyle(styleNext) && !atEOL) {
-				levelNext--;
-			}
-		}
-		if (style == SCE_C_TRIPLEVERBATIM || style == SCE_C_VERBATIM) {
+		if (style == SCE_C_TRIPLEVERBATIM || style == SCE_C_VERBATIM || (lexType == LEX_PHP && IsHear_NowDocStyle(style))) {
 			if (stylePrev != style) {
 				levelNext++;
-			} else if (styleNext != style && !atEOL) {
+			} else if (styleNext != style) {
 				levelNext--;
 			}
 		}

scintilla/lexers/LexDart.cxx
@@ -287,8 +287,8 @@ void ColouriseDartDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initSt
 				visibleCharsBefore = visibleChars;
 				const int chNext = sc.chNext;
 				sc.SetState((chNext == '/') ? SCE_DART_COMMENTLINE : SCE_DART_COMMENTBLOCK);
-				sc.Forward();
-				if (chNext == sc.chNext && sc.GetRelative(2) != chNext) {
+				sc.Forward(2);
+				if (sc.ch == chNext && sc.chNext != chNext) {
 					sc.ChangeState((chNext == '/') ? SCE_DART_COMMENTLINEDOC : SCE_DART_COMMENTBLOCKDOC);
 				}
 				if (chNext == '/') {
@@ -298,6 +298,7 @@ void ColouriseDartDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initSt
 				 } else {
 					commentLevel = 1;
 				 }
+				 continue;
 			} else if (sc.ch == 'r' && (sc.chNext == '\'' || sc.chNext == '"')) {
 				sc.SetState((sc.chNext == '\'') ? SCE_DART_RAWSTRING_SQ : SCE_DART_RAWSTRING_DQ);
 				sc.Forward(2);

scintilla/lexers/LexJava.cxx
@@ -323,10 +323,11 @@ void ColouriseJavaDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initSt
 				visibleCharsBefore = visibleChars;
 				docTagState = DocTagState::None;
 				sc.SetState(SCE_JAVA_COMMENTBLOCK);
-				sc.Forward();
-				if (sc.chNext == '*' && sc.GetRelative(2) != '*') {
+				sc.Forward(2);
+				if (sc.ch == '*' && sc.chNext != '*') {
 					sc.ChangeState(SCE_JAVA_COMMENTBLOCKDOC);
 				}
+				continue;
 			} else if (sc.Match('"', '"', '"')) {
 				sc.SetState(SCE_JAVA_TRIPLE_STRING);
 				sc.Forward(2);

scintilla/lexers/LexJavaScript.cxx
@@ -453,8 +453,8 @@ void ColouriseJsDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initStyl
 					visibleCharsBefore = visibleChars;
 					const int chNext = sc.chNext;
 					sc.SetState((chNext == '/') ? SCE_JS_COMMENTLINE : SCE_JS_COMMENTBLOCK);
-					sc.Forward();
-					if (sc.chNext == '!' || (chNext == sc.chNext && sc.GetRelative(2) != chNext)) {
+					sc.Forward(2);
+					if (sc.ch == '!' || (sc.ch == chNext && sc.chNext != chNext)) {
 						sc.ChangeState((chNext == '/') ? SCE_JS_COMMENTLINEDOC : SCE_JS_COMMENTBLOCKDOC);
 					}
 					if (chNext == '/') {
@@ -462,6 +462,7 @@ void ColouriseJsDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initStyl
 							lineStateLineType = JsLineStateMaskLineComment;
 						}
 					}
+					continue;
 				} else if (!IsEOLChar(sc.chNext) && IsRegexStart(chPrevNonWhite, stylePrevNonWhite)) {
 					insideRegexRange = false;
 					sc.SetState(SCE_JS_REGEX);

scintilla/lexers/LexKotlin.cxx
@@ -282,8 +282,8 @@ void ColouriseKotlinDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int init
 				visibleCharsBefore = visibleChars;
 				const int chNext = sc.chNext;
 				sc.SetState((chNext == '/') ? SCE_KOTLIN_COMMENTLINE : SCE_KOTLIN_COMMENTBLOCK);
-				sc.Forward();
-				if (sc.chNext == '!' || (chNext == sc.chNext && sc.GetRelative(2) != chNext)) {
+				sc.Forward(2);
+				if (sc.ch == '!' || (sc.ch == chNext && sc.chNext != chNext)) {
 					sc.ChangeState((chNext == '/') ? SCE_KOTLIN_COMMENTLINEDOC : SCE_KOTLIN_COMMENTBLOCKDOC);
 				}
 				if (chNext == '/') {
@@ -293,6 +293,7 @@ void ColouriseKotlinDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int init
 				} else {
 					commentLevel = 1;
 				}
+				continue;
 			} else if (sc.Match('"', '"', '"')) {
 				sc.SetState(SCE_KOTLIN_RAWSTRINGSTART);
 				sc.Forward(2);

scintilla/lexers/LexRust.cxx
@@ -294,8 +294,8 @@ void ColouriseRustDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initSt
 				visibleCharsBefore = visibleChars;
 				const int chNext = sc.chNext;
 				sc.SetState((chNext == '/') ? SCE_RUST_COMMENTLINE : SCE_RUST_COMMENTBLOCK);
-				sc.Forward();
-				if (sc.chNext == '!' || (chNext == sc.chNext && sc.GetRelative(2) != chNext)) {
+				sc.Forward(2);
+				if (sc.ch == '!' || (sc.ch == chNext && sc.chNext != chNext)) {
 					sc.ChangeState((chNext == '/') ? SCE_RUST_COMMENTLINEDOC : SCE_RUST_COMMENTBLOCKDOC);
 				}
 				if (chNext == '/') {
@@ -305,6 +305,7 @@ void ColouriseRustDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initSt
 				} else {
 					commentLevel = 1;
 				}
+				continue;
 			} else if (sc.ch == '\"') {
 				sc.SetState(SCE_RUST_STRING);
 			} else if (sc.ch == '\'') {

scintilla/lexers/LexSwift.cxx
@@ -301,8 +301,8 @@ void ColouriseSwiftDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initS
 				visibleCharsBefore = visibleChars;
 				const int chNext = sc.chNext;
 				sc.SetState((chNext == '/') ? SCE_SWIFT_COMMENTLINE : SCE_SWIFT_COMMENTBLOCK);
-				sc.Forward();
-				if (sc.chNext == ':' || sc.chNext == '!' || (chNext == sc.chNext && sc.GetRelative(2) != chNext)) {
+				sc.Forward(2);
+				if (sc.ch == ':' || sc.ch == '!' || (sc.ch == chNext && sc.chNext != chNext)) {
 					sc.ChangeState((chNext == '/') ? SCE_SWIFT_COMMENTLINEDOC : SCE_SWIFT_COMMENTBLOCKDOC);
 				}
 				if (chNext == '/') {
@@ -312,6 +312,7 @@ void ColouriseSwiftDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initS
 				} else {
 					commentLevel = 1;
 				}
+				continue;
 			} else if (sc.Match('"', '"', '"')) {
 				sc.SetState(SCE_SWIFT_TRIPLE_STRINGSTART);
 				sc.Forward(2);

scintilla/scripts/GenerateCaseConvert.py
@@ -526,11 +526,7 @@ static inline BOOL IsCharacterCaseSensitiveSecond(uint32_t ch) {{
 	uint32_t block = ch >> {blockSizeBit + 5};
 	uint32_t index = UnicodeCaseSensitivityIndex[block & {hex(blockIndexCount - 1)}];
 	block = index ^ (block >> {blockIndexValueBit - indexBitCount});
-#if 0//defined(__AVX2__)
-	index &= _bextr_u32(block - {hex(indexMask + 1)}, 8, {indexBitCount});
-#else
 	index &= ((block - {hex(indexMask + 1)}) >> 8) & {hex(indexMask)};
-#endif
 	if (index) {{
 		ch = ch & {hex(blockSize*32 - 1)};
 		index = {indexOffset} + (index << {blockSizeBit});

commit f8273f8d19d953c5b7d3158097efe2aa36aa908b

Fix LexCPP.cxx block comment broken, introduced by 085ae2daf2bb0c537276f86b2ade22d5d4410e71.

scintilla/lexers/LexCPP.cxx
@@ -169,7 +169,7 @@ static void ColouriseCppDoc(Sci_PositionU startPos, Sci_Position length, int ini
 		sc.Forward();
 	}
 
-	for (; sc.More(); sc.Forward()) {
+	while (sc.More()) {
 
 		if (sc.atLineStart) {
 			if (sc.state == SCE_C_STRING || sc.state == SCE_C_CHARACTER) {
@@ -537,10 +537,9 @@ static void ColouriseCppDoc(Sci_PositionU startPos, Sci_Position length, int ini
 			break;
 		case SCE_C_STRING:
 			if (sc.atLineEnd) {
-				if (lexType == LEX_ASY || lexType == LEX_PHP) {
-					continue;
+				if (!(lexType == LEX_ASY || lexType == LEX_PHP)) {
+					sc.ChangeState(SCE_C_STRINGEOL);
 				}
-				sc.ChangeState(SCE_C_STRINGEOL);
 			} else if (isIncludePreprocessor && sc.ch == '>') {
 				outerStyle = SCE_C_DEFAULT;
 				sc.ForwardSetState(SCE_C_DEFAULT);
@@ -777,7 +776,7 @@ static void ColouriseCppDoc(Sci_PositionU startPos, Sci_Position length, int ini
 				sc.Forward(5);
 				sc.SetState(SCE_C_DEFAULT);
 			}
-		} else
+		} else {
 			if (sc.state == SCE_C_DEFAULT) {
 				if (lexType == LEX_PHP && sc.Match("<?php")) {
 					sc.SetState(SCE_C_XML_TAG);
@@ -977,6 +976,7 @@ static void ColouriseCppDoc(Sci_PositionU startPos, Sci_Position length, int ini
 						}
 					}
 			}
+		}
 
 		// Handle line continuation generically.
 		if (sc.ch == '\\') {
@@ -986,6 +986,7 @@ static void ColouriseCppDoc(Sci_PositionU startPos, Sci_Position length, int ini
 				if (sc.ch == '\r' && sc.chNext == '\n') {
 					sc.Forward();
 				}
+				sc.Forward();
 				continuationLine = true;
 				continue;
 			}
@@ -996,6 +997,7 @@ static void ColouriseCppDoc(Sci_PositionU startPos, Sci_Position length, int ini
 			visibleChars++;
 		}
 		continuationLine = false;
+		sc.Forward();
 	}
 
 	sc.Complete();