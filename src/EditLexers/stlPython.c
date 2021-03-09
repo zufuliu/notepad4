ab18270d96c34aa6de059cd1ba90eba9b707272c
zufuliu <zufuliu@gmail.com>
2021-03-09 21:27:16 +0800

    Add keywords for Python 3.10.

--- src/EditLexers/stlPython.c
+++ src/EditLexers/stlPython.c
@@ -13,6 +13,7 @@ static KEYWORDLIST Keywords_Python = {{
 "async await " // python 3.7
 "and as assert break class continue def del elif else except finally for from "
 "global if import in is lambda nonlocal not or pass raise return try while with yield "
+"match case " // python 3.10
 // python 2
 "exec print "
 
