2504cdd435335959781bb8c18d4b473f42fe8ebb
zufuliu <zufuliu@gmail.com>
2021-03-28 19:36:55 +0800

    Remove indentation based code folding for ABAQUS and APDL (code added by 9e566d2c2e8f4f983fb6f1ea1c27be84097f510b), issue #277.

--- scintilla/lexers/LexAPDL.cxx
+++ scintilla/lexers/LexAPDL.cxx
@@ -22,25 +22,16 @@ using namespace Scintilla;
 
 namespace {
 
-#define FOLD_APDL_BY_INDENTATION	0
-
 enum {
 	APDLLineStateLineNone = 0,
 	APDLLineStateLineComment = 1,
-#if FOLD_APDL_BY_INDENTATION
-	APDLLineStateEmptyLine = 2,
-#else
 	APDLLineStateDataLine = 2,
-#endif
 };
 
 void ColouriseAPDLDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initStyle, LexerWordList keywordLists, Accessor &styler) {
 	const bool apdl = styler.GetPropertyInt("lexer.apdl") & true;
 
 	int visibleChars = 0;
-#if FOLD_APDL_BY_INDENTATION
-	int indentCount = 0;
-#endif
 	int lineStateLineType = APDLLineStateLineNone;
 	int stringStartChar = 0;
 	bool commandLine = false;
@@ -136,11 +127,9 @@ void ColouriseAPDLDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initSt
 				}
 			} else if (IsNumberStart(sc.ch, sc.chNext)) {
 				sc.SetState(SCE_APDL_NUMBER);
-#if !FOLD_APDL_BY_INDENTATION
 				if (visibleChars == 0) {
 					lineStateLineType = APDLLineStateDataLine;
 				}
-#endif
 				commandValue = 0;
 			} else if (sc.ch == '"' || sc.ch == '\'') {
 				sc.SetState(SCE_APDL_STRING);
@@ -154,35 +143,16 @@ void ColouriseAPDLDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initSt
 				sc.SetState((visibleChars == 0) ? SCE_APDL_COMMAND : SCE_APDL_IDENTIFIER);
 			} else if (isoperator(sc.ch)) {
 				sc.SetState(SCE_APDL_OPERATOR);
-#if !FOLD_APDL_BY_INDENTATION
 				if (sc.ch == '-' && visibleChars == 0 && (IsADigit(sc.chNext) || sc.chNext == '.')) {
 					lineStateLineType = APDLLineStateDataLine;
 				}
-#endif
 			}
 		}
 
-#if FOLD_APDL_BY_INDENTATION
-		if (visibleChars == 0) {
-			if (sc.ch == ' ' || sc.ch == '\t') {
-				++indentCount;
-			} else if (!isspacechar(sc.ch)) {
-				++visibleChars;
-			}
-		}
-#else
 		if (visibleChars == 0 && !isspacechar(sc.ch)) {
 			visibleChars++;
 		}
-#endif
 		if (sc.atLineEnd) {
-#if FOLD_APDL_BY_INDENTATION
-			if (visibleChars == 0 && lineStateLineType == APDLLineStateLineNone) {
-				lineStateLineType = APDLLineStateEmptyLine;
-			}
-			lineStateLineType |= indentCount << 8;
-			indentCount = 0;
-#endif
 			styler.SetLineState(sc.currentLine, lineStateLineType);
 			lineStateLineType = APDLLineStateLineNone;
 			visibleChars = 0;
@@ -195,101 +165,6 @@ void ColouriseAPDLDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initSt
 	sc.Complete();
 }
 
-#if FOLD_APDL_BY_INDENTATION
-constexpr int GetLineType(int lineState) noexcept {
-	return lineState & 3;
-}
-
-struct FoldLineState {
-	int lineType;
-	int indentCount;
-	constexpr explicit FoldLineState(int lineState) noexcept:
-		lineType(GetLineType(lineState)),
-		indentCount(lineState >> 8) {
-	}
-	constexpr bool Empty() const noexcept {
-		return lineType != APDLLineStateLineNone;
-	}
-};
-
-// code folding based on LexYAML
-void FoldAPDLDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int /*initStyle*/, LexerWordList, Accessor &styler) {
-	const Sci_Position maxPos = startPos + lengthDoc;
-	const Sci_Line docLines = styler.GetLine(styler.Length());
-	const Sci_Line maxLines = (maxPos == styler.Length()) ? docLines : styler.GetLine(maxPos - 1);
-
-	Sci_Line lineCurrent = styler.GetLine(startPos);
-	FoldLineState stateCurrent(styler.GetLineState(lineCurrent));
-	while (lineCurrent > 0) {
-		lineCurrent--;
-		stateCurrent = FoldLineState(styler.GetLineState(lineCurrent));
-		if (!stateCurrent.Empty()) {
-			break;
-		}
-	}
-
-	while (lineCurrent <= maxLines) {
-		Sci_Position lineNext = lineCurrent + 1;
-		FoldLineState stateNext = stateCurrent;
-		if (lineNext <= docLines) {
-			stateNext = FoldLineState(styler.GetLineState(lineNext));
-		}
-		if (stateNext.Empty()) {
-			stateNext.indentCount = stateCurrent.indentCount;
-		}
-		while ((lineNext < docLines) && stateNext.Empty()) {
-			lineNext++;
-			stateNext = FoldLineState(styler.GetLineState(lineNext));
-		}
-
-		int lev = stateCurrent.indentCount + SC_FOLDLEVELBASE;
-		if (!stateCurrent.Empty()) {
-			if (stateCurrent.indentCount < stateNext.indentCount) {
-				lev |= SC_FOLDLEVELHEADERFLAG;
-			}
-		}
-
-		styler.SetLevel(lineCurrent, lev);
-		lineCurrent++;
-
-		const int levelAfterBlank = stateNext.indentCount;
-		const int skipLevel = levelAfterBlank + SC_FOLDLEVELBASE;
-
-		if (lineCurrent < lineNext) {
-			int prevLineType = stateCurrent.lineType;
-			int nextLineType = GetLineType(styler.GetLineState(lineCurrent));
-			int prevLevel = skipLevel;
-			// comment on first line
-			if (prevLineType == APDLLineStateLineComment) {
-				nextLineType = prevLineType;
-				prevLineType = APDLLineStateLineNone;
-				--lineCurrent;
-			}
-			for (; lineCurrent < lineNext; lineCurrent++) {
-				int level = skipLevel;
-				const int currentLineType = nextLineType;
-				nextLineType = GetLineType(styler.GetLineState(lineCurrent + 1));
-				if (currentLineType == APDLLineStateLineComment) {
-					if (nextLineType == APDLLineStateLineComment && prevLineType != APDLLineStateLineComment) {
-						level |= SC_FOLDLEVELHEADERFLAG;
-					} else if (prevLevel & SC_FOLDLEVELHEADERFLAG) {
-						level++;
-					} else {
-						level = prevLevel;
-					}
-				}
-
-				styler.SetLevel(lineCurrent, level);
-				prevLineType = currentLineType;
-				prevLevel = level;
-			}
-		}
-
-		stateCurrent = stateNext;
-	}
-}
-
-#else
 struct FoldLineState {
 	int lineComment;
 	int dataLine;
@@ -370,7 +245,6 @@ void FoldAPDLDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int /*initStyle
 		}
 	}
 }
-#endif // FOLD_APDL_BY_INDENTATION
 
 }
 
