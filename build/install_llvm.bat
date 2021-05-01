99d1f44f0abce1d5ce5aa16c3f8d643437dfc77f
zufuliu <zufuliu@gmail.com>
2021-05-01 09:53:54 +0800

    Add AppVeyor build cache for LLVM installer.

--- build/install_llvm.bat
+++ build/install_llvm.bat
@@ -1,12 +1,17 @@
 @ECHO OFF
 @rem used for AppVeyor and GitHub Actions
 
-IF /I "%~1" == "latest" (
-  SHIFT
-  curl -fsSL -o "LLVM-12.0.0-win64.exe" "https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/LLVM-12.0.0-win64.exe"
-  LLVM-12.0.0-win64.exe /S
+IF NOT EXIST tools (
+  MKDIR tools
 )
 
-curl -fsSL -o "LLVM_VS2017.zip" "https://github.com/zufuliu/llvm-utils/releases/download/v20.03/LLVM_VS2017.zip"
-7z x -y "LLVM_VS2017.zip" >NUL
+IF NOT EXIST "tools/LLVM-12.0.0-win64.exe" (
+  curl -fsSL -o "tools/LLVM-12.0.0-win64.exe" "https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/LLVM-12.0.0-win64.exe"
+)
+"tools/LLVM-12.0.0-win64.exe" /S
+
+IF NOT EXIST "tools/LLVM_VS2017-v20.03.zip" (
+  curl -fsSL -o "tools/LLVM_VS2017-v20.03.zip" "https://github.com/zufuliu/llvm-utils/releases/download/v20.03/LLVM_VS2017.zip"
+)
+7z x -y -o"." "tools/LLVM_VS2017-v20.03.zip" >NUL
 CALL "LLVM_VS2017\install.bat" %1
