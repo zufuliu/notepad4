zufuliu/notepad4

commit b0b2f26a557da2c4296e107b04c999dfed297e95

Optimize BreakFinder::Next(), see https://sourceforge.net/p/scintilla/feature-requests/1413/

scintilla/src/PositionCache.cxx
@@ -509,14 +509,42 @@ LineLayout *LineLayoutCache::Retrieve(Sci::Line lineNumber, Sci::Line lineCaret,
 	return ret;
 }
 
+namespace {
+
 // Simply pack the (maximum 4) character bytes into an int
-static constexpr unsigned int KeyFromString(std::string_view charBytes) noexcept {
+#if 0
+constexpr unsigned int KeyFromString(std::string_view charBytes) noexcept {
 	PLATFORM_ASSERT(charBytes.length() <= 4);
 	unsigned int k = 0;
 	for (const unsigned char uc : charBytes) {
 		k = (k << 8) | uc;
 	}
 	return k;
+}
+
+#else
+inline unsigned int KeyFromString(std::string_view charBytes) noexcept {
+	unsigned int k = 0;
+	if (!charBytes.empty()) {
+#if 0//NP2_USE_AVX2
+		k = loadle_u32(charBytes.data());
+		if (charBytes.length() < 4) {
+			k = bit_zero_high_u32(k, charBytes.length()*8);
+		}
+		k = bswap32(k);
+#else
+		k = loadbe_u32(charBytes.data());
+		if (const size_t diff = 4 - charBytes.length()) {
+			k >>= diff*8;
+		}
+#endif
+	}
+	return k;
+}
+#endif
+
+constexpr unsigned int representationKeyCrLf = ('\r' << 8) | '\n';
+
 }
 
 void SpecialRepresentations::SetRepresentation(std::string_view charBytes, std::string_view value) {
@@ -527,6 +555,9 @@ void SpecialRepresentations::SetRepresentation(std::string_view charBytes, std::
 			// New entry so increment for first byte
 			const unsigned char ucStart = charBytes.empty() ? 0 : charBytes[0];
 			startByteHasReprs[ucStart]++;
+			if (key == representationKeyCrLf) {
+				crlf = true;
+			}
 		} else {
 			it->second = Representation(value);
 		}
@@ -560,13 +591,25 @@ void SpecialRepresentations::SetRepresentationColour(std::string_view charBytes,
 
 void SpecialRepresentations::ClearRepresentation(std::string_view charBytes) {
 	if (charBytes.length() <= 4) {
-		const auto it = mapReprs.find(KeyFromString(charBytes));
+		const unsigned int key = KeyFromString(charBytes);
+		const auto it = mapReprs.find(key);
 		if (it != mapReprs.end()) {
 			mapReprs.erase(it);
 			const unsigned char ucStart = charBytes.empty() ? 0 : charBytes[0];
 			startByteHasReprs[ucStart]--;
+			if (key == representationKeyCrLf) {
+				crlf = false;
+			}
 		}
 	}
+}
+
+const Representation *SpecialRepresentations::GetRepresentation(std::string_view charBytes) const {
+	const auto it = mapReprs.find(KeyFromString(charBytes));
+	if (it != mapReprs.end()) {
+		return &(it->second);
+	}
+	return nullptr;
 }
 
 const Representation *SpecialRepresentations::RepresentationFromCharacter(std::string_view charBytes) const {
@@ -595,6 +638,7 @@ void SpecialRepresentations::Clear() noexcept {
 	mapReprs.clear();
 	constexpr unsigned char none = 0;
 	std::fill(startByteHasReprs, std::end(startByteHasReprs), none);
+	crlf = false;
 }
 
 void BreakFinder::Insert(Sci::Position val) {
@@ -668,16 +712,23 @@ TextSegment BreakFinder::Next() {
 		const int prev = nextBreak;
 		while (nextBreak < lineRange.end) {
 			int charWidth = 1;
-			if (encodingFamily == EncodingFamily::unicode)
-				charWidth = UTF8DrawBytes(reinterpret_cast<unsigned char *>(&ll->chars[nextBreak]),
-					static_cast<int>(lineRange.end - nextBreak));
-			else if (encodingFamily == EncodingFamily::dbcs)
-				charWidth = pdoc->DBCSDrawBytes(
-					std::string_view(&ll->chars[nextBreak], lineRange.end - nextBreak));
-			// Special case \r\n line ends if there is a representation
-			if (preprs->Contains("\r\n") && ll->chars[nextBreak] == '\r' && ll->chars[nextBreak + 1] == '\n')
-				charWidth = 2;
-			const Representation *repr = preprs->RepresentationFromCharacter(std::string_view(&ll->chars[nextBreak], charWidth));
+			const char * const chars = &ll->chars[nextBreak];
+			const unsigned char ch = chars[0];
+			if (encodingFamily != EncodingFamily::eightBit && !UTF8IsAscii(ch)) {
+				if (encodingFamily == EncodingFamily::unicode) {
+					charWidth = UTF8DrawBytes(reinterpret_cast<const unsigned char *>(chars), static_cast<int>(lineRange.end - nextBreak));
+				} else {
+					charWidth = pdoc->DBCSDrawBytes(std::string_view(chars, lineRange.end - nextBreak));
+				}
+			}
+			const Representation *repr = nullptr;
+			if (preprs->MayContains(ch)) {
+				// Special case \r\n line ends if there is a representation
+				if (ch == '\r' && preprs->ContainsCrLf() && chars[1] == '\n') {
+					charWidth = 2;
+				}
+				repr = preprs->GetRepresentation(std::string_view(chars, charWidth));
+			}
 			if (((nextBreak > 0) && (ll->styles[nextBreak] != ll->styles[nextBreak - 1])) ||
 				repr ||
 				(nextBreak == saeNext)) {

src/EditAutoC.c
@@ -84,12 +84,17 @@ uint32_t WordList_Order(const void *pWord, uint32_t len) {
 		high <<= (NP2_AUTOC_ORDER_LENGTH - len)*8;
 	}
 
-#else
-	uint32_t high = *((const uint32_t *)pWord);
+#elif NP2_USE_AVX2
+	uint32_t high = loadle_u32(pWord);
 	if (len < NP2_AUTOC_ORDER_LENGTH) {
 		high = bit_zero_high_u32(high, len*8);
 	}
 	high = bswap32(high);
+#else
+	uint32_t high = loadbe_u32(pWord);
+	if (len < NP2_AUTOC_ORDER_LENGTH) {
+		high >>= (NP2_AUTOC_ORDER_LENGTH - len)*8;
+	}
 #endif
 	return high;
 }
@@ -111,13 +116,19 @@ uint32_t WordList_OrderCase(const void *pWord, uint32_t len) {
 		high <<= (NP2_AUTOC_ORDER_LENGTH - len)*8;
 	}
 
-#else
-	uint32_t high = *((const uint32_t *)pWord);
+#elif NP2_USE_AVX2
+	uint32_t high = loadle_u32(pWord);
 	high |= 0x20202020U; // only works for ASCII letters
 	if (len < NP2_AUTOC_ORDER_LENGTH) {
 		high = bit_zero_high_u32(high, len*8);
 	}
 	high = bswap32(high);
+#else
+	uint32_t high = loadbe_u32(pWord);
+	high |= 0x20202020U; // only works for ASCII letters
+	if (len < NP2_AUTOC_ORDER_LENGTH) {
+		high >>= (NP2_AUTOC_ORDER_LENGTH - len)*8;
+	}
 #endif
 	return high;
 }

scintilla/include/VectorISA.h
@@ -200,10 +200,14 @@ static inline uint32_t rotl8(uint32_t x) NP2_noexcept {
 }
 #endif
 
+static inline uint32_t loadle_u32(const void *ptr) NP2_noexcept {
+	return *((const uint32_t *)ptr);
+}
+
 #if NP2_USE_AVX2
 static inline uint32_t loadbe_u32(const void *ptr) NP2_noexcept {
 #if defined(__GNUC__)
-	return __builtin_bswap32(*((const uint32_t *)ptr));
+	return __builtin_bswap32(loadle_u32(ptr));
 #else
 	return _loadbe_i32(ptr);
 #endif
@@ -212,6 +216,11 @@ static inline uint32_t loadbe_u32(const void *ptr) NP2_noexcept {
 #define bit_zero_high_u32(x, index)	_bzhi_u32((x), (index))			// BMI2
 //#define bit_zero_high_u32(x, index)	_bextr_u32((x), 0, (index))		// BMI1
 #else
+
+static inline uint32_t loadbe_u32(const void *ptr) NP2_noexcept {
+	return bswap32(loadle_u32(ptr));
+}
+
 static inline uint32_t bit_zero_high_u32(uint32_t x, uint32_t index) NP2_noexcept {
 	return x & ((1U << index) - 1);
 }

scintilla/src/PositionCache.h
@@ -210,14 +210,22 @@ typedef std::map<unsigned int, Representation> MapRepresentation;
 class SpecialRepresentations {
 	MapRepresentation mapReprs;
 	unsigned char startByteHasReprs[0x100] {};
+	bool crlf = false;
 public:
 	SpecialRepresentations() noexcept {}
 	void SetRepresentation(std::string_view charBytes, std::string_view value);
 	void SetRepresentationAppearance(std::string_view charBytes, RepresentationAppearance appearance);
 	void SetRepresentationColour(std::string_view charBytes, ColourRGBA colour);
 	void ClearRepresentation(std::string_view charBytes);
+	const Representation *GetRepresentation(std::string_view charBytes) const;
 	const Representation *RepresentationFromCharacter(std::string_view charBytes) const;
 	bool Contains(std::string_view charBytes) const;
+	bool ContainsCrLf() const noexcept {
+		return crlf;
+	}
+	bool MayContains(unsigned char ch) const noexcept {
+		return startByteHasReprs[ch] != 0;
+	}
 	void Clear() noexcept;
 };
 

commit e5a3959b2b436c0fb4ec66c89a9c1708c04f2ea5

Fix auto-completion broken after commit b0b2f26a557da2c4296e107b04c999dfed297e95.

scintilla/src/PositionCache.cxx
@@ -526,18 +526,10 @@ constexpr unsigned int KeyFromString(std::string_view charBytes) noexcept {
 inline unsigned int KeyFromString(std::string_view charBytes) noexcept {
 	unsigned int k = 0;
 	if (!charBytes.empty()) {
-#if 0//NP2_USE_AVX2
-		k = loadle_u32(charBytes.data());
-		if (charBytes.length() < 4) {
-			k = bit_zero_high_u32(k, charBytes.length()*8);
-		}
-		k = bswap32(k);
-#else
 		k = loadbe_u32(charBytes.data());
 		if (const size_t diff = 4 - charBytes.length()) {
 			k >>= diff*8;
 		}
-#endif
 	}
 	return k;
 }

src/EditAutoC.c
@@ -84,17 +84,12 @@ uint32_t WordList_Order(const void *pWord, uint32_t len) {
 		high <<= (NP2_AUTOC_ORDER_LENGTH - len)*8;
 	}
 
-#elif NP2_USE_AVX2
+#else
 	uint32_t high = loadle_u32(pWord);
 	if (len < NP2_AUTOC_ORDER_LENGTH) {
 		high = bit_zero_high_u32(high, len*8);
 	}
 	high = bswap32(high);
-#else
-	uint32_t high = loadbe_u32(pWord);
-	if (len < NP2_AUTOC_ORDER_LENGTH) {
-		high >>= (NP2_AUTOC_ORDER_LENGTH - len)*8;
-	}
 #endif
 	return high;
 }
@@ -116,19 +111,13 @@ uint32_t WordList_OrderCase(const void *pWord, uint32_t len) {
 		high <<= (NP2_AUTOC_ORDER_LENGTH - len)*8;
 	}
 
-#elif NP2_USE_AVX2
+#else
 	uint32_t high = loadle_u32(pWord);
 	high |= 0x20202020U; // only works for ASCII letters
 	if (len < NP2_AUTOC_ORDER_LENGTH) {
 		high = bit_zero_high_u32(high, len*8);
 	}
 	high = bswap32(high);
-#else
-	uint32_t high = loadbe_u32(pWord);
-	high |= 0x20202020U; // only works for ASCII letters
-	if (len < NP2_AUTOC_ORDER_LENGTH) {
-		high >>= (NP2_AUTOC_ORDER_LENGTH - len)*8;
-	}
 #endif
 	return high;
 }