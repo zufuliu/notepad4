zufuliu/notepad4

commit a2a101351658f2da6968a67c360355a8e78f7fcd

Update Scintilla to 2021-05-20 8788:a3f36a286769.

src/Styles.c
@@ -417,6 +417,8 @@ enum ANSIArtStyleIndex {
 
 #define IMEIndicatorDefaultColor	RGB(0x10, 0x80, 0x10)
 #define MarkOccurrencesDefaultAlpha	100
+#define SelectionDefaultAlpha		95
+#define CaretLineDefaultAlpha		90
 
 #define	BookmarkImageDefaultColor	RGB(0x40, 0x80, 0x40)
 #define	BookmarkLineDefaultColor	RGB(0, 0xff, 0)
@@ -1665,25 +1667,27 @@ void Style_SetLexer(PEDITLEXER pLexNew, BOOL bLexerChanged) {
 	//! begin Selection
 	szValue = pLexGlobal->Styles[GlobalStyleIndex_Selection].szValue;
 	// never change text color on selecting.
-	SciCall_SetSelFore(FALSE, 0);
-
+	//SciCall_ResetElementColor(SC_ELEMENT_SELECTION_TEXT);
+	//SciCall_ResetElementColor(SC_ELEMENT_SELECTION_ADDITIONAL_TEXT);
+	//SciCall_ResetElementColor(SC_ELEMENT_SELECTION_SECONDARY_TEXT);
+	//SciCall_ResetElementColor(SC_ELEMENT_SELECTION_NO_FOCUS_TEXT);
 	// always set background color
 	if (!Style_StrGetBackColor(szValue, &rgb)) {
 		rgb = GetSysColor(COLOR_HIGHLIGHT);
 	}
-	SciCall_SetSelBack(TRUE, rgb);
-	SciCall_SetElementColor(SC_ELEMENT_LIST_SELECTED_BACK, rgb);
-	if (Style_StrGetForeColor(szValue, &rgb)) {
-		SciCall_SetAdditionalSelBack(rgb);
-	}
-
 	if (!Style_StrGetAlpha(szValue, &iValue)) {
-		iValue = SC_ALPHA_NOALPHA;
+		iValue = SelectionDefaultAlpha;
 	}
-	SciCall_SetSelAlpha(iValue);
-	if (Style_StrGetOutlineAlpha(szValue, &iValue)) {
-		SciCall_SetAdditionalSelAlpha(iValue);
-	}
+	SciCall_SetSelectionLayer(SC_LAYER_OVER_TEXT);
+	SciCall_SetElementColor(SC_ELEMENT_LIST_SELECTED_BACK, rgb);
+	SciCall_SetElementColor(SC_ELEMENT_SELECTION_BACK, ColorAlpha(rgb, iValue));
+	// additional selection
+	Style_StrGetForeColor(szValue, &rgb);
+	Style_StrGetOutlineAlpha(szValue, &iValue);
+	rgb = ColorAlpha(rgb, iValue);
+	SciCall_SetElementColor(SC_ELEMENT_SELECTION_ADDITIONAL_BACK, rgb);
+	SciCall_SetElementColor(SC_ELEMENT_SELECTION_SECONDARY_BACK, rgb);
+	//SciCall_SetElementColor(SC_ELEMENT_SELECTION_NO_FOCUS_BACK, rgb);
 
 	SciCall_SetSelEOLFilled(Style_StrGetEOLFilled(szValue));
 	if (!Style_StrGetSize(szValue, &iValue)) {
@@ -1695,23 +1699,22 @@ void Style_SetLexer(PEDITLEXER pLexNew, BOOL bLexerChanged) {
 	//! begin Whitespace
 	szValue = pLexGlobal->Styles[GlobalStyleIndex_Whitespace].szValue;
 	if (Style_StrGetForeColor(szValue, &rgb)) {
-		SciCall_SetWhitespaceFore(TRUE, rgb);
+		if (!Style_StrGetAlpha(szValue, &iValue)) {
+			iValue = SC_ALPHA_OPAQUE;
+		}
+		SciCall_SetElementColor(SC_ELEMENT_WHITE_SPACE, ColorAlpha(rgb, iValue));
 	} else {
-		SciCall_SetWhitespaceFore(FALSE, 0);
+		SciCall_ResetElementColor(SC_ELEMENT_WHITE_SPACE);
 	}
 	if (Style_StrGetBackColor(szValue, &rgb)) {
-		SciCall_SetWhitespaceBack(TRUE, rgb);
+		SciCall_SetElementColor(SC_ELEMENT_WHITE_SPACE_BACK, ColorAlpha(rgb, SC_ALPHA_OPAQUE));
 	} else {
-		SciCall_SetWhitespaceBack(FALSE, 0);
+		SciCall_ResetElementColor(SC_ELEMENT_WHITE_SPACE_BACK);
 	}
-	if (!Style_StrGetAlpha(szValue, &iValue)) {
-		iValue = SC_ALPHA_NOALPHA;
-	}
-	SciCall_SetWhitespaceForeAlpha(iValue);
 	//! end Whitespace
 
 	//! begin Caret
-	const COLORREF backColor = SciCall_StyleGetBack(STYLE_DEFAULT);
+	COLORREF backColor = SciCall_StyleGetBack(STYLE_DEFAULT);
 	COLORREF foreColor = SciCall_StyleGetFore(STYLE_DEFAULT);
 	SciCall_SetElementColor(SC_ELEMENT_LIST, foreColor);
 	SciCall_SetElementColor(SC_ELEMENT_LIST_BACK, backColor);
@@ -1723,11 +1726,13 @@ void Style_SetLexer(PEDITLEXER pLexNew, BOOL bLexerChanged) {
 	if (!VerifyContrast(rgb, backColor)) {
 		rgb = foreColor;
 	}
-	SciCall_SetCaretFore(rgb);
+	rgb = ColorAlpha(rgb, SC_ALPHA_OPAQUE);
+	SciCall_SetElementColor(SC_ELEMENT_CARET, rgb);
 	// additional caret fore
 	if (Style_StrGetBackColor(szValue, &rgb) && VerifyContrast(rgb, backColor)) {
-		SciCall_SetAdditionalCaretFore(rgb);
+		rgb = ColorAlpha(rgb, SC_ALPHA_OPAQUE);
 	}
+	SciCall_SetElementColor(SC_ELEMENT_CARET_ADDITIONAL, rgb);
 	//! end Caret
 
 	// IME indicator
@@ -1785,16 +1790,20 @@ void Style_SetLexer(PEDITLEXER pLexNew, BOOL bLexerChanged) {
 #endif
 
 		uint64_t iMarkerIDs = CodeFoldingMarkerList;
+		highlightColor = ColorAlpha(highlightColor, SC_ALPHA_OPAQUE);
+		foreColor = ColorAlpha(foreColor, SC_ALPHA_OPAQUE);
+		backColor = ColorAlpha(backColor, SC_ALPHA_OPAQUE);
 		do {
 			const int marker = (int)(iMarkerIDs & 0xff);
-			SciCall_MarkerSetBack(marker, foreColor);
-			SciCall_MarkerSetFore(marker, backColor);
-			SciCall_MarkerSetBackSelected(marker, highlightColor);
+			SciCall_MarkerSetBackTranslucent(marker, foreColor);
+			SciCall_MarkerSetForeTranslucent(marker, backColor);
+			SciCall_MarkerSetBackSelectedTranslucent(marker, highlightColor);
 			iMarkerIDs >>= 8;
 		} while (iMarkerIDs);
 
-		SciCall_MarkerSetFore(SC_MARKNUM_FOLDER, fillColor);
-		SciCall_MarkerSetFore(SC_MARKNUM_FOLDEREND, fillColor);
+		fillColor = ColorAlpha(fillColor, SC_ALPHA_OPAQUE);
+		SciCall_MarkerSetForeTranslucent(SC_MARKNUM_FOLDER, fillColor);
+		SciCall_MarkerSetForeTranslucent(SC_MARKNUM_FOLDEREND, fillColor);
 
 		Style_SetDefaultStyle(GlobalStyleIndex_FoldDispalyText);
 	} // end set folding style
@@ -3110,7 +3119,6 @@ void Style_SetLongLineColors(void) {
 // Style_HighlightCurrentLine()
 //
 void Style_HighlightCurrentLine(void) {
-	SciCall_SetCaretLineVisible(FALSE);
 	if (iHighlightCurrentLine != 0) {
 		LPCWSTR szValue = pLexGlobal->Styles[GlobalStyleIndex_CurrentLine].szValue;
 		// 1: background color, 2: outline frame
@@ -3124,14 +3132,13 @@ void Style_HighlightCurrentLine(void) {
 			}
 
 			SciCall_SetCaretLineFrame(size);
-			SciCall_SetCaretLineBack(rgb);
+			SciCall_SetCaretLineLayer(SC_LAYER_OVER_TEXT);
 
 			int alpha;
 			if (!Style_StrGetAlphaEx(outline, szValue, &alpha)) {
-				alpha = SC_ALPHA_NOALPHA;
+				alpha = CaretLineDefaultAlpha;
 			}
-			SciCall_SetCaretLineBackAlpha(alpha);
-			SciCall_SetCaretLineVisible(TRUE);
+			SciCall_SetElementColor(SC_ELEMENT_CARET_LINE_BACK, ColorAlpha(rgb, alpha));
 		}
 	}
 }
@@ -3181,9 +3188,10 @@ void Style_SetBookmark(void) {
 		sprintf(bookmark_pixmap_color, bookmark_pixmap_color_fmt, iBookmarkImageColor);
 		SciCall_MarkerDefinePixmap(MarkerNumber_Bookmark, bookmark_pixmap);
 #else
-		SciCall_MarkerSetBack(MarkerNumber_Bookmark, iBookmarkImageColor);
+		iBookmarkImageColor = ColorAlpha(iBookmarkImageColor, SC_ALPHA_OPAQUE);
+		SciCall_MarkerSetBackTranslucent(MarkerNumber_Bookmark, iBookmarkImageColor);
 		// set same color to avoid showing edge.
-		SciCall_MarkerSetFore(MarkerNumber_Bookmark, iBookmarkImageColor);
+		SciCall_MarkerSetForeTranslucent(MarkerNumber_Bookmark, iBookmarkImageColor);
 		SciCall_MarkerDefine(MarkerNumber_Bookmark, SC_MARK_VERTICALBOOKMARK);
 #endif
 	} else {
@@ -3195,8 +3203,9 @@ void Style_SetBookmark(void) {
 		if (!Style_StrGetAlpha(szValue, &iBookmarkLineAlpha)) {
 			iBookmarkLineAlpha = BookmarkLineDefaultAlpha;
 		}
-		SciCall_MarkerSetBack(MarkerNumber_Bookmark, iBookmarkLineColor);
-		SciCall_MarkerSetAlpha(MarkerNumber_Bookmark, iBookmarkLineAlpha);
+		iBookmarkLineColor = ColorAlpha(iBookmarkLineColor, iBookmarkLineAlpha);
+		SciCall_MarkerSetBackTranslucent(MarkerNumber_Bookmark, iBookmarkLineColor);
+		SciCall_MarkerSetLayer(MarkerNumber_Bookmark, SC_LAYER_OVER_TEXT);
 		SciCall_MarkerDefine(MarkerNumber_Bookmark, SC_MARK_BACKGROUND);
 	}
 	bBookmarkColorUpdated = FALSE;
@@ -3558,8 +3567,9 @@ BOOL Style_StrGetAlphaEx(BOOL outline, LPCWSTR lpszStyle, int *alpha) {
 
 	if (p != NULL) {
 		p += outline ? CSTRLEN(L"outline:") : CSTRLEN(L"alpha:");
-		if (CRTStrToInt(p, alpha)) {
-			*alpha = clamp_i(*alpha, SC_ALPHA_TRANSPARENT, SC_ALPHA_OPAQUE);
+		int iValue;
+		if (CRTStrToInt(p, &iValue)) {
+			*alpha = clamp_i(iValue, SC_ALPHA_TRANSPARENT, SC_ALPHA_OPAQUE);
 			return TRUE;
 		}
 	}

scintilla/GraphicTest.cpp
@@ -130,6 +130,50 @@ ColourAlpha MixedWith_sse2(ColourAlpha colour, ColourAlpha other) noexcept {
 }
 #endif // MixedWith
 
+#if 0 // MixAlpha
+constexpr unsigned int Mixed(unsigned char a, unsigned char b, double proportion) noexcept {
+	return static_cast<unsigned int>(a + proportion * (b - a));
+}
+
+ColourAlpha MixedWithProportion(ColourAlpha colour, ColourAlpha other) noexcept {
+	const double proportion = other.GetAlpha() / 255.0f;
+	return ColourAlpha(
+		Mixed(colour.GetRed(), other.GetRed(), proportion),
+		Mixed(colour.GetGreen(), other.GetGreen(), proportion),
+		Mixed(colour.GetBlue(), other.GetBlue(), proportion),
+		Mixed(colour.GetAlpha(), other.GetAlpha(), proportion));
+}
+
+ColourAlpha MixAlpha(ColourAlpha colour, ColourAlpha other) noexcept {
+	unsigned int alpha = other.GetAlpha();
+	const unsigned int red = (other.GetRed()*alpha + colour.GetRed()*(255 ^ alpha)) / 255;
+	const unsigned int green = (other.GetGreen()*alpha + colour.GetGreen()*(255 ^ alpha)) / 255;
+	const unsigned int blue = (other.GetBlue()*alpha + colour.GetBlue()*(255 ^ alpha)) / 255;
+	alpha = (alpha*alpha + colour.GetAlpha()*(255 ^ alpha)) / 255;
+	return ColourAlpha(red, green, blue, alpha);
+}
+
+#if NP2_USE_AVX2
+ColourAlpha MixAlpha_sse4(ColourAlpha colour, ColourAlpha other) noexcept {
+	__m128i i16x4Fore = unpack_color_epi16_sse4_si32(other.AsInteger());
+	__m128i i16x4Back = unpack_color_epi16_sse4_si32(colour.AsInteger());
+	__m128i i16x4Alpha = _mm_shufflelo_epi16(i16x4Fore, _MM_SHUFFLE(3, 3, 3, 3));
+	i16x4Fore = mm_alpha_blend_epi16(i16x4Fore, i16x4Back, i16x4Alpha);
+	const uint32_t color = pack_color_epi16_sse2_si32(i16x4Fore);
+	return ColourAlpha(color);
+}
+#endif
+
+ColourAlpha MixAlpha_sse2(ColourAlpha colour, ColourAlpha other) noexcept {
+	__m128i i16x4Fore = unpack_color_epi16_sse2_si32(other.AsInteger());
+	__m128i i16x4Back = unpack_color_epi16_sse2_si32(colour.AsInteger());
+	__m128i i16x4Alpha = _mm_shufflelo_epi16(i16x4Fore, _MM_SHUFFLE(3, 3, 3, 3));
+	i16x4Fore = mm_alpha_blend_epi16(i16x4Fore, i16x4Back, i16x4Alpha);
+	const uint32_t color = pack_color_epi16_sse2_si32(i16x4Fore);
+	return ColourAlpha(color);
+}
+#endif // MixAlpha
+
 #if 0 // AlphaBlend
 ColourAlpha AlphaBlend(ColourAlpha fore, ColourAlpha back, unsigned int alpha) noexcept {
 	const unsigned int red = (fore.GetRed()*alpha + back.GetRed()*(255 ^ alpha)) / 255;
@@ -701,6 +745,17 @@ int __cdecl main() {
 #endif
 	printf("     sse2 %08x\n", MixedWith_sse2(fore, back).AsInteger());
 #endif // MixedWith
+
+#if 0 // MixAlpha
+	const ColourAlpha other = fore;
+	printf("MixAlpha(%08x, %08x, %02x):\n", back, other.AsInteger(), other.GetAlpha());
+	printf("    float %08x\n", MixedWithProportion(back, other).AsInteger());
+	printf("    scale %08x\n", MixAlpha(back, other).AsInteger());
+#if NP2_USE_AVX2
+	printf("     sse4 %08x\n", MixAlpha_sse4(back, other).AsInteger());
+#endif
+	printf("     sse2 %08x\n", MixAlpha_sse2(back, other).AsInteger());
+#endif // MixAlpha
 
 #if 0 // AlphaBlend
 	printf("AlphaBlend(%08x, %08x, %02x):\n", fore, back, alpha);

scintilla/include/Scintilla.h
@@ -165,9 +165,6 @@ typedef sptr_t (*SciFnDirect)(sptr_t ptr, unsigned int iMessage, uptr_t wParam, 
 #define SC_MARKNUM_FOLDEROPEN 31
 #define SC_MASK_FOLDERS 0xFE000000U
 #define SCI_MARKERDEFINE 2040
-#define SCI_MARKERSETFORE 2041
-#define SCI_MARKERSETBACK 2042
-#define SCI_MARKERSETBACKSELECTED 2292
 #define SCI_MARKERSETFORETRANSLUCENT 2294
 #define SCI_MARKERSETBACKTRANSLUCENT 2295
 #define SCI_MARKERSETBACKSELECTEDTRANSLUCENT 2296
@@ -181,7 +178,8 @@ typedef sptr_t (*SciFnDirect)(sptr_t ptr, unsigned int iMessage, uptr_t wParam, 
 #define SCI_MARKERPREVIOUS 2048
 #define SCI_MARKERDEFINEPIXMAP 2049
 #define SCI_MARKERADDSET 2466
-#define SCI_MARKERSETALPHA 2476
+#define SCI_MARKERGETLAYER 2734
+#define SCI_MARKERSETLAYER 2735
 #define SC_MAX_MARGIN 4
 #define SC_MARGIN_SYMBOL 0
 #define SC_MARGIN_NUMBER 1
@@ -280,19 +278,37 @@ typedef sptr_t (*SciFnDirect)(sptr_t ptr, unsigned int iMessage, uptr_t wParam, 
 #define SC_ELEMENT_LIST_BACK 1
 #define SC_ELEMENT_LIST_SELECTED 2
 #define SC_ELEMENT_LIST_SELECTED_BACK 3
+#define SC_ELEMENT_SELECTION_TEXT 10
+#define SC_ELEMENT_SELECTION_BACK 11
+#define SC_ELEMENT_SELECTION_ADDITIONAL_TEXT 12
+#define SC_ELEMENT_SELECTION_ADDITIONAL_BACK 13
+#define SC_ELEMENT_SELECTION_SECONDARY_TEXT 14
+#define SC_ELEMENT_SELECTION_SECONDARY_BACK 15
+#define SC_ELEMENT_SELECTION_NO_FOCUS_TEXT 16
+#define SC_ELEMENT_SELECTION_NO_FOCUS_BACK 17
+#define SC_ELEMENT_CARET 40
+#define SC_ELEMENT_CARET_ADDITIONAL 41
+#define SC_ELEMENT_CARET_LINE_BACK 50
+#define SC_ELEMENT_WHITE_SPACE 60
+#define SC_ELEMENT_WHITE_SPACE_BACK 61
+#define SC_ELEMENT_HOT_SPOT_ACTIVE 70
+#define SC_ELEMENT_HOT_SPOT_ACTIVE_BACK 71
 #define SCI_SETELEMENTCOLOUR 2753
 #define SCI_GETELEMENTCOLOUR 2754
 #define SCI_RESETELEMENTCOLOUR 2755
 #define SCI_GETELEMENTISSET 2756
 #define SCI_GETELEMENTALLOWSTRANSLUCENT 2757
-#define SCI_SETSELFORE 2067
-#define SCI_SETSELBACK 2068
-#define SCI_GETSELALPHA 2477
-#define SCI_SETSELALPHA 2478
+#define SCI_GETELEMENTBASECOLOUR 2758
 #define SCI_GETSELEOLFILLED 2479
 #define SCI_SETSELEOLFILLED 2480
 #define SCI_SETEOLSELECTEDWIDTH 2475
-#define SCI_SETCARETFORE 2069
+#define SC_LAYER_BASE 0
+#define SC_LAYER_UNDER_TEXT 1
+#define SC_LAYER_OVER_TEXT 2
+#define SCI_GETSELECTIONLAYER 2762
+#define SCI_SETSELECTIONLAYER 2763
+#define SCI_GETCARETLINELAYER 2764
+#define SCI_SETCARETLINELAYER 2765
 #define SCI_ASSIGNCMDKEY 2070
 #define SCI_CLEARCMDKEY 2071
 #define SCI_CLEARALLCMDKEYS 2072
@@ -350,19 +366,11 @@ typedef sptr_t (*SciFnDirect)(sptr_t ptr, unsigned int iMessage, uptr_t wParam, 
 #define SCI_INDICGETFLAGS 2685
 #define SCI_INDICSETSTROKEWIDTH 2751
 #define SCI_INDICGETSTROKEWIDTH 2752
-#define SCI_SETWHITESPACEFORE 2084
-#define SCI_SETWHITESPACEBACK 2085
 #define SCI_SETWHITESPACESIZE 2086
 #define SCI_GETWHITESPACESIZE 2087
-#define SCI_SETWHITESPACEFOREALPHA 2091
-#define SCI_GETWHITESPACEFOREALPHA 2109
 #define SCI_SETLINESTATE 2092
 #define SCI_GETLINESTATE 2093
 #define SCI_GETMAXLINESTATE 2094
-#define SCI_GETCARETLINEVISIBLE 2095
-#define SCI_SETCARETLINEVISIBLE 2096
-#define SCI_GETCARETLINEBACK 2097
-#define SCI_SETCARETLINEBACK 2098
 #define SCI_GETCARETLINEFRAME 2704
 #define SCI_SETCARETLINEFRAME 2705
 #define SCI_STYLESETCHANGEABLE 2099
@@ -418,7 +426,6 @@ typedef sptr_t (*SciFnDirect)(sptr_t ptr, unsigned int iMessage, uptr_t wParam, 
 #define SCI_GETHIGHLIGHTGUIDE 2135
 #define SCI_GETLINEENDPOSITION 2136
 #define SCI_GETCODEPAGE 2137
-#define SCI_GETCARETFORE 2138
 #define SCI_GETREADONLY 2140
 #define SCI_SETCURRENTPOS 2141
 #define SCI_SETSELECTIONSTART 2142
@@ -797,10 +804,6 @@ typedef sptr_t (*SciFnDirect)(sptr_t ptr, unsigned int iMessage, uptr_t wParam, 
 #define SCI_SETYCARETPOLICY 2403
 #define SCI_SETPRINTWRAPMODE 2406
 #define SCI_GETPRINTWRAPMODE 2407
-#define SCI_SETHOTSPOTACTIVEFORE 2410
-#define SCI_GETHOTSPOTACTIVEFORE 2494
-#define SCI_SETHOTSPOTACTIVEBACK 2411
-#define SCI_GETHOTSPOTACTIVEBACK 2495
 #define SCI_SETHOTSPOTACTIVEUNDERLINE 2412
 #define SCI_GETHOTSPOTACTIVEUNDERLINE 2496
 #define SCI_SETHOTSPOTSINGLELINE 2421
@@ -875,8 +878,6 @@ typedef sptr_t (*SciFnDirect)(sptr_t ptr, unsigned int iMessage, uptr_t wParam, 
 #define SCI_SETPASTECONVERTENDINGS 2467
 #define SCI_GETPASTECONVERTENDINGS 2468
 #define SCI_SELECTIONDUPLICATE 2469
-#define SCI_SETCARETLINEBACKALPHA 2470
-#define SCI_GETCARETLINEBACKALPHA 2471
 #define CARETSTYLE_INVISIBLE 0
 #define CARETSTYLE_LINE 1
 #define CARETSTYLE_BLOCK 2
@@ -995,12 +996,6 @@ typedef sptr_t (*SciFnDirect)(sptr_t ptr, unsigned int iMessage, uptr_t wParam, 
 #define SCI_GETVIRTUALSPACEOPTIONS 2597
 #define SCI_SETRECTANGULARSELECTIONMODIFIER 2598
 #define SCI_GETRECTANGULARSELECTIONMODIFIER 2599
-#define SCI_SETADDITIONALSELFORE 2600
-#define SCI_SETADDITIONALSELBACK 2601
-#define SCI_SETADDITIONALSELALPHA 2602
-#define SCI_GETADDITIONALSELALPHA 2603
-#define SCI_SETADDITIONALCARETFORE 2604
-#define SCI_GETADDITIONALCARETFORE 2605
 #define SCI_ROTATESELECTION 2606
 #define SCI_SWAPMAINANCHORCARET 2607
 #define SCI_MULTIPLESELECTADDNEXT 2688

scintilla/include/Scintilla.iface
@@ -408,13 +408,13 @@ val SC_MASK_FOLDERS=0xFE000000
 fun void MarkerDefine=2040(int markerNumber, MarkerSymbol markerSymbol)
 
 # Set the foreground colour used for a particular marker number.
-set void MarkerSetFore=2041(int markerNumber, colour fore)
+#set void MarkerSetFore=2041(int markerNumber, colour fore)
 
 # Set the background colour used for a particular marker number.
-set void MarkerSetBack=2042(int markerNumber, colour back)
+#set void MarkerSetBack=2042(int markerNumber, colour back)
 
 # Set the background colour used for a particular marker number when its folding block is selected.
-set void MarkerSetBackSelected=2292(int markerNumber, colour back)
+#set void MarkerSetBackSelected=2292(int markerNumber, colour back)
 
 # Set the foreground colour used for a particular marker number.
 set void MarkerSetForeTranslucent=2294(int markerNumber, colouralpha fore)
@@ -457,7 +457,13 @@ fun void MarkerDefinePixmap=2049(int markerNumber, string pixmap)
 fun void MarkerAddSet=2466(line line, int markerSet)
 
 # Set the alpha used for a marker that is drawn in the text area, not the margin.
-set void MarkerSetAlpha=2476(int markerNumber, Alpha alpha)
+#set void MarkerSetAlpha=2476(int markerNumber, Alpha alpha)
+
+# Get the layer used for a marker that is drawn in the text area, not the margin.
+get Layer MarkerGetLayer=2734(int markerNumber,)
+
+# Set the layer used for a marker that is drawn in the text area, not the margin.
+set void MarkerSetLayer=2735(int markerNumber, Layer layer)
 
 val SC_MAX_MARGIN=4
 
@@ -688,6 +694,21 @@ val SC_ELEMENT_LIST=0
 val SC_ELEMENT_LIST_BACK=1
 val SC_ELEMENT_LIST_SELECTED=2
 val SC_ELEMENT_LIST_SELECTED_BACK=3
+val SC_ELEMENT_SELECTION_TEXT=10
+val SC_ELEMENT_SELECTION_BACK=11
+val SC_ELEMENT_SELECTION_ADDITIONAL_TEXT=12
+val SC_ELEMENT_SELECTION_ADDITIONAL_BACK=13
+val SC_ELEMENT_SELECTION_SECONDARY_TEXT=14
+val SC_ELEMENT_SELECTION_SECONDARY_BACK=15
+val SC_ELEMENT_SELECTION_NO_FOCUS_TEXT=16
+val SC_ELEMENT_SELECTION_NO_FOCUS_BACK=17
+val SC_ELEMENT_CARET=40
+val SC_ELEMENT_CARET_ADDITIONAL=41
+val SC_ELEMENT_CARET_LINE_BACK=50
+val SC_ELEMENT_WHITE_SPACE=60
+val SC_ELEMENT_WHITE_SPACE_BACK=61
+val SC_ELEMENT_HOT_SPOT_ACTIVE=70
+val SC_ELEMENT_HOT_SPOT_ACTIVE_BACK=71
 
 # Set the colour of an element. Translucency (alpha) may or may not be significant
 # and this may depend on the platform. The alpha byte should commonly be 0xff for opaque.
@@ -706,17 +727,20 @@ get bool GetElementIsSet=2756(Element element,)
 # Get whether an element supports translucency.
 get bool GetElementAllowsTranslucent=2757(Element element,)
 
+# Get the colour of an element.
+get colouralpha GetElementBaseColour=2758(Element element,)
+
 # Set the foreground colour of the main and additional selections and whether to use this setting.
-fun void SetSelFore=2067(bool useSetting, colour fore)
+#fun void SetSelFore=2067(bool useSetting, colour fore)
 
 # Set the background colour of the main and additional selections and whether to use this setting.
-fun void SetSelBack=2068(bool useSetting, colour back)
+#fun void SetSelBack=2068(bool useSetting, colour back)
 
 # Get the alpha of the selection.
-get Alpha GetSelAlpha=2477(,)
+#get Alpha GetSelAlpha=2477(,)
 
 # Set the alpha of the selection.
-set void SetSelAlpha=2478(Alpha alpha,)
+#set void SetSelAlpha=2478(Alpha alpha,)
 
 # Is the selection end of line filled?
 get bool GetSelEOLFilled=2479(,)
@@ -727,8 +751,25 @@ set void SetSelEOLFilled=2480(bool filled,)
 # Set the eol-is-selected rectangle width.
 set void SetEOLSelectedWidth=2475(int percent,)
 
+enu Layer=SC_LAYER_
+val SC_LAYER_BASE=0
+val SC_LAYER_UNDER_TEXT=1
+val SC_LAYER_OVER_TEXT=2
+
+# Get the layer for drawing selections
+get Layer GetSelectionLayer=2762(,)
+
+# Set the layer for drawing selections: either opaquely on base layer or translucently over text
+set void SetSelectionLayer=2763(Layer layer,)
+
+# Get the layer of the background of the line containing the caret.
+get Layer GetCaretLineLayer=2764(,)
+
+# Set the layer of the background of the line containing the caret.
+set void SetCaretLineLayer=2765(Layer layer,)
+
 # Set the foreground colour of the caret.
-set void SetCaretFore=2069(colour fore,)
+#set void SetCaretFore=2069(colour fore,)
 
 # When key+modifier combination keyDefinition is pressed perform sciCommand.
 fun void AssignCmdKey=2070(keymod keyDefinition, int sciCommand)
@@ -879,24 +920,16 @@ set void IndicSetStrokeWidth=2751(int indicator, int hundredths)
 get int IndicGetStrokeWidth=2752(int indicator,)
 
 # Set the foreground colour of all whitespace and whether to use this setting.
-fun void SetWhitespaceFore=2084(bool useSetting, colour fore)
+#fun void SetWhitespaceFore=2084(bool useSetting, colour fore)
 
 # Set the background colour of all whitespace and whether to use this setting.
-fun void SetWhitespaceBack=2085(bool useSetting, colour back)
+#fun void SetWhitespaceBack=2085(bool useSetting, colour back)
 
 # Set the size of the dots used to mark space characters.
 set void SetWhitespaceSize=2086(int size,)
 
 # Get the size of the dots used to mark space characters.
 get int GetWhitespaceSize=2087(,)
-
-
-# Set the foreground alpha of all whitespace.
-set void SetWhitespaceForeAlpha=2091(Alpha alpha,)
-
-# Get the foreground alpha of all whitespace.
-get Alpha GetWhitespaceForeAlpha=2109(,)
-
 
 # Used to hold extra styling information for each line.
 set void SetLineState=2092(line line, int state)
@@ -908,16 +941,16 @@ get int GetLineState=2093(line line,)
 get int GetMaxLineState=2094(,)
 
 # Is the background of the line containing the caret in a different colour?
-get bool GetCaretLineVisible=2095(,)
+#get bool GetCaretLineVisible=2095(,)
 
 # Display the background of the line containing the caret in a different colour.
-set void SetCaretLineVisible=2096(bool show,)
+#set void SetCaretLineVisible=2096(bool show,)
 
 # Get the colour of the background of the line containing the caret.
-get colour GetCaretLineBack=2097(,)
+#get colour GetCaretLineBack=2097(,)
 
 # Set the colour of the background of the line containing the caret.
-set void SetCaretLineBack=2098(colour back,)
+#set void SetCaretLineBack=2098(colour back,)
 
 # Retrieve the caret line frame width.
 # Width = 0 means this option is disabled.
@@ -1097,7 +1130,7 @@ get position GetLineEndPosition=2136(line line,)
 get int GetCodePage=2137(,)
 
 # Get the foreground colour of the caret.
-get colour GetCaretFore=2138(,)
+#get colour GetCaretFore=2138(,)
 
 # In read-only mode?
 get bool GetReadOnly=2140(,)
@@ -2218,16 +2251,16 @@ set void SetPrintWrapMode=2406(Wrap wrapMode,)
 get Wrap GetPrintWrapMode=2407(,)
 
 # Set a fore colour for active hotspots.
-set void SetHotspotActiveFore=2410(bool useSetting, colour fore)
+#set void SetHotspotActiveFore=2410(bool useSetting, colour fore)
 
 # Get the fore colour for active hotspots.
-get colour GetHotspotActiveFore=2494(,)
+#get colour GetHotspotActiveFore=2494(,)
 
 # Set a back colour for active hotspots.
-set void SetHotspotActiveBack=2411(bool useSetting, colour back)
+#set void SetHotspotActiveBack=2411(bool useSetting, colour back)
 
 # Get the back colour for active hotspots.
-get colour GetHotspotActiveBack=2495(,)
+#get colour GetHotspotActiveBack=2495(,)
 
 # Enable / Disable underlining active hotspots.
 set void SetHotspotActiveUnderline=2412(bool underline,)
@@ -2462,10 +2495,10 @@ get bool GetPasteConvertEndings=2468(,)
 fun void SelectionDuplicate=2469(,)
 
 # Set background alpha of the caret line.
-set void SetCaretLineBackAlpha=2470(Alpha alpha,)
+#set void SetCaretLineBackAlpha=2470(Alpha alpha,)
 
 # Get the background alpha of the caret line.
-get Alpha GetCaretLineBackAlpha=2471(,)
+#get Alpha GetCaretLineBackAlpha=2471(,)
 
 enu CaretStyle=CARETSTYLE_
 val CARETSTYLE_INVISIBLE=0
@@ -2810,23 +2843,23 @@ get int GetRectangularSelectionModifier=2599(,)
 
 # Set the foreground colour of additional selections.
 # Must have previously called SetSelFore with non-zero first argument for this to have an effect.
-set void SetAdditionalSelFore=2600(colour fore,)
+#set void SetAdditionalSelFore=2600(colour fore,)
 
 # Set the background colour of additional selections.
 # Must have previously called SetSelBack with non-zero first argument for this to have an effect.
-set void SetAdditionalSelBack=2601(colour back,)
+#set void SetAdditionalSelBack=2601(colour back,)
 
 # Set the alpha of the selection.
-set void SetAdditionalSelAlpha=2602(Alpha alpha,)
+#set void SetAdditionalSelAlpha=2602(Alpha alpha,)
 
 # Get the alpha of the selection.
-get Alpha GetAdditionalSelAlpha=2603(,)
+#get Alpha GetAdditionalSelAlpha=2603(,)
 
 # Set the foreground colour of additional carets.
-set void SetAdditionalCaretFore=2604(colour fore,)
+#set void SetAdditionalCaretFore=2604(colour fore,)
 
 # Get the foreground colour of additional carets.
-get colour GetAdditionalCaretFore=2605(,)
+#get colour GetAdditionalCaretFore=2605(,)
 
 # Set the main selection to the next selection.
 fun void RotateSelection=2606(,)

scintilla/lexers/LexGo.cxx
@@ -413,6 +413,8 @@ void ColouriseGoDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initStyl
 						case GoFunction_Param:
 							funcState = GoFunction_Return;
 							break;
+						default:
+							break;
 						}
 						break;
 					case ')':

scintilla/lexers/LexJSON.cxx
@@ -217,6 +217,8 @@ void ColouriseJSONDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initSt
 						chNext = styler.SafeGetCharAt(i + 1);
 					}
 					break;
+				default:
+					break;
 				}
 			}
 		}

scintilla/lexers/LexJulia.cxx
@@ -408,7 +408,7 @@ void ColouriseJuliaDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initS
 					}
 				}
 
-				if (sc.chNext == ':' && ( sc.ch == ':' || sc.ch == '<' || sc.ch == '>')) {
+				if (sc.chNext == ':' && (sc.ch == ':' || sc.ch == '<' || sc.ch == '>')) {
 					// type after ::, <:, >:
 					maybeType = true;
 					sc.Forward();

scintilla/lexers/LexTOML.cxx
@@ -156,6 +156,8 @@ void ColouriseTOMLDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initSt
 						keyState = TOMLKeyState_Unquoted;
 					}
 					break;
+				default:
+					break;
 				}
 				if (keyState == TOMLKeyState_Unquoted) {
 					if (sc.ch == '\'') {

commit 1acadf64c27121886a0a3aa078c6c1f2bfaf88ae

Change caret line default alpha back to 255 (opaque), issue #369.
Fixes wrong caret line configuration introduced by a2a101351658f2da6968a67c360355a8e78f7fcd.

src/Styles.c
@@ -427,7 +427,6 @@ enum ANSIArtStyleIndex {
 #define IMEIndicatorDefaultColor	RGB(0x10, 0x80, 0x10)
 #define MarkOccurrencesDefaultAlpha	100
 #define SelectionDefaultAlpha		95
-#define CaretLineDefaultAlpha		90
 
 #define	BookmarkImageDefaultColor	RGB(0x40, 0x80, 0x40)
 #define	BookmarkLineDefaultColor	RGB(0, 0xff, 0)
@@ -3190,7 +3189,7 @@ void Style_HighlightCurrentLine(void) {
 
 			int alpha;
 			if (!Style_StrGetAlphaEx(outline, szValue, &alpha)) {
-				alpha = CaretLineDefaultAlpha;
+				alpha = SC_ALPHA_OPAQUE;
 			}
 			SciCall_SetElementColor(SC_ELEMENT_CARET_LINE_BACK, ColorAlpha(rgb, alpha));
 			return;