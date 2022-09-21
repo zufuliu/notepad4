9f98fc612fe71394c98dd9815ff055ddc5a0296b
zufuliu <zufuliu@gmail.com>
2022-09-21 18:23:44 +0800

    LLVM-15.0.1.

--- build/install_llvm.bat
+++ build/install_llvm.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 @rem used for AppVeyor and GitHub Actions
 
-curl -fsSL -o "LLVM-15.0.0-win64.exe" "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.0/LLVM-15.0.0-win64.exe"
-"LLVM-15.0.0-win64.exe" /S
+curl -fsSL -o "LLVM-15.0.1-win64.exe" "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.1/LLVM-15.0.1-win64.exe"
+"LLVM-15.0.1-win64.exe" /S
 
 IF /I "%~1" == "latest" (
 git clone -q --depth=1 --branch=main https://github.com/zufuliu/llvm-utils.git
