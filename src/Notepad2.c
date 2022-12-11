62f9b8cbce68de14f891c310453a225d762f351d
zufuliu <zufuliu@gmail.com>
2022-12-11 08:48:24 +0800

    Add code to load toolbar image based on current DPI, issue #73.

--- src/Notepad2.c
+++ src/Notepad2.c
@@ -1968,7 +1968,14 @@ void CreateBars(HWND hwnd, HINSTANCE hInstance) {
 	if (hbmp != NULL) {
 		bExternalBitmap = true;
 	} else {
-		hbmp = (HBITMAP)LoadImage(hInstance, MAKEINTRESOURCE(IDR_MAINWND), IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION);
+		int resource = IDB_TOOLBAR16;
+		if (g_uCurrentDPI > USER_DEFAULT_SCREEN_DPI) {
+			NP2_static_assert(IDB_TOOLBAR48 - IDB_TOOLBAR16 == 6 - 2);
+			int scale = (g_uCurrentDPI + USER_DEFAULT_SCREEN_DPI/2 - 1) / (USER_DEFAULT_SCREEN_DPI/2);
+			scale = max_i(scale, 6);
+			resource = IDB_TOOLBAR16 + scale - 2;
+		}
+		hbmp = (HBITMAP)LoadImage(hInstance, MAKEINTRESOURCE(resource), IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION);
 	}
 	if (bAutoScaleToolbar) {
 		hbmp = ResizeToolbarImageForCurrentDPI(hbmp);
