6063c010eca0edc74ee08fc9bb39f69dab9f8306
zufuliu <zufuliu@gmail.com>
2023-01-31 19:58:59 +0800

    Fix missing parenthesis.

--- src/Bridge.cpp
+++ src/Bridge.cpp
@@ -598,7 +598,7 @@ extern "C" void EditPrintSetup(HWND hwnd) {
 namespace {
 
 #if (__cplusplus > 201703L || (defined(_MSVC_LANG) && _MSVC_LANG > 201703L)) && ( \
-	(defined(_MSC_VER) && _MSC_VER >= 1920 && (defined(_WIN64) || !defined(__clang__)) || \
+	(defined(_MSC_VER) && _MSC_VER >= 1920 && (defined(_WIN64) || !defined(__clang__))) || \
 	(defined(_LIBCPP_VERSION) && _LIBCPP_VERSION >= 16000) || \
 	(!defined(_LIBCPP_VERSION) && defined(__GNUC__) && __GNUC__ >= 11) )
 using std::make_unique_for_overwrite; // requires C++20 library support
