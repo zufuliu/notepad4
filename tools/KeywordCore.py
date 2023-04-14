3f0d9e1a0e9b8f9aceb04770475ca51e04511822
zufuliu <zufuliu@gmail.com>
2023-04-14 21:01:30 +0800

    [Markdown] Update HTML block element list.

--- tools/KeywordCore.py
+++ tools/KeywordCore.py
@@ -1639,7 +1639,7 @@ def parse_llvm_api_file(path):
 
 def parse_markdown_api_file(path):
 	blockTag = ['pre', 'script', 'style', 'textarea'] # type 1
-	type6 = "address, article, aside, base, basefont, blockquote, body, caption, center, col, colgroup, dd, details, dialog, dir, div, dl, dt, fieldset, figcaption, figure, footer, form, frame, frameset, h1, h2, h3, h4, h5, h6, head, header, hr, html, iframe, legend, li, link, main, menu, menuitem, nav, noframes, ol, optgroup, option, p, param, section, source, summary, table, tbody, td, tfoot, th, thead, title, tr, track, ul"
+	type6 = "address, article, aside, base, basefont, blockquote, body, caption, center, col, colgroup, dd, details, dialog, dir, div, dl, dt, fieldset, figcaption, figure, footer, form, frame, frameset, h1, h2, h3, h4, h5, h6, head, header, hr, html, iframe, legend, li, link, main, menu, menuitem, nav, noframes, ol, optgroup, option, p, param, search, section, summary, table, tbody, td, tfoot, th, thead, title, tr, track, ul"
 	blockTag.extend(type6.replace(',', ' ').split())
 	return [
 		('html block tag', blockTag, KeywordAttr.NoAutoComp),
