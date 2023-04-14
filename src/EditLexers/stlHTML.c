ded267973d4a4ec62dcf292995c5281b7a321b52
zufuliu <zufuliu@gmail.com>
2023-04-14 19:39:57 +0800

    [HTML] Add `search` element.

--- src/EditLexers/stlHTML.c
+++ src/EditLexers/stlHTML.c
@@ -9,7 +9,7 @@ static KEYWORDLIST Keywords_HTML = {{
 "form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd "
 "keygen label legend li link main map mark meta meter nav noscript object ol "
 "optgroup option output p param picture pre progress q rp rt ruby s samp script section "
-"select slot small source span strong style sub summary sup table tbody td template textarea "
+"search select slot small source span strong style sub summary sup table tbody td template textarea "
 "tfoot th thead time title tr track u ul var video wbr "
 "math svg "
 
