a083cefa52de96d9ac1ea9fa875536fb654b8517
zufuliu <zufuliu@gmail.com>
2024-06-28 18:34:36 +0800

    Fix MSVC CI failures.

--- scintilla/src/PositionCache.cxx
+++ scintilla/src/PositionCache.cxx
@@ -823,7 +823,7 @@ LineLayout *LineLayoutCache::Retrieve(Sci::Line lineNumber, Sci::Line lineCaret,
 namespace {
 
 // Simply pack the (maximum 4) character bytes into an int
-#if defined(__SANITIZE_ADDRESS__) || (defined(__clang__) && __has_feature(address_sanitizer))
+#if USE_ADDRESS_SANITIZER
 constexpr unsigned int KeyFromString(std::string_view charBytes) noexcept {
 	PLATFORM_ASSERT(charBytes.length() <= 4);
 	unsigned int k = 0;
