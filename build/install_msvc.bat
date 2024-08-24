3ca39c844962682e8c6e156ca04c9e08569158b9
zufuliu <zufuliu@gmail.com>
2024-08-24 10:34:17 +0800

    Revert wrongly committed file.

--- build/install_msvc.bat
+++ build/install_msvc.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
 @rem used for GitHub Actions
+SetLocal EnableExtensions
 
 SET "VSINSTALLER=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vs_installer.exe"
 SET "VSPATH=C:\Program Files\Microsoft Visual Studio\2022\Enterprise"
