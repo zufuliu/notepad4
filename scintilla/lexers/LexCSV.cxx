3a722d7cf88c985ff37db2b6e9690bc2eebcff98
YX Hao <lifenjoiner@163.com>
2024-12-20 18:52:55 +0800

    [CSV] Fix row count for group folding

--- scintilla/lexers/LexCSV.cxx
+++ scintilla/lexers/LexCSV.cxx
@@ -47,7 +47,7 @@ void ColouriseCSVDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initSty
 	initStyle = SCE_CSV_COLUMN_0;
 	Sci_Line lineCurrent = styler.GetLine(startPos);
 	if (lineCurrent > 0) {
-		rows = static_cast<int>(lineCurrent % CsvRowGroup);
+		rows = static_cast<int>((lineCurrent - 1) % CsvRowGroup);
 		const int lineState = styler.GetLineState(lineCurrent - 1);
 		if (lineState) {
 			quoted = true;
