434d1e5426373e142ccdd2b4a46e715f9d84d2ce
zufuliu <zufuliu@gmail.com>
2025-01-12 09:14:10 +0800

    Auto close HTML `<p>` tag.

--- src/EditAutoC.cpp
+++ src/EditAutoC.cpp
@@ -1987,7 +1987,8 @@ void EditAutoCloseXMLTag() noexcept {
 			tchIns[cchIns] = ' ';
 			tchIns[cchIns + 1] = '\0';
 
-			if (cchIns > 2 && (pLexCurrent->iLexer == SCLEX_HTML || pLexCurrent->iLexer == SCLEX_PHPSCRIPT)) {
+			if (cchIns > 3 && (pLexCurrent->iLexer == SCLEX_HTML || pLexCurrent->iLexer == SCLEX_PHPSCRIPT)) {
+				// HTML void tag except <p>
 				if (IsHtmlVoidTag(tchIns + 1)) {
 					autoClosed = true;
 					cchIns = 0;
