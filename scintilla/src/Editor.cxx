dd333126b769b7252d70bae050393cf0f488c786
zufuliu <zufuliu@gmail.com>
2025-09-29 20:47:12 +0800

    Eliminate `InvalidateStyleRedraw()` for `Message::SetUseTabs`. See https://sourceforge.net/p/scintilla/feature-requests/1567/

--- scintilla/src/Editor.cxx
+++ scintilla/src/Editor.cxx
@@ -735,7 +735,7 @@ void Editor::SetSelectionFromSerialized(const char *serialized) {
 		::new (&sel) Selection(serialized);
 		sel.Truncate(pdoc->Length());
 		SetRectangularRange();
-		InvalidateStyleRedraw();
+		Redraw();
 	}
 }
 #endif
@@ -7137,7 +7137,6 @@ sptr_t Editor::WndProc(Message iMessage, uptr_t wParam, sptr_t lParam) {
 
 	case Message::SetUseTabs:
 		pdoc->useTabs = wParam != 0;
-		InvalidateStyleRedraw();
 		break;
 
 	case Message::GetUseTabs:
