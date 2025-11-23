330b96ddeeedfe108c24b3c2c09c977a579f4e23
zufuliu <zufuliu@gmail.com>
2025-11-23 16:29:42 +0800

    LLVM 21.1.6.

--- build/install_llvm.bat
+++ build/install_llvm.bat
@@ -1,14 +1,14 @@
 @ECHO OFF
 @rem used for AppVeyor and GitHub Actions
 
-curl -fsSL -o "LLVM-21.1.5-win64.exe" "https://github.com/llvm/llvm-project/releases/download/llvmorg-21.1.5/LLVM-21.1.5-win64.exe"
-"LLVM-21.1.5-win64.exe" /S
+curl -fsSL -o "LLVM-21.1.6-win64.exe" "https://github.com/llvm/llvm-project/releases/download/llvmorg-21.1.6/LLVM-21.1.6-win64.exe"
+"LLVM-21.1.6-win64.exe" /S
 
 IF /I "%~1" == "latest" (
 git clone -q --depth=1 --branch=main https://github.com/zufuliu/llvm-utils.git
 CALL "llvm-utils\VS2017\install.bat" %2
 ) ELSE (
-curl -fsSL -o "LLVM_VS2017.zip" "https://github.com/zufuliu/llvm-utils/releases/download/v25.05/LLVM_VS2017.zip"
+curl -fsSL -o "LLVM_VS2017.zip" "https://github.com/zufuliu/llvm-utils/releases/download/v25.11/LLVM_VS2017.zip"
 7z x -y "LLVM_VS2017.zip" >NUL
 CALL "LLVM_VS2017\install.bat" %1
 )
