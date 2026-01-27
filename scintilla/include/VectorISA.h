7a307dfcfc1d06cc4b5f3d995a875c6337c47008
zufuliu <zufuliu@gmail.com>
2026-01-27 19:51:36 +0800

    Add missing changed file for previous commit.

--- scintilla/include/VectorISA.h
+++ scintilla/include/VectorISA.h
@@ -178,9 +178,13 @@
 
 
 #if defined(__GNUC__) || defined(__clang__)
+#define bswap16(x)				__builtin_bswap16(x)
 #define bswap32(x)				__builtin_bswap32(x)
+#define bswap64(x)				__builtin_bswap64(x)
 #else
+#define bswap16(x)				_byteswap_ushort(x)
 #define bswap32(x)				_byteswap_ulong(x)
+#define bswap64(x)				_byteswap_uint64(x)
 #endif
 
 #if defined(__clang__)
