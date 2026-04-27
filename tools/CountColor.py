ba3dd54b507b142d256049b388cd36e781146e90
zufuliu <zufuliu@gmail.com>
2026-04-27 18:34:37 +0800

    Minor refactoring scheme color count script.

--- tools/CountColor.py
+++ tools/CountColor.py
@@ -1,15 +1,26 @@
 import sys
 import os.path
 import re
+import codecs
+import ctypes
 
 # count color on exported scheme file
-kReColorHex = re.compile(r'#[0-9A-Fa-f]{6}')
+kReColorHex = re.compile(r'(fore|back)?\s*:?\s*(#[0-9A-Fa-f]{6})')
+CP_ACP = f'cp{ctypes.windll.kernel32.GetACP()}'
+
+def read_file(path):
+	with open(path, 'rb') as fd:
+		doc = fd.read()
+	if doc.startswith(codecs.BOM_UTF8):
+		return doc[len(codecs.BOM_UTF8):].decode('utf-8')
+	if doc.startswith(codecs.BOM_UTF16_LE):
+		return doc[len(codecs.BOM_UTF16_LE):].decode('utf_16_le')
+	try:
+		return doc.decode('utf-8')
+	except UnicodeDecodeError:
+		return doc.decode(CP_ACP)
 
 def parse_key_value(line):
-	line = line.strip()
-	if not line or line[0] in ';#[':
-		return None
-
 	items = line.split('=', 2)
 	if not items or len(items) != 2:
 		return None
@@ -22,12 +33,14 @@ def parse_key_value(line):
 	return items
 
 def find_color_in_file(path, color_map):
-	with open(path, encoding='utf-8') as fd:
-		lines = fd.readlines()
+	lines = read_file(path).splitlines()
 	scheme = ''
 	for line in lines:
-		if line.startswith('['):
-			scheme = line[1:-2]
+		line = line.strip()
+		if not line or line[0] in ';#':
+			continue
+		if line[0] == '[':
+			scheme = line[1:-1]
 			continue
 		items = parse_key_value(line)
 		if not items:
@@ -39,10 +52,12 @@ def find_color_in_file(path, color_map):
 
 		key = items[0]
 		for color in colors:
-			color = color.upper()
+			fore = color[0]
+			color = color[1].upper()
 			if color in color_map:
 				color_stat = color_map[color]
 				color_stat['total_count'] += 1
+				color_stat[fore] += 1
 				if key not in color_stat['usage']:
 					color_stat['usage'][key] = [scheme]
 				else:
@@ -50,15 +65,23 @@ def find_color_in_file(path, color_map):
 			else:
 				color_stat = {
 					'total_count': 1,
+					'fore': 0,
+					'back': 0,
 					'usage': {
 						key: [scheme],
 					},
 				}
+				color_stat[fore] = 1
 				color_map[color] = color_stat
 
 def print_color_count(color_map):
 	for color, color_stat in color_map.items():
-		print(f"{color}\t{color_stat['total_count']}")
+		lines = []
+		if fore := color_stat['fore']:
+			lines.append(f'fore: {fore}')
+		if back := color_stat['back']:
+			lines.append(f'back: {back}')
+		print(f"{color}\t{color_stat['total_count']} {' '.join(lines)}")
 		usage = color_stat['usage']
 		for key, items in usage.items():
 			count = len(items)
@@ -66,7 +89,7 @@ def print_color_count(color_map):
 			print(f'\t{count}\t{key}\t[{scheme}]')
 
 def count_color(path):
-	# { color : { total_count: total_count, usage: { key: [scheme]}}}
+	# { color : { total_count: 0, fore: 0, back: 0, usage: { key: [scheme]}}}
 	color_map = {}
 	find_color_in_file(path, color_map)
 
