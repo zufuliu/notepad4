7b85c3eeac5e44cff9a2f51678880094ab8f9b82
zufuliu <zufuliu@gmail.com>
2026-08-30 15:21:58 +0800

    LLVM 23.1.0.

--- build/install_llvm.bat
+++ build/install_llvm.bat
@@ -1,14 +1,16 @@
 @ECHO OFF
 @rem used for AppVeyor and GitHub Actions
 
-curl -fsSL -o "LLVM-22.1.8-win64.exe" "https://github.com/llvm/llvm-project/releases/download/llvmorg-22.1.8/LLVM-22.1.8-win64.exe"
-"LLVM-22.1.8-win64.exe" /S
+curl -fsSL -o "LLVM-23.1.0-win64.msi" "https://github.com/llvm/llvm-project/releases/download/llvmorg-23.1.0/LLVM-23.1.0-win64.msi"
+msiexec.exe /quiet /passive /qn /i "LLVM-23.1.0-win64.msi"
+reg add HKLM\SOFTWARE\LLVM\LLVM /t REG_SZ /d "C:\Program Files\LLVM\"
+del "LLVM-23.1.0-win64.msi"
 
 IF /I "%~1" == "latest" (
 git clone -q --depth=1 --branch=main https://github.com/zufuliu/llvm-utils.git
 CALL "llvm-utils\VS2017\install.bat" %2
 ) ELSE (
-curl -fsSL -o "LLVM_VS2017.zip" "https://github.com/zufuliu/llvm-utils/releases/download/v26.03/LLVM_VS2017.zip"
+curl -fsSL -o "LLVM_VS2017.zip" "https://github.com/zufuliu/llvm-utils/releases/download/v26.08/LLVM_VS2017.zip"
 7z x -y "LLVM_VS2017.zip" >NUL
 CALL "LLVM_VS2017\install.bat" %1
 )
