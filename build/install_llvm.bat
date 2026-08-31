d1ee641a8834f6140dbe7847927cd53a877d5d0c
zufuliu <zufuliu@gmail.com>
2026-08-31 13:46:16 +0800

    Correct `reg add` command for setting LLVM installation path.

--- build/install_llvm.bat
+++ build/install_llvm.bat
@@ -3,7 +3,7 @@
 
 curl -fsSL -o "LLVM-23.1.0-win64.msi" "https://github.com/llvm/llvm-project/releases/download/llvmorg-23.1.0/LLVM-23.1.0-win64.msi"
 msiexec.exe /quiet /passive /qn /i "LLVM-23.1.0-win64.msi"
-reg add HKLM\SOFTWARE\LLVM\LLVM /t REG_SZ /d "C:\Program Files\LLVM\"
+reg add "HKLM\SOFTWARE\LLVM\LLVM" /f /t REG_SZ /d "C:\Program Files\LLVM"
 del "LLVM-23.1.0-win64.msi"
 
 IF /I "%~1" == "latest" (
